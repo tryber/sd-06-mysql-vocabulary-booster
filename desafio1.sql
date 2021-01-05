4d2e9d4f021c88ff92f2238cd21a1829ca7fbc29
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-05 15:46:20 -0300

    fix: lint

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
 SELECT 
-	COUNTRY_NAME AS 'País',
-    IF (region_id = 1, 'incluído', 'não incluído') AS 'Status de Inclusão'
+COUNTRY_NAME AS 'País',
+  IF (region_id = 1, 'incluído', 'não incluído') AS 'Status de Inclusão'
 FROM hr.countries;
