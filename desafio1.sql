6eeaf6e060b1ac52cdf21eadf97850fc5f4da915
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-05 15:52:31 -0300

    fix: include text

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
 SELECT 
 COUNTRY_NAME AS 'País',
-  IF (region_id = 1, 'incluído', 'não incluído') AS 'Status de Inclusão'
+  IF (region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries;
