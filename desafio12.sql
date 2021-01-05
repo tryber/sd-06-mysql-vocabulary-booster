965b1e9c905c5ef61be5c2ba00afa74060d2d7df
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2021-01-05 18:15:55 -0300

    tabs

--- desafio12.sql
+++ desafio12.sql
@@ -5,7 +5,7 @@ CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS 'Nome completo funcionário 2',
 e2.SALARY AS 'Salário funcionário 2',
 e2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees AS e1
-	LEFT JOIN hr.employees AS e2
-    ON e1.JOB_ID = e2.JOB_ID
+LEFT JOIN hr.employees AS e2
+  ON e1.JOB_ID = e2.JOB_ID
 WHERE e1.PHONE_NUMBER <> e2.PHONE_NUMBER
 ORDER BY CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) ASC, CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) ASC;
--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN job_title VARCHAR(100))
 BEGIN
-  SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
-  FROM hr.employees AS e
-  LEFT JOIN hr.jobs AS j
-      ON e.JOB_ID = j.JOB_ID
-	WHERE j.JOB_TITLE = job_title;
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS e
+LEFT JOIN hr.jobs AS j
+    ON e.JOB_ID = j.JOB_ID
+WHERE j.JOB_TITLE = job_title;
 END $$
 
 DELIMITER ;
