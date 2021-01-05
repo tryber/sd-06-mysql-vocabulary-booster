c21bfff352c3a199a84ac6c305c2c115ea282f1f
HCaetano <hrcgeo@gmail.com>
2021-01-05 16:51:26 -0300

    Add challenge 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,10 @@
-SELECT job_title AS 'Cargo', 
+SELECT jobs.job_title AS 'Cargo', ROUND(AVG(employees.salary), 2) AS 'Média salarial',
 CASE
-  WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
-  WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
-  WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-  WHEN max_salary > 30000 THEN 'Altíssimo'
-END AS 'Nível'
-FROM jobs
-ORDER BY job_title;
+WHEN ROUND(AVG(employees.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(employees.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN ROUND(AVG(employees.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN ROUND(AVG(employees.salary), 2) > 10500 THEN 'CEO'
+END AS 'Senioridade'
+FROM employees
+INNER JOIN jobs ON employees.job_id = jobs.job_id
+GROUP BY jobs.job_title;
