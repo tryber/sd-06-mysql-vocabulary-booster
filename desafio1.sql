3ef17207e1518b924595b2ec23095249dc7e3c04
Paulo <paulocheloni@gmail.com>
2021-01-05 17:58:05 -0300

    adding exercise two

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,3 @@
-use hr;
-SELECT COUNTRY_NAME AS 'País', 
-IF(REGION_ID == 1, 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM countries ORDER BY COUNTRY_NAME ASC; 
+SELECT COUNTRY_NAME as 'País', 
+IF(REGION_ID=1, 'incluído', 'não incluído') as 'Status Inclusão'
+FROM hr.countries ORDER BY COUNTRY_NAME ASC; 
