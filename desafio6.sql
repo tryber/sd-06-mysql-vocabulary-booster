0de5129483bc2ed8e4f4e82e2e7087640042f5d3
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-01-05 19:44:57 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS 'Nome Completo',
+jo.JOB_TITLE AS Cargo,
+hi.START_DATE AS 'Data de início do cargo',
+de.DEPARTMENT_NAME AS Departamento
+FROM hr.employees AS em
+INNER JOIN hr.job_history AS hi
+ON hi.EMPLOYEE_ID = em.EMPLOYEE_ID
+INNER JOIN hr.jobs AS jo
+ON hi.JOB_ID = jo.JOB_ID
+INNER JOIN hr.departments AS de
+ON de.DEPARTMENT_ID = hi.DEPARTMENT_ID
+ORDER BY `Nome Completo` DESC, Cargo;
