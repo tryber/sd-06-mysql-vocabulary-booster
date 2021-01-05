4b19152dda7dc72ee8ffd5f34fce40b671d76348
LeandroParisi <lepcbelisario@gmail.com>
2021-01-05 17:09:20 -0300

    Ex.4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT j.job_title AS Cargo, ROUND(AVG(e.salary), 2) AS 'Média salarial',
+CASE
+WHEN ROUND(AVG(e.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(e.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN ROUND(AVG(e.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'Júnior'
+END AS 'Senioridade'
+FROM hr.jobs AS j
+JOIN hr.employees AS e ON j.job_id = e.job_id
+GROUP BY j.job_title
+ORDER BY AVG(e.salary) ASC, j.job_title ASC;
