2605cda542a7b4a808539a5394d5d29ae84a56b1
LeandroParisi <lepcbelisario@gmail.com>
2021-01-05 19:31:22 -0300

    Ex.9 CC 1

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FirstName, ' ', e.LastName) AS `Nome Completo`, COUNT(o.OrderID) AS `Total de pedidos`
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`, COUNT(o.OrderID) AS `Total de pedidos`
 FROM w3schools.orders AS o
 INNER JOIN w3schools.employees AS e ON e.EmployeeID = o.EmployeeID
 GROUP BY o.EmployeeID
