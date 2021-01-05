40a3b74ac59b0295df4789aae76bd218e9aaee2e
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-05 19:51:07 -0300

    Sixth challenge using the correct table

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,13 @@
 SELECT
 CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
 jo.JOB_TITLE AS Cargo,
-emp.HIRE_DATE AS 'Data de início do cargo',
+jh.START_DATE AS 'Data de início do cargo',
 dep.DEPARTMENT_NAME AS Departamento
-FROM hr.employees AS emp
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS emp
+ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID
 INNER JOIN hr.jobs AS jo
-ON emp.JOB_ID = jo.JOB_ID
+ON jh.JOB_ID = jo.JOB_ID
 INNER JOIN hr.departments AS dep
-ON emp.DEPARTMENT_ID = dep.DEPARTMENT_ID
-ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, Cargo;
+ON jh.DEPARTMENT_ID = dep.DEPARTMENT_ID
+ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, Cargo ASC;
