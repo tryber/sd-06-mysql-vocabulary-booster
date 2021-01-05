68d8541534f063b14db5e40f015f613e123465c3
isabellavjs <isabellavjs@gmail.com>
2021-01-05 18:58:34 -0300

    Correct chellenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -4,4 +4,5 @@ O.OrderDate AS `Data do pedido`
 FROM orders AS O
 INNER JOIN customers AS CU ON CU.CustomerID = O.CustomerID
 INNER JOIN shippers AS S ON S.ShipperID = O.ShipperID
+WHERE S.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY `Nome de contato`, S.ShipperName, O.OrderDate;
