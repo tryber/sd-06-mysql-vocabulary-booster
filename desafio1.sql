1dedcb9bacba8b0765df5497a6a722c520d58b8b
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-05 17:15:44 -0300

    Desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,5 @@
-SELECT 
-	c.COUNTRY_NAME as `País`,
-	IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') as `Status Inclusão`
+SELECT c.COUNTRY_NAME as `País`,
+IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') as `Status Inclusão`
 FROM hr.countries c
 INNER JOIN hr.regions r ON c.REGION_ID = r.REGION_ID
 ORDER BY c.COUNTRY_NAME;
