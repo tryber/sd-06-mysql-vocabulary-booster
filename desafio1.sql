65b27808252c77dd0b1849c3ab44b944685611db
Pedro <ikbenpedro@gmail.com>
2021-01-05 16:59:32 -0300

    desafio1.3

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
-SELECT COUNTRY_NAME AS `País`,
-CASE WHEN REGION_ID=1 THEN 'incluído'
-ELSE 'não incluído'
-END AS `Status Inclusão`
+SELECT COUNTRY_NAME AS `**País**`,
+CASE WHEN REGION_ID=1 THEN '"incluído"'
+ELSE '"não incluído"'
+END AS `**Status Inclusão**`
 FROM hr.countries;
