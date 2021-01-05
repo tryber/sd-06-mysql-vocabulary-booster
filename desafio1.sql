f4d20b1fe1abd597241ce30a95afe244fd34b853
HCaetano <hrcgeo@gmail.com>
2021-01-05 16:03:02 -0300

    Refactor challenge 1 to have order by

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
 SELECT country_name AS 'País', CASE
-  WHEN region_id = 1 THEN 'incluído' ELSE 'não incluído'
+    WHEN region_id = 1 THEN 'incluído' ELSE 'não incluído'
   END AS 'Status Inclusão'
-FROM hr.countries;
+FROM hr.countries
+ORDER BY country_name;
