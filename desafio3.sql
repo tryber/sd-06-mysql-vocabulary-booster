9f72aae5c260a63612f004b4422bf2b852894916
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-05 18:34:40 -0300

    ajeitando o ordenamento

--- desafio3.sql
+++ desafio3.sql
@@ -1,2 +1,2 @@
-SELECT JOB_TITLE AS "Cargo", (MAX_SALARY - MIN_SALARY) as "Diferença entre salários máximo e mínimo"
-FROM hr.jobs order by JOB_TITLE;
+SELECT JOB_TITLE AS "Cargo", (MAX_SALARY - MIN_SALARY) AS "Diferença entre salários máximo e mínimo"
+FROM hr.jobs ORDER BY (MAX_SALARY - MIN_SALARY) ASC;
