833695b56e6c1bc343e272bf18c612c098b7c63a
Thales Rangel <thalesgr@gmail.com>
2021-01-05 18:38:30 -0300

    exe 9 correçao2

--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,7 @@
-SELECT CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome Completo',
-COUNT(O.EmployeeID) AS 'Total de pedidos'
-FROM w3schools.employees AS E
-INNER JOIN w3schools.orders AS O
-ON O.EmployeeID = E.EmployeeID
-GROUP BY CONCAT(E.FirstName, ' ', E.LastName)
-ORDER BY COUNT(O.EmployeeID);
+SELECT CONCAT(emp.FirstName,' ',emp.LastName) AS 'Nome completo',
+COUNT(ord.EmployeeID) AS 'Total de pedidos'
+FROM w3schools.employees AS emp
+INNER JOIN w3schools.orders AS ord
+ON emp.EmployeeID = ord.EmployeeID
+GROUP BY CONCAT(emp.FirstName,' ',emp.LastName)
+ORDER BY COUNT(ord.EmployeeID);
