37c333b409d6c3ab947b025630e141cd52a14e4c
DehSartoreto <fixsartoreto@gmail.com>
2021-01-05 17:27:45 -0300

    desafio7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(emp.FIRST_NAME,' ', emp.LAST_NAME)) AS 'Nome completo',
+jobhis.START_DATE AS 'Data de início do cargo',
+emp.SALARY AS 'Salário'
+FROM hr.job_history as jobhis
+INNER JOIN hr.employees AS emp
+ON emp.EMPLOYEE_ID = jobhis.EMPLOYEE_ID
+WHERE MONTH(jobhis.START_DATE) <= 3
+ORDER BY emp.FIRST_NAME, jobhis.START_DATE;
