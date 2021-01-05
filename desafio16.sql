a9854c69dee6789d386d5ec13053cb3059a6da28
Luiz Mello <luizmacmillan@gmail.com>
2021-01-05 20:03:57 -0300

    Correção do Desafio 16

--- desafio16.sql
+++ desafio16.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE total_empregos INT
+DECLARE total_empregos INT;
 SELECT COUNT(j.EMPLOYEE_ID)
 FROM hr.employees e
 INNER JOIN hr.job_history j ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
 WHERE e.EMAIL = email
-INTO total_empregos
+INTO total_empregos;
 RETURN total_empregos;
 END $$
 
