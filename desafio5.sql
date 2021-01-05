99bfcb50e6b210dcf4b4b30637d10130cfc92cd8
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-05 19:09:30 -0300

    :bug: Corrigindo Desafio 05 (Segunda Correcao)

--- desafio5.sql
+++ desafio5.sql
@@ -6,5 +6,4 @@ ROUND(jobs.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM hr.employees AS emp
 JOIN hr.jobs AS jobs
 WHERE emp.JOB_ID = jobs.JOB_ID
-GROUP BY jobs.JOB_TITLE
 ORDER BY (jobs.MAX_SALARY - jobs.MIN_SALARY), jobs.JOB_TITLE;
