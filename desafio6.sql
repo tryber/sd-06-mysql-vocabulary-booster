df953e4dff78f7550bd2780c5e60a3e0d41a1f3e
Isa Koga <isadorakoga710@gmail.com>
2021-01-05 17:36:02 -0300

    :rotating-light: fix Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,11 @@ SELECT CONCAT(Employees.FIRST_NAME, ' ', Employees.LAST_NAME) AS 'Nome completo'
 Jobs.JOB_TITLE AS 'Cargo',
 Job_History.START_DATE AS 'Data de início do cargo',
 Departments.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS Employees
-INNER JOIN hr.jobs AS Jobs
-ON Jobs.JOB_ID = Employees.JOB_ID
-INNER JOIN hr.job_history AS Job_History
-ON Employees.EMPLOYEE_ID = Job_History.EMPLOYEE_ID
-INNER JOIN hr.departments AS Departments
-ON Job_History.DEPARTMENT_ID = Departments.DEPARTMENT_ID
+FROM hr.job_history AS job_history
+INNER JOIN hr.jobs AS jobs
+ON jobs.JOB_ID = job_history.JOB_ID
+INNER JOIN hr.employees AS employees
+ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+INNER JOIN hr.departments AS departments
+ON departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
 ORDER BY Employees.FIRST_NAME DESC, Jobs.JOB_TITLE;
