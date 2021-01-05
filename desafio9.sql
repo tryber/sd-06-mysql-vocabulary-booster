3ba0d22be31c6882b820c1e9ba3d8b5f9e70b873
Thales Rangel <thalesgr@gmail.com>
2021-01-05 18:31:45 -0300

    exe 9 correçao1

--- desafio9.sql
+++ desafio9.sql
@@ -3,5 +3,5 @@ COUNT(O.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS E
 INNER JOIN w3schools.orders AS O
 ON O.EmployeeID = E.EmployeeID
-GROUP BY E.EmployeeID
+GROUP BY CONCAT(E.FirstName, ' ', E.LastName)
 ORDER BY COUNT(O.EmployeeID);
