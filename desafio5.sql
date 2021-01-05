e9f9b467940161fccc87dbe69c6ab876b4a9548c
HCaetano <hrcgeo@gmail.com>
2021-01-05 17:10:06 -0300

    Add challenge 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
-SELECT job_title AS 'Cargo', 
-CASE
-  WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
-  WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
-  WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-  WHEN max_salary > 30000 THEN 'Altíssimo'
-END AS 'Nível'
-FROM jobs
-ORDER BY job_title;
+SELECT
+jobs.job_title AS Cargo,
+max_salary - min_salary AS 'Variação Salarial',
+ROUND(AVG(min_salary)/12, 2) AS 'Média mínima mensal',
+ROUND(AVG(max_salary )/12, 2) AS 'Média máxima mensal'
+FROM employees
+INNER JOIN jobs ON employees.job_id = jobs.job_id
+GROUP BY jobs.job_title
+ORDER BY max_salary - min_salary, Cargo;
