68a723de0c50b181c5553bfbfb30480ed46cb129
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-01-05 17:35:38 -0300

    Checking first requirement

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,4 @@
-First commit
+SELECT COUNTRY_NAME AS 'Pais',
+IF(REGION_ID = 1, 'incluído', 'não não incluído') AS 'Status Inclusão'
+FROM hr.countries
+GROUP BY COUNTRY_NAME ORDER BY COUNTRY_NAME ASC;
