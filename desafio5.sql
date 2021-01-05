3144e8cc9d46cb09516d1d23e200376e18d2ce10
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-05 19:38:51 -0300

    :bug: Desafio 05 (Quarta Correcao)

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,8 @@
 SELECT
-jobs.JOB_TITLE AS 'Cargo',
-(jobs.MAX_SALARY - jobs.MIN_SALARY) AS 'Variação Salarial',
-ROUND(jobs.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
-ROUND(jobs.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
-FROM hr.employees AS emp
-JOIN hr.jobs AS jobs
-WHERE emp.JOB_ID = jobs.JOB_ID
-GROUP BY jobs.JOB_TITLE
-ORDER BY (jobs.MAX_SALARY - jobs.MIN_SALARY), jobs.JOB_TITLE;
+JOB_TITLE AS 'Cargo',
+(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
+FROM hr.jobs
+GROUP BY JOB_ID
+ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
