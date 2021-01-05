eee79049050881b3fb25d056d6013d1c9425d54e
Thales Rangel <thalesgr@gmail.com>
2021-01-05 19:22:03 -0300

    exe 11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT C1.ContactName AS 'Nome',
+C1.Country AS 'País',
+(COUNT(*) - 1) AS 'Número de compatriotas'
+FROM w3schools.customers AS C1, w3schools.customers AS C2
+WHERE C1.Country = C2.Country
+GROUP BY C1.ContactName, C1.Country
+ORDER BY C1.ContactName;
