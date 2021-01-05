1d9a474a8d7ff71be8125cd56fa9026ed5606907
Luiz Mello <luizmacmillan@gmail.com>
2021-01-05 20:03:54 -0300

    Correção do Desafio 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,18 +1,18 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(
-IN email VARCHAR(100),
-OUT total_empregos INT
-)
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA
 BEGIN
-SELECT COUNT(j.EMPLOYEE_ID) AS `Total de Empregos` INTO total_empregos
+DECLARE total_empregos INT
+SELECT COUNT(j.EMPLOYEE_ID)
 FROM hr.employees e
 INNER JOIN hr.job_history j ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
-WHERE e.EMAIL = email;
+WHERE e.EMAIL = email
+INTO total_empregos
+RETURN total_empregos;
 END $$
 
 DELIMITER ;
 
-CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR', @total_empregos);
-SELECT @total_empregos;
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
