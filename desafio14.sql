23f473ca195fa3ec57f66a1445c6e0d8939b6916
Pablo Santos <pablo.trybe@gmail.com>
2021-01-05 20:23:30 -0300

    Fixing CC

--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,7 @@
-(SELECT Country AS 'País' FROM w3schools.customers)
+SELECT c.Country AS 'País'
+FROM w3schools.customers AS c
 UNION
-(SELECT Country FROM w3schools.suppliers)
-ORDER BY `País` LIMIT 5;
+SELECT s.Country AS 'País'
+FROM w3schools.suppliers AS s
+ORDER BY `País`
+LIMIT 5;
