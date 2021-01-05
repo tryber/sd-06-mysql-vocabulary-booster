7aae98c2b6e3f87ec0fe3811a7b019154a18b5c6
HCaetano <hrcgeo@gmail.com>
2021-01-05 18:19:55 -0300

    Add chellenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,6 @@
-SELECT job_title AS 'Cargo', 
-CASE
-  WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
-  WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
-  WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-  WHEN max_salary > 30000 THEN 'Altíssimo'
-END AS 'Nível'
-FROM jobs
-ORDER BY job_title;
+SELECT
+UPPER(CONCAT(first_name, ' ', last_name)) AS `Nome completo`,
+hire_date AS `Data de início`,
+salary AS Salario
+FROM employees
+ORDER BY `Nome completo`, `Data de início`;
