edfac6b1e1a45f658ad194d9d3cc464c991ab8b4
Alvaro Neto <alvaroshn@gmail.com>
2021-01-05 15:48:00 -0300

    Challenge 01

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
 USE hr;
-SELECT country_name AS 'País', IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão';
-FROM hr.countries;
+SELECT country_name AS 'País', IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries
 ORDER BY country_name;
