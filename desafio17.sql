41c5152c0354906f0b1ba1e72f4d8f885f512d41
Luiz Mello <luizmacmillan@gmail.com>
2021-01-05 20:57:41 -0300

    Desafio 17 incompleto.

---
+++ desafio17.sql
@@ -0,0 +1,17 @@
+USE w3schools;
+
+DELIMITER $$
+CREATE TRIGGER trigger_order_insert
+BEFORE INSERT ON orders
+FOR EACH ROW
+BEGIN
+SET NEW.OrderDate = DATE();
+END$$
+
+DELIMITER ;
+
+INSERT INTO w3schools.orders (CustomerID, EmployeeID, ShipperID)
+VALUES (4, 2, 2);
+
+SELECT * FROM w3schools.orders
+WHERE CustomerID = 4;
