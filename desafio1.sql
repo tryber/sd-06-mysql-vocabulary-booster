3357273cd27caba4e638179a18f5b812921633ea
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2021-01-05 16:42:03 -0300

    slight corrections

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT c.COUNTRY_NAME AS 'País',
-  IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluido') AS 'Status Inclusão'
+  IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS c
   LEFT JOIN hr.regions AS r
     ON c.REGION_ID = r.REGION_ID;
--- desafio6.sql
+++ desafio6.sql
@@ -6,7 +6,7 @@ FROM hr.job_history AS jh
 LEFT JOIN hr.employees AS e
   ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 LEFT JOIN hr.jobs AS j
-  ON e.JOB_ID = j.JOB_ID
+  ON jh.JOB_ID = j.JOB_ID
 LEFT JOIN hr.departments AS d
   ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.JOB_TITLE ASC;
