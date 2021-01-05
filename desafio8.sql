a7d292bbce8ed138ac6df6655f2a13ee6c383b79
HCaetano <hrcgeo@gmail.com>
2021-01-05 20:41:43 -0300

    Add challenge 9, add semi colon to challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -6,4 +6,4 @@ FROM order_details
 INNER JOIN orders ON order_details.OrderID = orders.OrderID 
 INNER JOIN customers ON orders.CustomerID = customers.CustomerID 
 INNER JOIN shippers ON shippers.ShipperID = orders.ShipperID
-ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,7 @@
-SELECT job_title AS 'Cargo', 
-CASE
-  WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
-  WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
-  WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-  WHEN max_salary > 30000 THEN 'Altíssimo'
-END AS 'Nível'
-FROM jobs
-ORDER BY job_title;
+SELECT
+CONCAT(firstName, ' ', lastName) AS `Nome completo`,
+COUNT(*) AS `Total de pedidos`
+FROM w3schools.employees
+INNER JOIN orders ON orders.EmployeeID = employees.EmployeeID 
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
