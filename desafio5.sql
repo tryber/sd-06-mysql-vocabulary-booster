8dde8036e23a6cab3fb37a33289ce60c61e5f40b
Alvaro Neto <alvaroshn@gmail.com>
2021-01-05 17:45:54 -0300

    Challenge 05 and 06

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,5 @@
 SELECT job_title AS 'Cargo',
 (max_salary - min_salary) AS 'Variação Salarial',
-ROUND(AVG(min_salary/12), 2) AS 'Média mínima mensal',
-ROUND(AVG(max_salary/12), 2) AS 'Média máxima mensal'
-FROM hr.employees AS emp
-INNER JOIN hr.jobs AS job
-ON (emp.job_id = job.job_id) GROUP BY job.job_title, (job.max_salary - job.min_salary)
-ORDER BY (job.max_salary - job.min_salary) ASC, job.job_title ASC;
+ROUND(min_salary/12, 2) AS 'Média mínima mensal',
+ROUND(max_salary/12, 2) AS 'Média máxima mensal'
+FROM hr.jobs ORDER BY (max_salary - min_salary), job_title;
