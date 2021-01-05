d7adabc3f648346873282b09da94c41cc7a1b796
Pablo Santos <pablo.trybe@gmail.com>
2021-01-05 20:18:29 -0300

    Fixing CC

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT job_title AS 'Cargo', ROUND(AVG(emp.salary), 2) AS 'Média Salarial',
+SELECT job_title AS 'Cargo', ROUND(AVG(emp.salary), 2) AS 'Média salarial',
 CASE 
   WHEN ROUND(AVG(emp.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
   WHEN ROUND(AVG(emp.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
