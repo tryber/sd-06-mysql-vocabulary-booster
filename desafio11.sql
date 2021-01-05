0c0c92968a41014f9bda661e5d3e5388c9582d48
Isa Koga <isadorakoga710@gmail.com>
2021-01-05 19:29:23 -0300

    :rotating-light: fix Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,5 +3,5 @@ customers1.Country AS 'País',
 (COUNT(*) - 1) AS 'Número de compatriotas'
 FROM w3schools.customers AS customers1, w3schools.customers AS customers2
 WHERE customers1.Country = customers2.Country
-GROUP BY customers1.ContactName, customers1.Country
+GROUP BY customers1.ContactName, customers1.Country HAVING (COUNT(*) - 1) > 0
 ORDER BY customers1.ContactName;
