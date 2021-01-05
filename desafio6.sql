e294cc95decff86b6a6ab4a9ce26dbecee458c7e
HCaetano <hrcgeo@gmail.com>
2021-01-05 18:12:38 -0300

    Add actual changes to challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,9 @@
 SELECT
-	CONCAT(first_name, ' ', last_name) AS 'Nome completo',
-  job_title AS Cargo,
-	hire_date AS 'Data de início do cargo',
-	department_name AS Departamento
+CONCAT(first_name, ' ', last_name) AS `Nome completo`,
+job_title AS Cargo,
+hire_date AS `Data de início do cargo`,
+department_name AS Departamento
 FROM employees
 INNER JOIN jobs ON employees.job_id = jobs.job_id
 INNER JOIN departments ON employees.department_id = departments.department_id
-GROUP BY jobs.job_title
-ORDER BY CONCAT(first_name, ' ', last_name) DESC, Cargo;
+ORDER BY `Nome completo` DESC, Cargo;
