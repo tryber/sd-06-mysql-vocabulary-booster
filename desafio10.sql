2b9c88255c4bc1afc2f80c64d0572ef0f649f514
DehSartoreto <fixsartoreto@gmail.com>
2021-01-05 18:34:43 -0300

    desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,9 @@
-a
+SELECT prod.ProductName AS 'Produto',
+MIN(orderD.Quantity) AS 'Mínima',
+MAX(orderD.Quantity) AS 'Máxima',
+ROUND(AVG(orderD.Quantity),2) AS 'Média'
+FROM w3schools.order_details AS orderD
+INNER JOIN w3schools.products AS prod
+ON prod.ProductID = orderD.ProductID
+GROUP BY prod.ProductName
+ORDER BY ROUND(AVG(orderD.Quantity),2), prod.ProductName;
