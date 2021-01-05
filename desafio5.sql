413de85eb90957f68cf685d55e210c96b8cbe716
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-01-05 19:13:00 -0300

    CFC

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 USE hr;
 SELECT JOB_TITLE AS 'Cargo',
-       MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
-       ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
-       ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
+        MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
+        ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+        ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM jobs
 ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE ASC;
