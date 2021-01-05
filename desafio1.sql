8de805f220ae92e40c13f5a8cff202baa03bc51a
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-05 17:27:50 -0300

    corrigindo a ordem do primeiro requisito

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,6 @@
-SELECT COUNTRY_NAME as "País", IF(REGION_ID = 1, "incluído", "não incluído") as "Status Inclusão" FROM hr.countries;
+SELECT COUNTRY_NAME as "País",
+	IF(REGION_ID = 1,
+		"incluído",
+        "não incluído"
+	) as "Status Inclusão"
+FROM hr.countries ORDER BY COUNTRY_NAME;
