5cb9012c6d651684ed9443e6ee879ab5fddec356
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-05 19:11:06 -0300

    fix: orderd as required

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,7 @@
 USE hr;
-SELECT c.COUNTRY_NAME País,
-  IF (r.REGION_NAME = 'Europe', 'incluído', 'não incluído') 
+SELECT c.COUNTRY_NAME AS País,
+  IF (r.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
     FROM countries c
       LEFT JOIN regions r
-        ON c.REGION_ID = r.REGION_ID;
+        ON c.REGION_ID = r.REGION_ID
+ORDER BY c.COUNTRY_NAME ASC;
