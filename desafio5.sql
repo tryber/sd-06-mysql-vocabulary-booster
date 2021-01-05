33dbb7fec0f1005883104b62b262551d548254f3
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-05 19:00:01 -0300

    Chal.5 stylelint fix

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,4 @@
-SELECT
-	hr_jobs.JOB_TITLE AS 'Cargo',
+SELECT hr_jobs.JOB_TITLE AS 'Cargo',
   (hr_jobs.MAX_SALARY - hr_jobs.MIN_SALARY) AS 'Variação Salarial',
   ROUND((hr_jobs.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
   ROUND((hr_jobs.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
