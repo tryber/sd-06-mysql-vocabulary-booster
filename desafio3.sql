48b6dbdeba6902a2df278aabd63f6a2da8afd042
vapatty6848 <vapatty@gmail.com>
2021-01-05 19:47:55 -0300

    correçao ortografica

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,3 @@
-SELECT job_title AS 'Cargo', (max_salary - min_salary) AS `Diferenca entre salários máximo e mínimo`
+SELECT job_title AS 'Cargo', (max_salary - min_salary) AS `Diferença entre salários máximo e mínimo`
 FROM hr.jobs
-ORDER BY `Diferenca entre salários máximo e mínimo`;
+ORDER BY `Diferença entre salários máximo e mínimo`;
