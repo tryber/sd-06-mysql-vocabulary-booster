c0729792fa3d2045d943cc5541f7e188f00f4043
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-01-05 18:53:57 -0300

    Fixing...

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,10 @@
 SELECT
-    JOB_ID AS 'Cargo',
+    JOB_TITLE AS 'Cargo',
     CASE
-        WHEN SALARY > 5000 AND SALARY < 10000 THEN 'Baixo'
-        WHEN SALARY > 10001 AND SALARY < 20000 THEN 'Médio'
-        WHEN SALARY > 20001 AND SALARY < 30000 THEN 'Alto'
+        WHEN MAX_SALARY>= 5000 AND MAX_SALARY <= 10000 THEN 'Baixo'
+        WHEN MAX_SALARY >= 10001 AND MAX_SALARY <= 20000 THEN 'Médio'
+        WHEN MAX_SALARY >= 20001 AND MAX_SALARY <= 30000 THEN 'Alto'
         ELSE 'Altíssimo'
-    END AS 'Nível'
-FROM hr.employees
-ORDER BY 'Cargo' ASC;
+        END AS 'Nível'
+FROM hr.jobs
+ORDER BY Cargo ASC;
--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,13 @@
 SELECT j.JOB_TITLE AS 'Cargo',
         ROUND(AVG(SALARY), 2) AS 'Média salarial',
         CASE
-            WHEN AVG(SALARY) >= 2000 AND AVG(SALARY) <= 5800 THEN 'Júnior'
-            WHEN AVG(SALARY) >= 5801 AND AVG(SALARY) <= 7500 THEN 'Pleno'
-            WHEN AVG(SALARY) >= 7501 AND AVG(SALARY) <= 10500 THEN 'Sênior'
+            WHEN AVG(e.SALARY) >= 2000 AND AVG(e.SALARY) <= 5800 THEN 'Júnior'
+            WHEN AVG(e.SALARY) >= 5801 AND AVG(e.SALARY) <= 7500 THEN 'Pleno'
+            WHEN AVG(e.SALARY) >= 7501 AND AVG(e.SALARY) <= 10500 THEN 'Sênior'
             ELSE 'CEO'
-        END AS Senioridade
-FROM employees AS e
-        INNER JOIN hr.jobs AS j
+        END AS 'Senioridade'
+FROM jobs AS j
+        INNER JOIN employees AS e
                 ON e.JOB_ID = j.JOB_ID
 GROUP BY j.JOB_TITLE
-ORDER BY 'Média salarial' ASC, j.JOB_TITLE ASC;
+ORDER BY AVG(e.SALARY) ASC, j.JOB_TITLE ASC;
