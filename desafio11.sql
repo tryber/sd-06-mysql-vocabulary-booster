a344cc58b6b51601fe160c31fc57dbd4bd01ba6e
Luiz Mello <luizmacmillan@gmail.com>
2021-01-05 18:02:08 -0300

    Correção do Desafio 11.

--- desafio11.sql
+++ desafio11.sql
@@ -5,4 +5,5 @@ COUNT(p.Country) - 1 AS `Número de compatriotas`
 FROM w3schools.customers c
 JOIN w3schools.customers p ON c.Country = p.Country
 GROUP BY `Nome`
+HAVING `Número de compatriotas` <> 0
 ORDER BY `Nome`;
