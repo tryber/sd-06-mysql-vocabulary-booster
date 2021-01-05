76f371c46932d18514d685ea0d5a4e39cf394155
DehSartoreto <fixsartoreto@gmail.com>
2021-01-05 18:17:44 -0300

    desafio9

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(emp.FirstName,' ',emp.LastName) AS 'Nome completo',
-COUNT(ord.EmployeeID) AS 'Total de Pedidos'
+COUNT(ord.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS emp
 INNER JOIN w3schools.orders AS ord
 ON emp.EmployeeID = ord.EmployeeID
