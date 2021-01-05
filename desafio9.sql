fb2b2e4faf17682b09350750c3126aed4256be5f
DehSartoreto <fixsartoreto@gmail.com>
2021-01-05 18:10:55 -0300

    desafio9

--- desafio9.sql
+++ desafio9.sql
@@ -3,5 +3,5 @@ COUNT(ord.EmployeeID) AS 'Total de Pedidos'
 FROM w3schools.employees AS emp
 INNER JOIN w3schools.orders AS ord
 ON emp.EmployeeID = ord.EmployeeID
-GROUP BY emp.FirstName
+GROUP BY CONCAT(emp.FirstName,' ',emp.LastName)
 ORDER BY COUNT(ord.EmployeeID);
