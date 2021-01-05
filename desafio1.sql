8170223939ee62914b3743465ad1d2c4bf5d6fee
Cássio Rubens <cassiorubensadv@gmail.com>
2021-01-05 19:50:11 -0300

    FIX: Desafio 2

--- desafio1.sql
+++ desafio1.sql
@@ -4,7 +4,8 @@
 -- A primeira deve possuir o alias "País" e exibir o nome do país.
 -- A segunda deve possuir o alias "Status Inclusão" e deve exibir "incluído" se o país em questão está incluso em "Europe", ou "não incluído", caso contrário.
 -- Os resultados devem estar ordenados pelo nome do país em ordem alfabética.
--- SELECT * FROM hr.countries;
+SELECT * FROM hr.countries;
+
 SELECT COUNTRY_NAME AS `País`, IF(REGION_ID = 1, 'incluído', 'não incluído') AS `Status Inclusão`
 FROM hr.countries
 ORDER BY País;
