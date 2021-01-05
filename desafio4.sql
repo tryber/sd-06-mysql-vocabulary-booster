f313cd414d642a5c9c6f1e4441b3fc22066d2d3e
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-05 16:53:52 -0300

    Desafio 6

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,10 @@
-SELECT jobs.JOB_TITLE AS 'Cargo',
-ROUND(AVG(employees.SALARY),2) AS 'Média salarial',
-CASE
-WHEN AVG(employees.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
-WHEN AVG(employees.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
-WHEN AVG(employees.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
-WHEN AVG(employees.SALARY) > 10500 THEN 'CEO'
-END AS 'Senioridade' 
+SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS 'Nome completo',
+jobs.JOB_TITLE AS 'Cargo',
+employees.HIRE_DATE AS 'Data de início do cargo',
+departments.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.jobs
 INNER JOIN hr.employees
 ON jobs.JOB_ID = employees.JOB_ID
-GROUP BY JOB_TITLE
-ORDER BY AVG(employees.SALARY), JOB_TITLE;
+INNER JOIN hr.departments
+ON departments.DEPARTMENT_ID = employees.DEPARTMENT_ID
+ORDER BY CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) DESC, jobs.JOB_TITLE ASC;
