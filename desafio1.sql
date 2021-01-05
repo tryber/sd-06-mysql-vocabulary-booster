4307ba8088e98f8f0eb9c20ee267184ae007f276
Ana Higo <55517497+anahigo@users.noreply.github.com>
2021-01-05 15:28:21 -0300

    Update desafio1.sql

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
 SELECT COUNTRY_NAME AS 'País',
-IF (region_id = 1, 'incluído', 'não icluído') AS 'Status Inclusão'
+IF (REGION_ID = 1, 'incluído', 'não icluído') AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY COUNTRY_NAME;
