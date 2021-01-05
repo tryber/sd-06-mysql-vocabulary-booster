a166fd12d1651fa6dea83f4dafa1906ee79e1db4
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2021-01-05 16:18:53 -0300

    questionable tabbing rule fix

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,5 @@
-SELECT
-	c.COUNTRY_NAME AS 'País',
+SELECT c.COUNTRY_NAME AS 'País',
   IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluido') AS 'Status Inclusão'
-FROM
-	hr.countries AS c
+FROM hr.countries AS c
   LEFT JOIN hr.regions AS r
     ON c.REGION_ID = r.REGION_ID;
