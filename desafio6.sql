57509149e0affa624ba7c168c91253e354e46156
danieleperse <danieleperse3@gmail.com>
2021-01-05 19:27:40 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,15 @@
+SELECT
+CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME) AS 'Nome completo',
+t2.JOB_TITLE AS 'Cargo',
+t3.START_DATE AS 'Data de início do cargo',
+t4.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history AS t3
+INNER JOIN hr.jobs AS t2
+ON t3.JOB_ID = t2.JOB_ID
+INNER JOIN hr.employees AS t1
+ON t3.EMPLOYEE_ID = t1.EMPLOYEE_ID
+INNER JOIN hr.departments AS t4
+ON t3.DEPARTMENT_ID = t4.DEPARTMENT_ID
+ORDER BY
+CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME) DESC,
+t2.JOB_TITLE ASC;
