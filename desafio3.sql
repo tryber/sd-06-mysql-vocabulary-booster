36869e766c9605c970402c5224933b29a75fbac5
CristianoNunes <cristianonunes99@gmail.com>
2021-01-05 17:57:45 -0300

    Desafio 3 - Correcao

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
 SELECT j.JOB_TITLE as 'Cargo',
-(j.MAX_SALARY - j.MIN_SALARY) AS 'Diferença entre salários máximos e mínimo'
+(j.MAX_SALARY - j.MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs as j
 ORDER BY (j.MAX_SALARY - j.MIN_SALARY), Cargo;
