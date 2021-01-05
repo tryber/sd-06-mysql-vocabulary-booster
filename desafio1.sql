dc3acbbd260da268469daeb411241547058d62dd
Isa Koga <isadorakoga710@gmail.com>
2021-01-05 15:59:37 -0300

    :rotating-light: CC fix

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT
-	COUNTRY_NAME AS 'País',
-	CASE
-		WHEN REGION_ID=1 THEN 'Incluído'
-        ELSE 'Não incluído'
-        END AS 'Status inclusão'
-	FROM hr.countries
-    ORDER BY COUNTRY_NAME;
+COUNTRY_NAME AS 'País',
+CASE
+	WHEN REGION_ID=1 THEN 'Incluído'
+      ELSE 'Não incluído'
+      END AS 'Status inclusão'
+FROM hr.countries
+  ORDER BY COUNTRY_NAME;
