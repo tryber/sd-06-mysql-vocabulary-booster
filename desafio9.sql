33ea4ea13eea83ddf4bf93903910f9324d78fee3
Thales Rangel <thalesgr@gmail.com>
2021-01-05 18:27:42 -0300

    exe 9 correçao

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome Completo',
-COUNT(O.EmployeeID) AS 'Total de Pedidos'
+COUNT(O.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS E
 INNER JOIN w3schools.orders AS O
 ON O.EmployeeID = E.EmployeeID
