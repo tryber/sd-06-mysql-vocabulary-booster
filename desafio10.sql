6ea78f8e233ea372712ec044df60660aec230aea
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2021-01-05 18:14:17 -0300

    tabs

--- desafio10.sql
+++ desafio10.sql
@@ -3,10 +3,10 @@ MIN(od.Quantity) AS 'Mínima',
 MAX(od.Quantity) AS 'Máxima',
 ROUND(AVG(od.Quantity), 2) AS 'Média'
 FROM w3schools.orders AS o
-	LEFT JOIN w3schools.order_details AS od
-    ON o.OrderID = od.OrderID
-  LEFT JOIN w3schools.products AS p
-    ON p.ProductID = od.ProductID
+LEFT JOIN w3schools.order_details AS od
+  ON o.OrderID = od.OrderID
+LEFT JOIN w3schools.products AS p
+  ON p.ProductID = od.ProductID
 GROUP BY p.ProductName
 HAVING ROUND(AVG(od.Quantity), 2) > 20.00
 ORDER BY AVG(od.Quantity) ASC, p.ProductName ASC;
--- desafio16.sql
+++ desafio16.sql
@@ -5,17 +5,17 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(employee_email VARCHAR(255))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE job_count INT;
+DECLARE job_count INT;
 
-  (
-	  SELECT COUNT(*)
-	  FROM hr.job_history AS jh
-		  LEFT JOIN hr.employees AS e
-		  ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-	  WHERE e.EMAIL = employee_email
-	) INTO job_count;
+(
+  SELECT COUNT(*)
+  FROM hr.job_history AS jh
+    LEFT JOIN hr.employees AS e
+    ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+  WHERE e.EMAIL = employee_email
+) INTO job_count;
 
-    RETURN job_count;
+  RETURN job_count;
 END $$
 
 DELIMITER ;
