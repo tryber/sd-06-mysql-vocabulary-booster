368661aadf26782ee16490aa6bdcb074647d88fc
Dilenio Enderle <dilenio@gmail.com>
2021-01-05 16:15:39 -0300

    feat: challenge 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,3 @@
-USE hr;
-
-SELECT COUNTRY_NAME as 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão'
+SELECT COUNTRY_NAME AS 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY COUNTRY_NAME ASC;
