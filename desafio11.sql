cfede7fb07c2b0ff6e9b8db82be3f4349ea4f36a
DehSartoreto <fixsartoreto@gmail.com>
2021-01-05 19:22:58 -0300

    desafio11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT c1.ContactName AS 'Nome',
+c1.Country AS 'País',
+(COUNT(*) - 1) AS 'Número de compatriotas'
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country
+GROUP BY Nome, País
+ORDER BY Nome;
