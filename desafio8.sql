fbc87dad3084b05e4a49efbede965ca75716f21c
isabellavjs <isabellavjs@gmail.com>
2021-01-05 18:28:39 -0300

    Add chellenge 8

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT CU.ContactName AS `Nome de contato`,
+S.ShipperName AS `Empresa que fez o envio`,
+O.OrderDate AS `Data do pedido`
+FROM orders AS O
+INNER JOIN customers AS CU ON CU.CustomerID = O.CustomerID
+INNER JOIN shippers AS S ON S.ShipperID = O.ShipperID
+ORDER BY `Nome de contato`, S.ShipperName, O.OrderDate;
