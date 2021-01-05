54c6fa0012ea2d8135671886d08896b31b057531
HCaetano <hrcgeo@gmail.com>
2021-01-05 15:34:55 -0300

    Fix challenge 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,4 @@
-SELECT * FROM hr.countries
-WHERE region_id <> 1;
+SELECT country_name AS 'País', CASE
+		WHEN region_id = 1 THEN 'incluído' ELSE 'não incluído'
+        END AS 'Status Inclusão'
+FROM hr.countries;
