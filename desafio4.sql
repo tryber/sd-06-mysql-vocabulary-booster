6d76ae46fef3fef999976b50b9f329b3ed584335
LeandroParisi <lepcbelisario@gmail.com>
2021-01-05 17:23:23 -0300

    Ex.4.2

--- desafio4.sql
+++ desafio4.sql
@@ -3,7 +3,7 @@ CASE
 WHEN ROUND(AVG(e.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(e.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
 WHEN ROUND(AVG(e.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'Júnior'
+WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'CEO'
 END AS 'Senioridade'
 FROM hr.jobs AS j
 JOIN hr.employees AS e ON j.job_id = e.job_id
