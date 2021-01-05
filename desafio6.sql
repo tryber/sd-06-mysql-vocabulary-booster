af6f981c380eef1116a7834f10857e0b0cf8e57f
DehSartoreto <fixsartoreto@gmail.com>
2021-01-05 16:36:19 -0300

    desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(emp.FIRST_NAME,' ', emp.LAST_NAME) AS 'Nome completo',
-	job.JOB_TITLE AS 'Cargo',
-    emp.HIRE_DATE AS 'Data de início do cargo',
-    dep.DEPARTMENT_NAME AS 'Departamento'
+job.JOB_TITLE AS 'Cargo',
+emp.HIRE_DATE AS 'Data de início do cargo',
+dep.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.jobs as job
 INNER JOIN hr.employees AS emp
 ON job.JOB_ID = emp.JOB_ID
