cce3eb1769fb9c9fc30c6fee21671f5bc6957af7
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-01-05 19:57:20 -0300

    serio? o erro foi um C

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS 'Nome Completo',
+SELECT CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS 'Nome completo',
 jo.JOB_TITLE AS Cargo,
 hi.START_DATE AS 'Data de início do cargo',
 de.DEPARTMENT_NAME AS Departamento
@@ -9,4 +9,4 @@ INNER JOIN hr.jobs AS jo
 ON hi.JOB_ID = jo.JOB_ID
 INNER JOIN hr.departments AS de
 ON de.DEPARTMENT_ID = hi.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC, Cargo;
+ORDER BY `Nome completo` DESC, Cargo;
