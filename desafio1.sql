e56c40dbf1cdc4bdd6fc4433ca44654209d62a1c
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-01-05 16:54:41 -0300

    Subindo desafio 3

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
 SELECT
 COUNTRY_NAME AS "País",
-IF(COUNTRY_NAME = 1, "incluído", "não incluído") AS "Status Inclusão"
-FROM hr.countries;
+IF(REGION_ID = 1, "incluído", "não incluído") AS "Status Inclusão"
+FROM hr.countries
+ORDER BY COUNTRY_NAME;
