84cffc9123d8c5189cdc8c21df3dd5998e1556bd
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-05 19:04:07 -0300

    :bug: Corrigindo Desafio 05

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT
 jobs.JOB_TITLE AS 'Cargo',
 (jobs.MAX_SALARY - jobs.MIN_SALARY) AS 'Variação Salarial',
-ROUND((jobs.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
-ROUND((jobs.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
+ROUND(jobs.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+ROUND(jobs.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM hr.employees AS emp
 JOIN hr.jobs AS jobs
 WHERE emp.JOB_ID = jobs.JOB_ID
