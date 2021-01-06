8a9918a7792ecf87fcb16c0b9f80793959e33e3d
Paulo <paulocheloni@gmail.com>
2021-01-06 12:19:29 -0300

    adding ex6

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+SELECT
+  JOB_TITLE as "Cargo",
+ (MAX_SALARY - MIN_SALARY) as "Variação Salarial",
+  ROUND((MIN_SALARY / 12), 2) as "Média mínima mensal",
+  ROUND((MAX_SALARY / 12), 2) as "Média máxima mensal"
+FROM
+  hr.jobs
+ORDER BY
+  (MAX_SALARY - MIN_SALARY),
+  JOB_TITLE
