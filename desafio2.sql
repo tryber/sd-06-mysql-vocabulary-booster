396b67d004ee3c657952645ef87dadeda997ae52
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-06 17:11:23 -0300

    desafios 1 e 2 com ORDER BY

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 SELECT JOB_TITLE as Cargo,
 CASE
-WHEN max_salary > 5000 AND max_salary <= 10000 THEN 'Baixo'
+WHEN max_salary <= 10000 THEN 'Baixo'
 WHEN max_salary > 10000 AND max_salary <= 20000 THEN 'Médio'
 WHEN max_salary > 20000 AND max_salary <= 30000 THEN 'Baixo'
 WHEN max_salary > 30000 THEN 'Altíssimo'
