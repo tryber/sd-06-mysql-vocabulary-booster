38359931fb658f0d32fc87f9937610dfa70c01d0
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-05 21:56:36 -0300

    :bug: Desafio 06 (Primeira Correcao)

--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,11 @@ SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
 jobs.JOB_TITLE AS 'Cargo',
 emp.HIRE_DATE AS 'Data de início do cargo',
 deps.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS emp
+FROM hr.job_history AS hist
+JOIN hr.employees AS emp
+ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
 JOIN hr.jobs AS jobs
-ON emp.JOB_ID = jobs.JOB_ID
+ON hist.JOB_ID = jobs.JOB_ID
 JOIN hr.departments AS deps
 ON emp.DEPARTMENT_ID = deps.DEPARTMENT_ID
-JOIN hr.job_history AS hist
-ON emp.EMPLOYEE_ID = hist.EMPLOYEE_ID
-ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, deps.DEPARTMENT_NAME;
+ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, jobs.JOB_TITLE;
