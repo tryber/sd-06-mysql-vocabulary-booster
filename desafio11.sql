e10f7a1531164e9cd7cedbdef10b5d0dba2cd976
HCaetano <hrcgeo@gmail.com>
2021-01-06 18:02:14 -0300

    Fix challenge 8, filter rows where countrymen > 1

--- desafio11.sql
+++ desafio11.sql
@@ -5,4 +5,5 @@ COUNT(c1.ContactName) - 1 as `Número de compatriotas`
 FROM customers AS c1
 INNER JOIN customers AS c2 ON c1.Country = c2.Country
 GROUP BY c1.CustomerID
+HAVING `Número de compatriotas` > 0
 ORDER BY Nome;
