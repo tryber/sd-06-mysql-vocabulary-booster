4f5a09a7ff916b029dff9b4a44d9617ac6e479f4
kissyla <kissylamottapereira@gmail.com>
2021-01-06 20:20:27 -0300

    desafio 5 corrigido de novo, e desafio 6

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT JOB_TITLE AS 'Cargo',
 MAX_SALARY - MIN_SALARY AS 'Variação salarial',
-MIN_SALARY AS 'Média mínima mensal',
-MAX_SALARY AS 'Média máxima mensal'
+round((MIN_SALARY/12), 2) AS 'Média mínima mensal',
+round((MAX_SALARY/12), 2) AS 'Média máxima mensal'
 FROM hr.jobs
 ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,10 @@
-SELECT JOB_TITLE AS 'Cargo',
-MAX_SALARY - MIN_SALARY AS 'Variação salarial',
-round((MIN_SALARY/12), 2) AS 'Média mínima mensal',
-round((MAX_SALARY/12), 2) AS 'Média máxima mensal'
-FROM hr.jobs
-ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
+SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
+jobs.JOB_TITLE AS 'Cargo',
+emp.hire_date AS 'Data de início do cargo',
+dep.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees AS emp
+INNER JOIN hr.jobs
+ON jobs.JOB_ID = emp.JOB_ID
+INNER JOIN hr.departments AS dep
+ON emp.DEPARTMENT_ID = dep.DEPARTMENT_ID
+ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, jobs.JOB_TITLE ASC;
