8d63f4bfd44e619a48ec307724ef9a7850e89527
ilanribeiro <ilanribeiro@gmail.com>
2021-01-06 20:05:52 -0300

    change: requirement 7 - add start date condition

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,10 @@
 SELECT UPPER(CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME)) AS "Nome completo",
-START_DATE AS "Data de início",
+job_history.START_DATE AS "Data de início",
 employees.SALARY AS "Salário"
 FROM hr.job_history AS job_history
 INNER JOIN hr.employees AS employees
 ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+WHERE MONTH (job_history.START_DATE) BETWEEN "01" AND "03"
 ORDER BY
 UPPER(CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME)),
 START_DATE;
