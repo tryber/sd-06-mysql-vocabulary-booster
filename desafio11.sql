f2992cc2433a8c5ea10c16f4c21a38790a17d22f
flaviannymaciel <flaviannymaciel@gmail.com>
2021-01-06 17:04:06 -0300

    desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,7 @@
--- SELECT ContactName AS "Nome", Country AS "País",
--- (COUNT(*) -1) AS "Número de compatriotas" 
--- FROM w3schools.customers
--- GROUP BY ContactName, Country
--- HAVING COUNT(*) > 1
--- ORDER BY ContactName;
+SELECT c1.ContactName AS "Nome", c1.Country AS "País",
+COUNT(c2.Country) AS "Número de compatriotas" 
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country
+AND c1.ContactName <> c2.ContactName
+GROUP BY c1.CustomerID
+ORDER BY c1.ContactName;
