1516c04b8bc09cb4786d77213afcf1d23984a0c9
rafaelgelatti <rafael.gelatti@gmail.com>
2021-01-06 16:54:11 -0300

    req7 ooops

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(UCASE(e.FIRST_NAME), ' ',  UCASE(e.LAST_NAME)),
+SELECT CONCAT(UCASE(e.FIRST_NAME), ' ',  UCASE(e.LAST_NAME)) AS 'Nome completo',
 jh.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM hr.employees AS e, hr.job_history AS jh
