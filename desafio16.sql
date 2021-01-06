fd4798e0d3953dd82e5c7d87844109c3825feb0c
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-01-06 16:03:11 -0300

    challanges 16 correction

--- desafio16.sql
+++ desafio16.sql
@@ -1,10 +1,16 @@
 USE hr;
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN EmplEmail varchar(100))
+DELIMITER $$
 
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EmplEmail varchar(100))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE num_jobs INT;
 SELECT COUNT(EMAIL)
 FROM hr.employees
 INNER JOIN hr.job_history
 ON hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
-WHERE EMAIL = EmplEmail;
+WHERE EMAIL = EmplEmail INTO num_jobs;
+RETURN num_jobs;
+END $$
 
-CALL buscar_quantidade_de_empregos_por_funcionario("NKOCHHAR");
+SELECT buscar_quantidade_de_empregos_por_funcionario("NKOCHHAR");
