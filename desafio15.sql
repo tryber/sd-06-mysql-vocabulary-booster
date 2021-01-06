7342eaf29bc68bc1c903af371eedd67de5cfec06
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-06 18:47:33 -0300

    Trying to solve CC

--- desafio15.sql
+++ desafio15.sql
@@ -3,12 +3,12 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(200))
 BEGIN
-	SELECT ROUND(AVG(emp.SALARY), 2) AS 'Média salarial'
-    FROM hr.employees AS emp
-    INNER JOIN hr.jobs AS jo
-    ON emp.JOB_ID = jo.JOB_ID
-    WHERE jo.JOB_TITLE LIKE cargo
-    GROUP BY jo.JOB_ID;
+SELECT ROUND(AVG(emp.SALARY), 2) AS 'Média salarial'
+  FROM hr.employees AS emp
+  INNER JOIN hr.jobs AS jo
+  ON emp.JOB_ID = jo.JOB_ID
+  WHERE jo.JOB_TITLE LIKE cargo
+  GROUP BY jo.JOB_ID;
 END $$
 
 DELIMITER ;
