1f1ac506d8ed7f1b6f7e5488946b8e8500202646
CristianoNunes <cristianonunes99@gmail.com>
2021-01-06 19:31:11 -0300

    Desafio 17 - Correcao

--- desafio17.sql
+++ desafio17.sql
@@ -3,12 +3,11 @@ DROP TRIGGER IF EXISTS trigger_orders_insert;
 
 DELIMITER $$
 CREATE TRIGGER trigger_orders_insert
-	BEFORE INSERT ON orders
-    FOR EACH ROW
+BEFORE INSERT ON orders
+FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END; $$
 DELIMITER ;
-
 INSERT INTO orders(CustomerID, EmployeeID, ShipperID) VALUES(4, 2, 2);
 SELECT * FROM orders WHERE CustomerID = 4;
