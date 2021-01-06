f973240036075a886496913c45bc4d2aec64e2c8
Ana Higo <anahigo.arq@gmail.com>
2021-01-06 17:03:24 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -5,4 +5,8 @@ FROM w3schools.customers AS Cb
 WHERE Cb.Country = Ca.Country
 ) - 1 AS `Número de compatriotas`
 FROM w3schools.customers AS Ca
+WHERE (SELECT COUNT(Cb.Country)
+FROM w3schools.customers AS Cb
+WHERE Cb.Country = Ca.Country
+) - 1 > 0
 ORDER BY `Nome`;
