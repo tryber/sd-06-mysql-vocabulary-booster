c2b65916d0d8728995fb97c343f25e86dd398f03
rafMewtwo <ponciano.raf@gmail.com>
2021-01-06 14:52:01 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT * FROM w3schools.orders;
+SELECT * FROM w3schools.customers;
+SELECT * FROM w3schools.shippers;
+
+SELECT c.ContactName as "Nome de contato",
+s.ShipperName as "Empresa que fez o envio",
+o.OrderDate as "Data do pedido"
+FROM w3schools.customers AS c
+INNER JOIN w3schools.orders AS o
+ON c.CustomerID = o.CustomerID
+INNER JOIN w3schools.shippers AS s
+ON o.ShipperID = s.ShipperID
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
