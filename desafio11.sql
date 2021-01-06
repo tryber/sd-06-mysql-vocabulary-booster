4c54b7a37386ea03edfba03a278dc38f58165535
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-01-06 16:59:32 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT a.ContactName AS Nome,
+a.Country AS País,
+COUNT(a.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS a, w3schools.customers AS b
+WHERE a.Country = b.Country
+GROUP BY a.CustomerID
+HAVING `Número de compatriotas` > 0
+ORDER BY Nome;
