7e9de2fe7186fafae1d78a504f680bbe5dc6fbd0
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-06 19:00:21 -0300

    mudando comandos para uppercase

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
-select
-customers.ContactName as 'Nome de contato',
-shippers.ShipperName as 'Empresa que fez o envio',
-orders.OrderDate as 'Data do pedido'
-from w3schools.orders
-inner join customers on customers.CustomerID = orders.CustomerID
-inner join shippers on shippers.ShipperID = orders.ShipperID
-where shippers.ShipperName = 'Speedy Express' or shippers.ShipperName = 'SUnited Package'
-order by customers.ContactName asc, shippers.ShipperName asc, orders.OrderDate asc;
+SELECT
+customers.ContactName AS 'Nome de contato',
+shippers.ShipperName AS 'Empresa que fez o envio',
+orders.orderDate AS 'Data do pedido'
+FROM w3schools.orders
+INNER JOIN customers ON customers.CustomerID = orders.CustomerID
+INNER JOIN shippers ON shippers.ShipperID = orders.ShipperID
+WHERE shippers.ShipperName = 'Speedy Express' OR shippers.ShipperName = 'United Package'
+ORDER BY customers.ContactName ASC, shippers.ShipperName ASC, orders.orderDate ASC;
