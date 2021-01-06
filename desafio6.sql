49e501867069b4158e16da7c6b2e07f8ef2aaed1
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-06 12:22:27 -0300

    madando tabela para job_history

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,11 @@
 SELECT
 CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) AS 'Nome completo',
 jobs.JOB_TITLE AS 'Cargo',
-employees.HIRE_DATE AS 'Data de início do cargo',
+job_history.START_DATE AS 'Data de início do cargo',
 departments.DEPARTMENT_NAME AS 'Departamento'
-FROM ((hr.employees
+FROM (((hr.employees
 INNER JOIN jobs ON employees.JOB_ID = jobs.JOB_ID)
 INNER JOIN departments ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID)
+inner join job_history ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID)
 ORDER BY concat(employees.FIRST_NAME, " ", employees.LAST_NAME) DESC,
 jobs.JOB_TITLE ASC;
