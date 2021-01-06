59fe07ceb8c84df97cc554baa2617647c563f347
CristianoNunes <cristianonunes99@gmail.com>
2021-01-06 18:54:01 -0300

    Desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,14 @@
+USE hr;
+
+DELIMITER $$
+CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(
+IN email VARCHAR(25))
+BEGIN
+SELECT COUNT(*) AS total_empregos
+FROM hr.job_history AS jh
+INNER JOIN hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE e.EMAIL = email;
+END $$
+DELIMITER ;
+
+CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
