6231bdecf04b2bf78349caf44abfe92378e31abc
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-06 16:44:50 -0300

    fix: 5th requirement avg decimal place

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT j.JOB_TITLE Cargo,
   j.MAX_SALARY - j.MIN_SALARY 'Variação Salarial',
-  AVG(j.MIN_SALARY)/12 'Média mínima mensal',
-  AVG(j.MAX_SALARY)/12 'Média máxima mensal'
+  ROUND(AVG(j.MIN_SALARY)/12, 2) 'Média mínima mensal',
+  ROUND(AVG(j.MAX_SALARY)/12, 2) 'Média máxima mensal'
 FROM hr.jobs j
 GROUP BY j.JOB_ID
 ORDER BY j.MAX_SALARY - j.MIN_SALARY ASC, j.JOB_TITLE;
