43e59faa5be0785610fa4ee0f0d9afa17a1e9c71
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-06 15:34:27 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
 SELECT  
   c1.ContactName AS `Nome`,
   c1.Country AS `País`,
-  (SELECT COUNT(c2.Country)
+  (SELECT COUNT(c2.Country)-1
     FROM w3schools.customers AS c2
-    WHERE c1.Country = c2.Country)-1
+    WHERE c1.Country = c2.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS c1
 GROUP BY `Nome`, `País`
 ORDER BY `Nome`;
