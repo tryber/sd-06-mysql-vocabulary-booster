9243a588d68686c8bc587aefcadbf36eac70cec6
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-06 16:13:49 -0300

    Chal.7 fix

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,6 @@
 SELECT UPPER(CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME)) AS 'Nome completo',
   hist.START_DATE AS 'Data de início',
-  emp.SALARY AS 'Salário',
-  MONTH(hist.START_DATE) AS MES
+  emp.SALARY AS 'Salário'
 FROM hr.job_history AS hist
 INNER JOIN hr.employees AS emp
   ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
