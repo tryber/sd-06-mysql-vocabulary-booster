802e25b1819a454f7d181faa194c9cbbb096dd00
CristianoNunes <cristianonunes99@gmail.com>
2021-01-06 18:01:53 -0300

    Desafio 14

---
+++ desafio14.sql
@@ -0,0 +1,9 @@
+(SELECT
+DISTINCT (c.Country) AS `País`
+FROM w3schools.customers AS c)
+UNION ALL
+(SELECT
+DISTINCT (s.Country) AS `País`
+FROM w3schools.suppliers AS s)
+ORDER BY `País`
+LIMIT 5;
