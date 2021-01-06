c467cc0fa797ff1644103d6c5a694da15bfc6e0d
William Gomes <williamjog@hotmail.com>
2021-01-06 14:48:29 -0300

    11th challenge

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,1 @@
-SELECT c1.ContactName AS 'Nome', c1.Country AS 'País', COUNT(c2.Country) - 1 AS 'Número de compatriotas' FROM w3schools.customers c1, w3schools.customers c2 WHERE c1.Country = c2.Country GROUP BY c1.CustomerID ORDER BY c1.ContactName;
+SELECT c1.ContactName AS 'Nome', c1.Country AS 'País', COUNT(c2.Country) - 1 AS 'Número de compatriotas' FROM w3schools.customers c1, w3schools.customers c2 WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName GROUP BY c1.CustomerID ORDER BY c1.ContactName;
