6f3504f1669ff28fe0fff8bed449623f8e8d0ed7
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-01-06 16:47:54 -0300

    Adding sixth requirement

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,13 @@
-SELECT 
-CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome Completo`,
+SELECT
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome Completo`,
 j.JOB_TITLE AS Cargo,
 e.HIRE_DATE AS 'Data de início do cargo',
-d.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS e
+d.DEPARTMENT_NAME AS Departamento
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j
-ON e.JOB_ID = j.JOB_ID
+ON j.JOB_ID = jh.JOB_ID
 INNER JOIN hr.departments AS d
-ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC, d.DEPARTMENT_NAME ASC;
+ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome Completo` DESC, Cargo;
