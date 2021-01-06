a827706437c1ee6398ccf94eaa9e763c50880c23
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-06 17:50:52 -0300

    :bug: corrigindo desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS `Nome completo`,
 jb.JOB_TITLE AS Cargo,
-emp.HIRE_DATE AS `Data de início do cargo`,
+jh.START_DATE AS `Data de início do cargo`,
 dp.DEPARTMENT_NAME AS `Departamento`
-FROM hr.employees AS emp, hr.jobs AS jb, hr.departments AS dp
-WHERE emp.JOB_ID=jb.JOB_ID AND emp.DEPARTMENT_ID=dp.DEPARTMENT_ID
+FROM hr.employees AS emp, hr.jobs AS jb, hr.job_history AS jh, hr.departments AS dp
+WHERE emp.EMPLOYEE_ID=jh.EMPLOYEE_ID AND jb.JOB_ID=jh.JOB_ID AND dp.DEPARTMENT_ID=jh.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo;
