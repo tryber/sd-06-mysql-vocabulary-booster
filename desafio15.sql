f56973c160e5484956944cbc8010133c5a102c64
CristianoNunes <cristianonunes99@gmail.com>
2021-01-06 18:59:56 -0300

    Desafio 15 - Correcao 2

--- desafio15.sql
+++ desafio15.sql
@@ -4,11 +4,11 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_cargo(
 IN job_name VARCHAR(35))
 BEGIN
-SELECT ROUND(AVG(e.SALARY, 2)) AS `Média salarial`
+SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
 WHERE j.JOB_TITLE = job_name;
 END $$
 DELIMITER ;
 
-CALL buscar_media_cargo('Programmer', @avg_salary);
+CALL buscar_media_cargo('Programmer');
