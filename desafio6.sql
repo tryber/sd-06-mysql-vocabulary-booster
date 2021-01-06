fd6b10d7624dec53c6b70f8a84ad91792e398a39
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-01-06 08:20:35 -0300

    challanges 6 correction

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ JOB_TITLE AS "Cargo",
 START_DATE AS "Data de início do cargo",
 DEPARTMENT_NAME AS "Departamento"
 FROM hr.employees, hr.jobs, hr.job_history, hr.departments
-WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID
+WHERE hr.job_history.JOB_ID = hr.jobs.JOB_ID
 AND hr.employees.EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID
 AND hr.departments.DEPARTMENT_ID = hr.job_history.DEPARTMENT_ID
 ORDER BY `Nome Completo` DESC, Cargo;
