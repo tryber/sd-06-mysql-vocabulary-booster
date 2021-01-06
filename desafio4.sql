dee488220406a14beb4401e6f5d4bd474d46cefd
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-06 00:41:38 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+SELECT
+    a.job_title AS 'Cargo',
+	ROUND(AVG(b.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN b.SALARY <= 5800  THEN 'Júnior'
+        WHEN b.SALARY <= 7500 THEN 'Pleno'
+        WHEN b.SALARY <= 10500  THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM hr.employees AS b
+JOIN hr.jobs AS a
+ON b.job_id = a.job_id
+GROUP BY Cargo
+ORDER BY ROUND(AVG(b.SALARY), 2), Cargo;
