54e84e3b577afc9c13202993d8f8c0835b73af36
Tiago Berwanger <berwangertiago@gmail.com>
2021-01-06 11:20:26 -0300

    desafio 12

---
+++ desafio15.sql
@@ -0,0 +1,11 @@
+USE hr.jobs; -- obrigatório para criar a procedure no banco correto
+DELIMITER $$ -- definindo delimitador
+CREATE PROCEDURE buscar_media_por_cargo(@JOB_TITLE) -- parâmetros
+BEGIN -- delimitando o início do código SQL
+
+JOB_TITLE AS 'Cargo',
+ROUND(AVG(SALARY), 2) AS 'Média salarial',
+
+END $$ -- delimitando o final do código SQL
+DELIMITER ; -- muda o delimitador de volta para ; - o espaço entre
+DELIMITER e o ';' é necessário
---
+++ desafio17.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER InsertDate
+AFTER INSERT ON w3schools.orders
+FOR EACH ROW
+BEGIN
+
+NOW(OrderDate)
+
+END;
+DELIMITER $$ ;
