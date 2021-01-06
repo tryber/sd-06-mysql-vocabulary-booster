199c2671f4d94e0ce998c2aa0f91eae8d3c3858e
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-06 18:24:25 -0300

    :bug: corrigindo nome da coluna

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(UCASE(emp.FIRST_NAME), ' ', UCASE(emp.LAST_NAME)) AS `Nome completo`,
-jh.START_DATE AS `Data de início do cargo`,
+jh.START_DATE AS `Data de início`,
 emp.SALARY AS `Salário`
 FROM hr.employees AS emp, hr.job_history AS jh
 WHERE emp.EMPLOYEE_ID=jh.EMPLOYEE_ID AND MONTH(jh.START_DATE) IN (1,2,3)
