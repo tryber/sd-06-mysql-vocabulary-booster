2babb7df62cb4bc8d3e0a2433cfc40f197b4df77
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-06 16:29:36 -0300

    criando desafio4

---
+++ desafio4.sql
@@ -0,0 +1,6 @@
+USE hr;
+
+SELECT JOB_TITLE as Cargo,
+(MAX_SALARY - MIN_SALARY) AS "Diferença entre salários máximo e mínimo"
+FROM jobs
+ORDER BY "Diferença entre salários máximo e mínimo", Cargo;
