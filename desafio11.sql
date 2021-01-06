1b571b255f59abca029ee8a0da3f009613493f3f
ricardorosa-dev <ricardorosa@gmail.com>
2021-01-06 20:44:41 -0300

    Resolvendo problemas no desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,11 @@
 SELECT DISTINCT c.ContactName AS Nome,
 c.Country AS "País",
-CountryNumbers.compatriotas -1 AS "Número de compatriotas"
+CountryNumbers.compatriotas AS "Número de compatriotas"
 FROM (
 SELECT Country, COUNT(Country) AS compatriotas
 FROM w3schools.customers
 GROUP BY Country
 ) as CountryNumbers
 INNER JOIN w3schools.customers AS c ON CountryNumbers.Country = c.Country
+WHERE CountryNumbers.compatriotas > 0
 ORDER BY Nome
