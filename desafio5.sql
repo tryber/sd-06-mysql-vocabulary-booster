6ceb952ca00706e5208b0beec22b4b8ae1b7e64b
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-06 20:44:08 -0300

    desafio 4

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,4 @@
-SELECT
-job_title AS 'Cargo',
+SELECT job_title AS 'Cargo',
 (MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
 ROUND((MIN_SALARY / 12), 2) AS 'Média mínima mensal',
 ROUND((MAX_SALARY / 12), 2) AS 'Média máxima mensal'
