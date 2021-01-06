565be523dac9748a810e9badc0958ca2c1d05a23
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-06 17:02:27 -0300

    Corrected type-o 'compatrioras'

--- desafio11.sql
+++ desafio11.sql
@@ -5,7 +5,7 @@ Country AS 'País',
 SELECT Count(*) - 1
 FROM w3schools.customers 
 WHERE Country = c.Country
-) AS 'Número de compatrioras' 
+) AS 'Número de compatriotas' 
 FROM w3schools.customers AS c
 WHERE (
 SELECT Count(*) - 1
