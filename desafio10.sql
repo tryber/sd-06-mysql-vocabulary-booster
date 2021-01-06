cd0172d612c6485f67a7eeaa077b7f08c4537428
HCaetano <hrcgeo@gmail.com>
2021-01-06 16:05:27 -0300

    Add 'having' to challenge 10, add remaining, empty files

--- desafio10.sql
+++ desafio10.sql
@@ -6,4 +6,5 @@ ROUND(AVG(Quantity), 2) AS Média
 FROM order_details
 INNER JOIN products ON order_details.ProductID = products.ProductID
 GROUP BY ProductName
+HAVING Média > 20.00
 ORDER BY Média, Produto;
