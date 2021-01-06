361e0cf977082766314846b59e3fdd13632ff30f
William Gomes <williamjog@hotmail.com>
2021-01-06 05:11:22 -0300

    9th challenge

---
+++ desafio9.sql
@@ -0,0 +1,1 @@
+SELECT CONCAT(emp.FirstName, ' ', emp.LastName) AS 'Nome completo', COUNT(o.OrderID) AS 'Total de pedidos' FROM w3schools.employees AS emp INNER JOIN w3schools.orders AS o ON emp.EmployeeID = o.EmployeeID GROUP BY `Nome completo` ORDER BY `Total de pedidos`;
