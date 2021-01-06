7f6efbfb594262328e2c37786d8baaa088bb90a8
Leandro <leandro.feitoza8018@gmail.com>
2021-01-06 15:26:58 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,25 @@
+SELECT 
+    JOB_TITLE AS 'Cargo', ROUND(AVG(SALARY)) AS 'Média salarial',
+    CASE
+        WHEN
+            ROUND(AVG(SALARY), 2) > 2000
+                AND ROUND(AVG(SALARY), 2) <= 5800
+        THEN
+            'Júnior'
+        WHEN
+            ROUND(AVG(SALARY), 2) > 5800
+                AND ROUND(AVG(SALARY), 2) <= 7500
+        THEN
+            'Pleno'
+        WHEN
+            ROUND(AVG(SALARY), 2) > 7500
+                AND ROUND(AVG(SALARY), 2) <= 10500
+        THEN
+            'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM
+    hr.jobs
+        INNER JOIN
+    hr.employees ON jobs.JOB_ID = employees.JOB_ID
+GROUP BY JOB_TITLE;
