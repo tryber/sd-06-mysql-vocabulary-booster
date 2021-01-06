73bd7c271cb93fa21a4adccf4c2d08e1a75bb542
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-01-06 14:53:53 -0300

    Adding sixth requirement

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+SELECT 
+CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome Completo`,
+j.JOB_TITLE AS Cargo,
+e.HIRE_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments AS d
+ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome Completo` DESC, d.DEPARTMENT_NAME ASC;
