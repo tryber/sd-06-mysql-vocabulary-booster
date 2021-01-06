1bb65a687e2b0ba953a6a2f78cdc6657b4ade939
CristianoNunes <cristianonunes99@gmail.com>
2021-01-06 16:27:29 -0300

    Desafio 10 - cORRECAO

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT
 p.ProductName AS `Produto`,
 MIN(od.Quantity) AS `Mínima`,
-MIN(od.Quantity) AS `Máxima`,
+MAX(od.Quantity) AS `Máxima`,
 ROUND(AVG(od.Quantity), 2) AS `Média`
 FROM w3schools.order_details AS od
 INNER JOIN w3schools.products AS p ON od.ProductID = p.ProductID
