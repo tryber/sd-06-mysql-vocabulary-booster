e8a1fc383d41b4900aac9fa6d44b55edaea59e0d
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-06 11:33:46 -0300

    Desafio 9

--- desafio8.sql
+++ desafio8.sql
@@ -4,6 +4,6 @@ SELECT
   o.OrderDate AS `Data do pedido`
 FROM w3schools.orders AS o
 INNER JOIN w3schools.customers AS c ON o.CustomerID = c.CustomerID
-INNER Join w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
 WHERE s.ShipperName IN('Speedy Express', 'United Package')
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
