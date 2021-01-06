9a24eecd23411b62eaaec30c83dcec7c06892271
Júlia Pôrto <juportob@gmail.com>
2021-01-06 15:23:24 -0300

    Desafio6 - fix code

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ J.JOB_TITLE AS Cargo,
 JH.START_DATE AS 'Data de início do cargo',
 D.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS E
-JOIN hr.jobs AS J ON J.JOB_ID = E.JOB_ID
+JOIN hr.jobs AS J ON J.JOB_ID = JH.JOB_ID
 JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 JOIN hr.departments AS D ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo;
