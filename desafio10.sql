89e4c769775fb0eca77e8ba73fab0c09a537547c
Renan Cunha <renan2610@gmail.com>
2021-01-06 18:28:07 -0300

    Fixing challenge 10 - The return

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,10 @@
-select
-p.ProductName as `Produto`,
-min(od.Quantity) as `Mínima`,
-max(od.Quantity) as `Máxima`,
-round(avg(od.Quantity), 2) as `Média`
-from w3schools.order_details as od
-inner join w3schools.products as p on od.ProductID = p.ProductID
-where od.Quantity > 20
-group by `Produto`
-order by `Média`, `Produto`;
+SELECT
+p.ProductName AS `Produto`,
+MIN(od.Quantity) AS `Mínima`,
+MAX(od.Quantity) AS `Máxima`,
+ROUND(AVG(od.Quantity), 2) AS `Média`
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p ON od.ProductID = p.ProductID
+GROUP BY `Produto`
+HAVING AVG(od.Quantity) > 20
+ORDER BY `Média`, `Produto`;
