ee0af95eda4283ddff95f678844609be833083be
Paulo <paulocheloni@gmail.com>
2021-01-06 15:37:59 -0300

    fix ex6

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,13 @@
-SELECT 
-  UPPER(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) as "Nome completo", 
-  job_history.START_DATE as "Data de início" ,
-  employees.SALARY as "Salário"
+SELECT
+  CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) as 'Nome completo',
+  JOB_TITLE as 'Cargo',
+  job_history.START_DATE as 'Data de início do cargo',
+  DEPARTMENT_NAME as 'Departamento'
 FROM
-  hr.job_history
-  INNER JOIN hr.employees on employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
-  WHERE MONTH(job_history.START_DATE) BETWEEN 1 AND 3
-  ORDER BY CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME), START_DATE
+  hr.employees
+  INNER JOIN hr.job_history on employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+  INNER JOIN hr.departments on job_history.DEPARTMENT_ID = departments.DEPARTMENT_ID
+  INNER JOIN hr.jobs on job_history.JOB_ID = jobs.JOB_ID
+ORDER BY
+  CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) DESC,
+  JOB_TITLE ASC
