15461c353e1bc79a03d16ed499373a113d8d79f4
isabellavjs <isabellavjs@gmail.com>
2021-01-06 14:40:09 -0300

    Correct 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,4 +4,5 @@ COUNT(B.Country) - 1 AS `Número de compatriotas`
 FROM customers AS B
 INNER JOIN customers AS A ON A.Country = B.Country
 GROUP BY A.CustomerID
+HAVING `Número de compatriotas` > 1
 ORDER BY Nome;
