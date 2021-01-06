c1d954533d5987cb9acf13d2d4efac24acaa2c23
Renan Cunha <renan2610@gmail.com>
2021-01-06 15:51:54 -0300

    Fixing challenge 6 - fifth time

--- desafio6.sql
+++ desafio6.sql
@@ -7,4 +7,4 @@ FROM hr.job_history AS jh
 INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY 'Nome completo' DESC, Cargo;
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, Cargo;
