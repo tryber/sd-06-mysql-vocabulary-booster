f40c78cf0f36da2afdb5290116ae0899e48f3238
HCaetano <hrcgeo@gmail.com>
2021-01-06 19:25:31 -0300

    Fix challenge 16, try new alias

--- desafio16.sql
+++ desafio16.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN search_term VARCHAR(200))
 BEGIN
-SELECT COUNT(job_history.employee_id)
+SELECT COUNT(job_history.employee_id) AS total_empregos
 FROM job_history
 INNER JOIN employees ON job_history.employee_id = employees.employee_id
 WHERE email = search_term;
