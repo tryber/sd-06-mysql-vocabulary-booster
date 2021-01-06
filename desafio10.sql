04ceb1c8ce12e761f4259b20dd5cfa281fd717c8
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-06 19:47:12 -0300

    Refactoring tenth challenge

--- desafio10.sql
+++ desafio10.sql
@@ -1,23 +1,11 @@
 SELECT
 p.ProductName AS Produto,
-(
-SELECT MIN(Quantity)
-FROM w3schools.order_details 
-WHERE ProductID = od.ProductID
-) AS 'Mínima',
-(
-SELECT MAX(Quantity)
-FROM w3schools.order_details 
-WHERE ProductID = od.ProductID
-) AS 'Máxima',
-(
-SELECT ROUND(AVG(Quantity), 2)
-FROM w3schools.order_details 
-WHERE ProductID = od.ProductID
-) AS 'Média'
+MIN(od.Quantity) AS 'Mínima',
+MAX(od.Quantity) AS 'Máxima',
+ROUND(AVG(od.Quantity), 2) AS `Média`
 FROM w3schools.order_details AS od
 INNER JOIN w3schools.products AS p
 ON od.ProductID = p.ProductID
 GROUP BY od.ProductID
-HAVING AVG(od.Quantity) > 20
-ORDER BY AVG(od.Quantity), p.ProductName;
+HAVING `Média` > 20
+ORDER BY `Média`, Produto;
