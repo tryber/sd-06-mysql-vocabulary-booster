9f1bdbd5cbf3f67de74a3cf61ef7989d81da5823
CristianoNunes <cristianonunes99@gmail.com>
2021-01-06 19:04:54 -0300

    Desafio 15 - Correcao 3

--- desafio15.sql
+++ desafio15.sql
@@ -1,14 +1,13 @@
 USE hr;
-
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
 DELIMITER $$
-CREATE PROCEDURE buscar_media_cargo(
-IN job_name VARCHAR(35))
+CREATE PROCEDURE buscar_media_por_cargo(
+  IN job_name VARCHAR(35))
 BEGIN
-SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
-FROM hr.employees AS e
-INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-WHERE j.JOB_TITLE = job_name;
+  SELECT ROUND(AVG(e.SALARY),2) AS `Média salarial`
+  FROM hr.employees AS e
+  INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+  WHERE j.JOB_TITLE = job_name;
 END $$
 DELIMITER ;
-
-CALL buscar_media_cargo('Programmer');
+CALL buscar_media_por_cargo('Programmer');
