4cef58e982c2cc817cd8e63fe7c7adb47ebf51a3
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-06 16:08:00 -0300

    Chal.7 fix

--- desafio7.sql
+++ desafio7.sql
@@ -3,10 +3,10 @@ SELECT UPPER(CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME)) AS 'Nome completo',
   emp.SALARY AS 'Salário',
   MONTH(hist.START_DATE) AS MES
 FROM hr.job_history AS hist
-  INNER JOIN hr.employees AS emp
-    ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
-WHERE MONTH(hist.START_DATE) != 1
-  AND MONTH(hist.START_DATE) != 2
-  AND MONTH(hist.START_DATE) != 3
+INNER JOIN hr.employees AS emp
+  ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
+WHERE MONTH(hist.START_DATE) = 1
+  OR MONTH(hist.START_DATE) = 2
+  OR MONTH(hist.START_DATE) = 3
 ORDER BY CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME) ASC,
   hist.START_DATE ASC;
