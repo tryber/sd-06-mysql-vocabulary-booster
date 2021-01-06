c0dc9d920181fa6a909e17f94ea167a94bf0157d
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-06 12:09:29 -0300

    corrigindo Nome completo

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT
-CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) AS 'Nome Completo',
+CONCAT(employees.FIRST_NAME, " ", employees.LAST_NAME) AS 'Nome completo',
 jobs.JOB_TITLE AS 'Cargo',
 employees.HIRE_DATE AS 'Data de início do cargo',
 departments.DEPARTMENT_NAME AS 'Departamento'
