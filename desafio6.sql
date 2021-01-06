5f07319a15d520634360f842f58f1eceb1f32a52
Zeca Costa <jccostaso@gmail.com>
2021-01-06 17:10:57 -0300

    refact: desafio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -3,8 +3,8 @@ CONCAT(e.FIRST_NAME, " ", e.LAST_NAME) AS `Nome Completo`,
 j.JOB_TITLE AS Cargo,
 jh.START_DATE AS `Data de início do cargo`,
 d.DEPARTMENT_NAME AS Departamento
-FROM hr.employees AS e
-INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
 INNER JOIN hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome Completo` DESC, Cargo ASC
