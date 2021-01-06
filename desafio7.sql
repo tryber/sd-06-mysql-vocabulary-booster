2640fd4bdb5224a4bf78067e2db85b287c426c56
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-06 17:10:32 -0300

    corrigindo ordenamento do requisito7

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,5 @@ employees.SALARY AS 'Salário'
 FROM hr.employees
 INNER JOIN job_history ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
 WHERE MONTH(job_history.START_DATE) BETWEEN 1 AND 3
-ORDER BY 'Nome completo', 'Data de início';
+ORDER BY UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)),
+job_history.START_DATE asc;
