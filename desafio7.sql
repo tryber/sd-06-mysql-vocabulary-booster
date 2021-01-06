9349ea2d84ac919aec078917d1b4d2cfc07906fa
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-06 20:01:29 -0300

    style: indentation

--- desafio7.sql
+++ desafio7.sql
@@ -2,7 +2,7 @@ SELECT
     UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) 'Nome completo',
     h.START_DATE 'Data de início',
     e.SALARY 'Salário'
- FROM hr.job_history h
+  FROM hr.job_history h
     LEFT JOIN hr.employees e
     ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
   WHERE MONTH(h.START_DATE) IN (1,2,3)
