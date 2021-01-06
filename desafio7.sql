9ffc2a8e978491164b8f5c7021214b4ccefb6757
DekoPossas <andrepossaster@gmail.com>
2021-01-06 16:53:03 -0300

    nomes em caixa alta, salario, data de inicio de janeiro a março

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS 'Nome Completo',
+jobhis.START_DATE AS 'Data de inicio',
+emp.SALARY AS 'Salário'
+FROM hr.job_history as jobhis
+INNER JOIN hr.employees AS emp
+ON emp.EMPLOYEE_ID = jobhis.EMPLOYEE_ID
+WHERE MONTH(jobhis.START_DATE) <= 3
+ORDER BY emp.FIRST_NAME, jobhis.START_DATE;
