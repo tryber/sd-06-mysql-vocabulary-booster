f89b71bb875df9c92605739303b6e0ceb6682436
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-06 16:43:11 -0300

    Added avg

--- desafio10.sql
+++ desafio10.sql
@@ -9,9 +9,15 @@ WHERE ProductID = od.ProductID
 SELECT MAX(Quantity)
 FROM w3schools.order_details 
 WHERE ProductID = od.ProductID
-) AS 'Máxima'
+) AS 'Máxima',
+(
+SELECT AVG(Quantity)
+FROM w3schools.order_details 
+WHERE ProductID = od.ProductID
+) AS 'Média'
 FROM w3schools.order_details AS od
 INNER JOIN w3schools.products AS p
 ON od.ProductID = p.ProductID
 GROUP BY od.ProductID
-ORDER BY p.ProductName;
+HAVING AVG(od.Quantity) > 20
+ORDER BY AVG(od.Quantity), p.ProductName;
