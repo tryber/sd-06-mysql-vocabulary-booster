78519e460c64804f1698e845fa927060f49269f5
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-06 15:10:54 -0300

    Chal.7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME) AS 'Nome completo',
+  hist.START_DATE AS 'Data de início',
+  emp.SALARY AS 'Salário'
+FROM hr.job_history AS hist
+INNER JOIN hr.employees AS emp
+  ON hist.EMPLOYEE_ID = emp.EMPLOYEE_ID
+ORDER BY CONCAT(emp.FIRST_NAME, " ", emp.LAST_NAME) ASC,
+  hist.START_DATE ASC;
