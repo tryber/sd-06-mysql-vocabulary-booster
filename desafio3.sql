2b80c145e09487921bd0683392f82515d4247fa5
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-05 23:49:00 -0300

    correcao desafio 1

--- desafio3.sql
+++ desafio3.sql
@@ -2,7 +2,7 @@ SELECT
     job_title AS 'Cargo',
     CASE
         WHEN MIN_SALARY  THEN (MAX_SALARY -  MIN_SALARY)
-        ELSE 'bla'
+        ELSE ''
     END AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
 ORDER BY (MAX_SALARY -  MIN_SALARY) ASC, job_title ASC;
