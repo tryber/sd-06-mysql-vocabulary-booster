608f5538ef3abca94bc369f9e0cb40bb7895dfcf
Dilenio Enderle <dilenio@gmail.com>
2021-01-06 16:18:05 -0300

    feat: challenge 11

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+SELECT
+  c1.ContactName AS `Nome`,
+  c1.Country AS `País`,
+  COUNT(c2.Country) - 1 AS `Número de compatriotas`
+FROM
+  w3schools.customers AS c1
+JOIN
+  w3schools.customers AS c2 ON c1.Country = c2.Country
+GROUP BY
+  c1.CustomerID
+HAVING
+  `Número de compatriotas` <> 0
+ORDER BY
+  c1.ContactName;
