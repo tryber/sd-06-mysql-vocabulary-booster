0aab0b1c597b8024e1b1c5dd40ecba50f8892824
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-01-06 16:00:31 -0300

    agora o 2 vai

--- desafio2.sql
+++ desafio2.sql
@@ -3,7 +3,7 @@ CASE
 WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
 WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
 WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-ELSE 'Altissímo'
+ELSE 'Altíssimo'
 END AS 'Nível'
 FROM jobs
 ORDER BY Cargo;
