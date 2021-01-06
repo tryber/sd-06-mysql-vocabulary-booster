9802ec652be532084b8fd7e6c8182568e7ff2589
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-06 18:04:21 -0300

    :construction: desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(UCASE(emp.FIRST_NAME), ' ', UCASE(emp.LAST_NAME)) AS `Nome completo`,
+jh.START_DATE AS `Data de início do cargo`,
+emp.SALARY AS `Salário`
+FROM hr.employees AS emp, hr.job_history AS jh
+WHERE emp.EMPLOYEE_ID=jh.EMPLOYEE_ID AND MONTH(jh.START_DATE) IN (1,2,3)
+ORDER BY `Nome completo`, `Data de início do cargo`;
