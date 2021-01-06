076a8407e65dfd2a2585e42e73726e141a52abfd
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-01-06 15:04:13 -0300

    :sparkles: Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT
+a.ContactName AS 'Nome',
+a.Country AS 'País',
+COUNT(b.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS a
+JOIN w3schools.customers AS b
+ON a.Country = b.Country
+GROUP BY (a.CustomerID)
+Having COUNT(b.Country) > 0
+ORDER BY a.ContactName;
