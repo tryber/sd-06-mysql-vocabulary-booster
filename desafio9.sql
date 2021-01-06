c43c7c5c81a91e95c45f0e2121bf135b941d6ae9
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-01-06 15:34:15 -0300

    Solved order by

--- desafio9.sql
+++ desafio9.sql
@@ -3,4 +3,5 @@ COUNT(*) AS 'Total de pedidos'
 FROM w3schools.employees AS e
 INNER JOIN w3schools.orders AS o
 ON e.EmployeeID = o.EmployeeID
-GROUP BY o.EmployeeID;
+GROUP BY o.EmployeeID
+ORDER BY COUNT(*);
