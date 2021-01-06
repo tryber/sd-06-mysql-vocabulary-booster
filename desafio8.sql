6aa50b21bdc85ead63b183c8b0f0daf56998af4a
HCaetano <hrcgeo@gmail.com>
2021-01-06 17:04:44 -0300

    Fix challenge 8, remove order_details from query, add filter by shipper's name

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
 SELECT
-CustomerName AS `Nome de contato`,
-OrderDate AS `Data do pedido`,
-ShipperName AS `Empresa que fez o envio`
-FROM order_details
-INNER JOIN orders ON order_details.OrderID = orders.OrderID 
-INNER JOIN customers ON orders.CustomerID = customers.CustomerID 
-INNER JOIN shippers ON shippers.ShipperID = orders.ShipperID
-ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
+ContactName AS `Nome de contato`,
+ShipperName AS `Empresa que fez o envio`,
+OrderDate AS `Data do pedido`
+FROM w3schools.orders AS o
+INNER JOIN w3schools.customers AS c ON o.CustomerID = c.CustomerID 
+INNER JOIN w3schools.shippers AS s ON s.ShipperID = o.ShipperID
+WHERE ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`
