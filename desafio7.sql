5a00cb5239220494446f3e609700830a8b8e4f7f
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-01-06 08:41:38 -0300

    challanges 7 correction

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT upper(concat(FIRST_NAME, " ",LAST_NAME)) AS "Nome completo",
-START_DATE AS "Data de início do cargo",
+START_DATE AS "Data de início",
 SALARY AS "Salário"
 FROM hr.employees, hr.jobs, hr.job_history
 WHERE hr.employees.JOB_ID = hr.jobs.JOB_ID
