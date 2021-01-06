7f358c9f9e7f4dc96f94620b6f5ec8e078e83e1f
labelle <beelsdelonge@gmail.com>
2021-01-06 17:25:33 -0300

    corrigindo Desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,7 @@
-SELECT C.ContactName AS `Nome`,
-C.Country AS `País`,
-COUNT(D.Country) AS `Número de compatriotas`
+SELECT D.ContactName AS `Nome`,
+D.Country AS `País`,
+COUNT(C.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS C
 INNER JOIN w3schools.customers AS D ON C.Country = D.Country AND C.ContactName <> D.ContactName
-GROUP BY C.CustomerID
-HAVING `Número de compatriotas` > 1
+GROUP BY D.CustomerID
 ORDER BY `Nome` ASC;
