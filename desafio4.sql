4c446e51a0013eb53c6e80c75d63450ff29ae367
rafMewtwo <ponciano.raf@gmail.com>
2021-01-06 08:57:15 -0300

    desafio 4.1

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SELECT j.JOB_TITLE AS "Cargo",
-ROUND(AVG(e.SALARY), 2) AS "Média salarial",
+ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
 CASE
 WHEN ROUND(AVG(e.SALARY),2) BETWEEN 2000 AND 5800 THEN "Júnior"
 WHEN ROUND(AVG(e.SALARY),2) BETWEEN 5801 AND 7500 THEN "Pleno"
@@ -10,4 +10,4 @@ FROM hr.employees AS e
 INNER JOIN hr.jobs as j
 ON e.JOB_ID = j.JOB_ID
 GROUP BY j.JOB_TITLE
-ORDER BY ROUND(AVG(e.SALARY), 2), j.JOB_ID;
+ORDER BY `Média salarial`, j.JOB_ID;
