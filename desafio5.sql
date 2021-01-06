5d9407089563afbdfebef9ad2acf112d984eb417
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-06 08:39:24 -0300

    tirando as casas decimai exe5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
-SELECT JOB_TITLE as "Cargo", ROUND((MAX_SALARY - MIN_SALARY), 2) as "Variação Salarial",
+SELECT JOB_TITLE as "Cargo", ROUND((MAX_SALARY - MIN_SALARY), 0) as "Variação Salarial",
 ROUND((MIN_SALARY / 12), 2) AS "Média mínima mensal",
 ROUND((MAX_SALARY / 12), 2) AS "Média máxima mensal"
 FROM jobs order by ROUND((MAX_SALARY - MIN_SALARY), 2), JOB_TITLE;
