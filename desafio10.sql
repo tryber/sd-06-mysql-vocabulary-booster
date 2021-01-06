a1d38cd6233c34cc1b2445ef3629ad255e37dd9f
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-01-06 11:35:09 -0300

    challanges 10 correction

--- desafio10.sql
+++ desafio10.sql
@@ -5,6 +5,6 @@ ROUND(AVG(Quantity),2) AS "Média"
 FROM w3schools.products AS prod
 INNER JOIN w3schools.order_details AS ord
 ON prod.ProductID = ord.ProductID
-WHERE Quantity > 20
 GROUP BY ProductName
+HAVING Média > 20
 ORDER BY Média, Produto; 
