2f1fd24ed89b5b8a9a60d5177fbdbfbe2d1ed6b4
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-06 18:20:43 -0300

    tirando o use e atribuindo o esquema na tabela

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,8 @@
-use w3schools;
 select
 customers.ContactName as 'Nome de contato',
 shippers.ShipperName as 'Empresa que fez o envio',
 orders.OrderDate as 'Data do pedido'
-from orders
+from w3schools.orders
 inner join customers on customers.CustomerID = orders.CustomerID
 inner join shippers on shippers.ShipperID = orders.ShipperID
 where shippers.ShipperName = 'Speedy Express' or shippers.ShipperName = 'SUnited Package'
