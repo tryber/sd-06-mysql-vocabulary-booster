02667fdfb878f755c0714bec4c51f0137305c19c
HCaetano <hrcgeo@gmail.com>
2021-01-06 16:22:35 -0300

    Fix challenge 7, adding month check and using correct date

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,8 @@
 SELECT
 UPPER(CONCAT(first_name, ' ', last_name)) AS `Nome completo`,
-hire_date AS `Data de início`,
+start_date AS `Data de início`,
 salary AS Salario
-FROM employees
+FROM hr.employees
+INNER JOIN hr.job_history ON job_history.employee_id = employees.employee_id
+WHERE MONTH(start_date) IN (1, 2, 3)
 ORDER BY `Nome completo`, `Data de início`;
