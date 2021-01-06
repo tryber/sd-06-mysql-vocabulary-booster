b69098d09dd373e83ba6a6935fb30b414a7bb044
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-06 16:56:36 -0300

    fix: start_date now taken from where it should be

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,13 @@
 SELECT
   CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo',
   j.JOB_TITLE 'Cargo',
-  e.HIRE_DATE 'Data de início do cargo',
+  h.START_DATE 'Data de início do cargo',
   d.DEPARTMENT_NAME 'Departamento'
 FROM hr.employees e
   LEFT JOIN hr.jobs j
     ON e.JOB_ID = j.JOB_ID
   LEFT JOIN hr.departments d
     ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+  LEFT JOIN hr.job_history h
+    ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 ORDER BY e.FIRST_NAME, e.LAST_NAME, j.JOB_TITLE;
