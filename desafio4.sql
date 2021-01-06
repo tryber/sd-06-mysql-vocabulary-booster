3fc38b0e49bf78511864d847c8b96d71ba0d8817
Ana Higo <anahigo.arq@gmail.com>
2021-01-06 16:13:47 -0300

    Alteraçao Where para Inner JOIN

--- desafio4.sql
+++ desafio4.sql
@@ -6,7 +6,8 @@ WHEN AVG(E.SALARY) >= 5801 AND AVG(E.SALARY) <= 7500 THEN 'Pleno'
 WHEN AVG(E.SALARY) >= 7501 AND AVG(E.SALARY) <= 10500 THEN 'Sênior'
 WHEN AVG(E.SALARY) > 10500 THEN 'CEO'
 END AS `Senioridade`
-FROM hr.jobs AS J, hr.employees AS E
-WHERE J.JOB_ID = E.JOB_ID
+FROM hr.jobs AS J
+INNER JOIN hr.employees AS E
+ON J.JOB_ID = E.JOB_ID
 GROUP BY `Cargo`
 ORDER BY `Média salarial`, `Cargo`;
--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,11 @@ SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
 J.JOB_TITLE AS `Cargo`,
 JH.START_DATE AS `Data de início do cargo`,
 D.DEPARTMENT_NAME AS `Departamento`
-FROM hr.employees AS E,
-hr.jobs AS J,
-hr.job_history AS JH,
-hr.departments AS D
-WHERE J.JOB_ID = JH.JOB_ID
-AND E.EMPLOYEE_ID = JH.EMPLOYEE_ID
-AND D.DEPARTMENT_ID = JH.DEPARTMENT_ID
+FROM hr.job_history AS JH
+INNER JOIN  hr.employees AS E
+ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+INNER JOIN hr.jobs AS J
+ON J.JOB_ID = JH.JOB_ID
+INNER JOIN hr.departments AS D
+ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo`;
