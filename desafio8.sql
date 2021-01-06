b203bbb328d68356c04e9671aff638ea4567497a
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-06 18:29:54 -0300

    colocando a ordem crescente

--- desafio8.sql
+++ desafio8.sql
@@ -6,4 +6,4 @@ from w3schools.orders
 inner join customers on customers.CustomerID = orders.CustomerID
 inner join shippers on shippers.ShipperID = orders.ShipperID
 where shippers.ShipperName = 'Speedy Express' or shippers.ShipperName = 'SUnited Package'
-order by customers.ContactName, shippers.ShipperName, orders.OrderDate;
+order by customers.ContactName asc, shippers.ShipperName asc, orders.OrderDate asc;
