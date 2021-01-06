c0d7aff189594e5b5da1d4f6f3797986cc547a37
Júlia Pôrto <juportob@gmail.com>
2021-01-06 14:48:31 -0300

    Desafio5

---
+++ desafio5.sql
@@ -0,0 +1,6 @@
+SELECT J.JOB_TITLE AS Cargo,
+MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
+ROUND((MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+ROUND((MAX_SALARY /12), 2) AS 'Média máxima mensal'
+FROM hr.jobs AS J
+ORDER BY MAX_SALARY - MIN_SALARY, Cargo;
