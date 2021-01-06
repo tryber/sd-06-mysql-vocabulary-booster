8bd58649551127b3eba55e8bf2e970cb233a8b71
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-01-06 19:02:57 -0300

    Desafio 16 correção

--- desafio16.sql
+++ desafio16.sql
@@ -3,13 +3,15 @@ DROP PROCEDURE IF EXISTS buscar_quantidade_de_empregos_por_funcionario;
 
 DELIMITER $$
 CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(
-  IN email VARCHAR(25))
+  IN email VARCHAR(25),
+  OUT total INT)
 BEGIN 
-  SELECT COUNT(*) AS total_empregos
+  SELECT COUNT(*) INTO total
   FROM hr.job_history AS jh
   INNER JOIN hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
   WHERE e.EMAIL = email;
 END $$
 DELIMITER ;
 
-CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
+CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR', @tot_empregos);
+SELECT ROUND(@tot_empregos, 2) AS total_empregos;
