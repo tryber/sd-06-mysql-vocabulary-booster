e73266f77c59c0dcdd1832e6ba5448f1b32ae48a
Renan Cunha <renan2610@gmail.com>
2021-01-06 18:14:53 -0300

    Adding challenge 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+select
+p.ProductName as `Produto`,
+min(od.Quantity) as `Mínima`,
+max(od.Quantity) as `Máxima`,
+round(avg(od.Quantity), 2) as `Média`
+from w3schools.order_details as od
+inner join w3schools.products as p on od.ProductID = p.ProductID
+where od.Quantity > 20
+group by od.ProductID
+order by `Média`, `Produto`;
