4f724c8907982df2cdcefd4f113698f2268c207f
ilanribeiro <ilanribeiro@gmail.com>
2021-01-06 14:58:52 -0300

    change: desafio4.sql

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT job.JOB_TITLE AS "Cargo",
+SELECT jobs.JOB_TITLE AS "Cargo",
 ROUND(AVG(SALARY), 2) as "Média salarial",
 CASE 
 WHEN ROUND(AVG(SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
@@ -6,8 +6,8 @@ WHEN ROUND(AVG(SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
 WHEN ROUND(AVG(SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
 ELSE 'CEO'
 END AS 'Senioridade'
-FROM hr.employees AS emp
-INNER JOIN hr.jobs AS job
-ON emp.JOB_ID = job.JOB_ID
+FROM hr.employees AS employees
+INNER JOIN hr.jobs AS jobs
+ON employees.JOB_ID = jobs.JOB_ID
 GROUP BY Cargo
-ORDER BY 2, 1;
+ORDER BY ROUND(AVG(SALARY), 2), Cargo;
