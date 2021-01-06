d08fc9760ddd6e475148f02cd2a18d0278e17956
HCaetano <hrcgeo@gmail.com>
2021-01-06 15:52:50 -0300

    Add challenge 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,9 @@
-SELECT job_title AS 'Cargo', 
-CASE
-  WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
-  WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
-  WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-  WHEN max_salary > 30000 THEN 'Altíssimo'
-END AS 'Nível'
-FROM jobs
-ORDER BY job_title;
+SELECT
+ProductName AS Produto,
+MIN(Quantity) AS Mínima,
+MAX(Quantity) AS Máxima,
+ROUND(AVG(Quantity), 2) AS Média
+FROM order_details
+INNER JOIN products ON order_details.ProductID = products.ProductID
+GROUP BY ProductName
+ORDER BY Média, Produto;
