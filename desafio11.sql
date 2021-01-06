62329a34a1acd313872724c41aab84adfb1a106b
isabellavjs <isabellavjs@gmail.com>
2021-01-06 16:37:40 -0300

    Correct 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 SELECT A.ContactName AS Nome,
 A.Country AS `País`,
-COUNT(B.Country) - 1 AS `Número de compatriotas`
+COUNT(B.Country) AS `Número de compatriotas`
 FROM customers AS B
-INNER JOIN customers AS A ON A.Country = B.Country
+INNER JOIN customers AS A ON A.Country = B.Country AND A.ContactName <> B.ContactName
 GROUP BY A.CustomerID
 HAVING `Número de compatriotas` > 1
 ORDER BY Nome;
