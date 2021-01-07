939029bc6b6eef8bf4a333be8bc2913127eebb80
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-07 19:15:12 -0300

    Chal.12

--- desafio12.sql
+++ desafio12.sql
@@ -1,9 +1,11 @@
-SELECT c1.ContactName AS 'Nome',
-  c1.Country AS 'País',
-  COUNT(c2.Country) - 1 AS 'Número de compatriotas'
-FROM w3schools.customers AS c1
-  JOIN w3schools.customers AS c2
-    ON c1.Country = c2.Country
-GROUP BY c1.ContactName, c1.Country
-HAVING COUNT(c1.Country) - 1 > 0
-ORDER BY c1.ContactName;
+SELECT (CONCAT(em.first_name, ' ', em.last_name)) AS 'Nome completo funcionário 1',
+  em.SALARY AS 'Salário funcionário 1',
+  em.PHONE_NUMBER AS 'Telefone funcionário 1',
+  (CONCAT(em2.first_name, ' ', em2.last_name)) AS 'Nome completo funcionário 2',
+  em2.SALARY AS 'Salário funcionário 2',
+  em2.PHONE_NUMBER AS 'Telefone funcionário 2'
+FROM hr.employees AS em
+  INNER JOIN hr.employees AS em2
+    ON em.JOB_ID = em2.JOB_ID
+WHERE em.EMPLOYEE_ID <> em2.EMPLOYEE_ID
+ORDER BY (CONCAT(em.first_name, ' ', em.last_name)) ASC, (CONCAT(em2.first_name, ' ', em2.last_name)) ASC;
