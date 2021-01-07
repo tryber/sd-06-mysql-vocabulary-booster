f2e6d06360ca41aac3391bef853597314ebf2c95
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-06 22:46:11 -0300

    req 01, 02, 03, 04

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,7 @@
-SELECT product_name FROM northwind.products;
+SELECT countries.COUNTRY_NAME AS 'País',
+IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries
+ORDER BY COUNTRY_NAME;
 
 -- REFERENCIAS
 -- https://www.sqlservertutorial.net/
--- desafio3.sql
+++ desafio3.sql
@@ -1,1 +1,8 @@
-SELECT id FROM northwind.products;
+SELECT jobs.JOB_TITLE AS 'Cargo',
+(MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+FROM hr.jobs
+ORDER BY 'Diferença entre salários máximo e mínimo', 'Cargo';
+-- (CASE
+--     WHEN  IS NULL THEN Country
+--     ELSE 'Diferença entre salários máximo e mínimo'
+-- END);
