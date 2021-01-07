a47f269e877a2ff3b9b8f5993c5ef0bdab7d0697
Paulo Frazão <paulo4187@gmail.com>
2021-01-07 14:18:52 -0300

    inclusion file challenge 7 change upper strings name full

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT concat(Emp.FIRST_NAME,' ', Emp.LAST_NAME) AS 'Nome completo',
+SELECT UPPER(concat(Emp.FIRST_NAME,' ', Emp.LAST_NAME)) AS 'Nome completo',
 HIS.START_DATE AS 'Data de início', Emp.SALARY AS 'Salário'  FROM hr.job_history AS HIS 
 INNER JOIN hr.employees AS Emp  ON HIS.EMPLOYEE_ID = Emp.EMPLOYEE_ID 
 WHERE MONTH(HIS.START_DATE) IN (01,02,03)
