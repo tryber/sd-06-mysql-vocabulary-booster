a344e059cb259f71e707c9cfc7627f8c3b2a6d84
rafMewtwo <ponciano.raf@gmail.com>
2021-01-07 19:19:29 -0300

    desafio 11.2

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
 SELECT t1.ContactName AS "Nome",
 t1.Country AS "País",
-COUNT(*) - 1 AS "Número de compatriotas"
+(COUNT(*) - 1) AS "Número de compatriotas"
 FROM w3schools.customers AS t1
 INNER JOIN w3schools.customers AS t2
 ON t1.Country = t2.Country
