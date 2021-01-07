6a695abc45c481e8c3b628c069cf601454eda32b
Renan Cunha <renan2610@gmail.com>
2021-01-07 13:17:59 -0300

    Fixing challenge 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,10 +2,11 @@ SELECT
 c1.ContactName AS `Nome`,
 c1.Country AS `País`,
 (
-SELECT COUNT(c1.Country) - 1
+SELECT COUNT(c2.Country) - 1
+FROM w3schools.customers AS c2
 WHERE c1.Country = c2.Country
 ) AS `Número de compatriotas`
-FROM w3schools.customers AS c1, w3schools.customers AS c2
-WHERE c1.Country = c2.Country
+FROM w3schools.customers AS c1
 GROUP BY `Nome`, `País`
+HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
