3526202f1515bedad2dc4ca203349988ed79e9bd
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-06 23:42:23 -0300

    teste 01

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
-SELECT countries.COUNTRY_NAME AS 'País',
-IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM hr.countries
-ORDER BY COUNTRY_NAME;
+-- SELECT countries.COUNTRY_NAME AS 'País',
+-- IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+-- FROM hr.countries
+-- ORDER BY COUNTRY_NAME;
 
 -- REFERENCIAS
 -- https://www.sqlservertutorial.net/
