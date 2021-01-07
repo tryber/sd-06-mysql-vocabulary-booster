a8aa595465c9e7b6d08df885d8f36195b7aba0a9
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-07 02:31:48 -0300

    req 06-test; req 07-ip

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
-SELECT UPPER(CONCAT(hr.employees.FIRST_NAME, ' ', hr.employees.LAST_NAME)) AS 'Nome completo',
-hr.job_history.START_DATE AS  'Data de início',
-hr.employees.SALARY AS 'Salário',
-FROM hr.employees
-INNER JOIN hr.job_history ON hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
-ORDER BY hr.employees.FIRST_NAME, hr.job_history.START_DATE;
+-- SELECT UPPER(CONCAT(hr.employees.FIRST_NAME, ' ', hr.employees.LAST_NAME)) AS 'Nome completo',
+-- hr.job_history.START_DATE AS  'Data de início',
+-- hr.employees.SALARY AS 'Salário',
+-- FROM hr.employees
+-- INNER JOIN hr.job_history ON hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
+-- ORDER BY hr.employees.FIRST_NAME, hr.job_history.START_DATE;
