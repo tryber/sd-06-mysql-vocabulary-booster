ca7d52c390f328bf91600b0d5600e6d5e927508b
afornagieri <fornagieri.artur@gmail.com>
2021-01-07 13:18:43 -0300

    fix desafios 15 e 16

--- desafio15.sql
+++ desafio15.sql
@@ -3,9 +3,9 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN nome_do_cargo TINYTEXT)
 BEGIN
-	SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial'
-    FROM employees AS E, jobs AS J
-    WHERE J.JOB_ID = E.JOB_ID AND J.JOB_TITLE = nome_do_cargo;
+SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial'
+FROM employees AS E, jobs AS J
+WHERE J.JOB_ID = E.JOB_ID AND J.JOB_TITLE = nome_do_cargo;
 END $$
 
 DELIMITER ;
