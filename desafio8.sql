5712a038c150b8fbc00441608d8bee61a23e70b3
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-07 20:01:59 -0300

    testando desafio 8.

--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,4 @@ FROM w3schools.customers AS c
 JOIN w3schools.orders AS o ON o.CustomerID = c.CustomerID
 JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
 WHERE o.ShipperID BETWEEN 1 AND 2
-ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
