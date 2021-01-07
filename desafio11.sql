054bb08c6fb63d9de934e2ad75d49e9e5f935e25
rafMewtwo <ponciano.raf@gmail.com>
2021-01-07 20:58:16 -0300

    desafio 11.5

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,8 @@
-SELECT A.ContactName AS Nome,
-A.Country AS `País`,
-COUNT(B.Country) AS `Número de compatriotas`
-FROM customers AS B
-INNER JOIN customers AS A ON A.Country = B.Country AND A.ContactName <> B.ContactName
-GROUP BY A.CustomerID
+SELECT t1.ContactName AS Nome,
+t1.Country AS `País`,
+COUNT(t2.Country) AS `Número de compatriotas`
+FROM customers AS t2
+INNER JOIN customers AS t1 ON t1.Country = t2.Country
+AND t1.ContactName <> t2.ContactName
+GROUP BY t1.CustomerID
 ORDER BY Nome;
