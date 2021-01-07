e0df5b7f5903321f3fff70edfe8b5892462aa904
Jean Machado <jeanvictor.arq@gmail.com>
2021-01-06 21:07:42 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT
+cust.ContactName AS 'Nome de contato',
+ship.ShipperName AS 'Empresa que fez o envio',
+ord.OrderDate AS 'Data do pedido'
+FROM w3schools.orders AS ord
+INNER JOIN w3schools.customers AS cust
+ON ord.CustomerID = cust.CustomerID
+INNER JOIN w3schools.shippers AS ship
+ON ord.ShipperID = ship.ShipperID
+WHERE ship.ShipperName = 'Speedy Express' || ship.ShipperName = 'United Package'
+ORDER BY
+cust.ContactName,
+ship.ShipperName,
+ord.OrderDate;
