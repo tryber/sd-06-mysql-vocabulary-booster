11cd6a77bbd2d733c62dc6a9127efc3caf5a3c6c
Marxneves <marxkhristie@gmail.com>
2021-01-07 00:23:58 -0300

    Fixing some bugs with pull master

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
 SELECT country_name AS `País`, 
 CASE 
-	WHEN region_id = 1 THEN 'incluído'
-  ELSE 'não incluído'
+WHEN region_id = 1 THEN 'incluído'
+ELSE 'não incluído'
 END AS `Status Inclusão` 
 FROM hr.countries;
--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 SELECT job_title AS `Cargo`,
 CASE 
-	WHEN max_salary BETWEEN 5000 and 10000  THEN 'Baixo'
-    WHEN max_salary > 10001 and max_salary < 20000 THEN 'Médio'
-    WHEN max_salary > 20001 and max_salary < 30000 THEN 'Alto'
-    WHEN max_salary > 30000 THEN 'Altíssimo'
+WHEN max_salary BETWEEN 5000 and 10000  THEN 'Baixo'
+WHEN max_salary > 10001 and max_salary < 20000 THEN 'Médio'
+WHEN max_salary > 20001 and max_salary < 30000 THEN 'Alto'
+WHEN max_salary > 30000 THEN 'Altíssimo'
 END AS `Nível` 
 FROM hr.jobs
 ORDER BY `Cargo`;
