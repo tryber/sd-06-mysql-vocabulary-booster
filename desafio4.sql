4fb837f45654607273841bcacfe7f18269592e1c
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-06 22:56:53 -0300

    req 01, 02, 03, 04 - codeclimate

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 SELECT jobs.JOB_TITLE AS 'Cargo',
 ROUND(AVG(hr.employees.SALARY), 2) AS 'Média salarial',
 CASE
-	WHEN AVG(hr.employees.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
-	WHEN AVG(hr.employees.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
-	WHEN AVG(hr.employees.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
-	WHEN AVG(hr.employees.SALARY) > 10500 THEN 'CEO'
+WHEN AVG(hr.employees.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN AVG(hr.employees.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN AVG(hr.employees.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN AVG(hr.employees.SALARY) > 10500 THEN 'CEO'
 END AS 'Senioridade'
 FROM hr.employees
 INNER JOIN hr.jobs
