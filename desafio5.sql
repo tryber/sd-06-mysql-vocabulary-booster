ea547b7b3b306dcb00575e5be1f4e32e54a6943e
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-07 00:10:12 -0300

    req 05

--- desafio5.sql
+++ desafio5.sql
@@ -1,1 +1,7 @@
-SELECT * FROM northwind.products LIMIT 10 OFFSET 3;
+SELECT hr.jobs.JOB_TITLE AS 'Cargo',
+ROUND(hr.jobs.MAX_SALARY - hr.jobs.MIN_SALARY) AS 'Variação Salarial',
+ROUND(AVG(hr.jobs.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+ROUND(AVG(hr.jobs.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
+FROM hr.jobs
+ORDER BY (hr.jobs.MAX_SALARY - hr.jobs.MIN_SALARY), hr.jobs.JOB_TITLE;
+-- ORDER BY 'Variação Salarial', 'Cargo';
