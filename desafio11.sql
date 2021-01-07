e36e526856a0eeb25ee29dc2534da67596ecd97a
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-07 19:50:24 -0300

    correcoes 11 e 13.

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,4 @@
-SELECT e1.ContactName AS `Nome`, e1.Country AS `País`, COUNT(e2.Country) - 1 AS `Número de compatriotas`
+SELECT c1.ContactName AS `Nome`, c1.Country AS `País`, COUNT(c2.Country) - 1 AS `Número de compatriotas`
 FROM w3schools.customers AS c1
 JOIN w3schools.customers AS c2 ON c1.Country = c2.Country
 GROUP BY c1.CustomerID
