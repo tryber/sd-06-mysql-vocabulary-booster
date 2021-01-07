4e6ff5c0137297399726b4f9d7edca18f5ec8388
Júlia Pôrto <juportob@gmail.com>
2021-01-07 10:22:52 -0300

    Desafio 13 - fix code

--- desafio13.sql
+++ desafio13.sql
@@ -2,5 +2,5 @@ SELECT P.ProductName AS Produto,
 P.Price AS Price
 FROM w3schools.products AS P
 JOIN w3schools.order_details AS O ON O.ProductID = P.ProductID
-WHERE Quantity > 80
+WHERE O.Quantity > 80
 ORDER BY Produto;
