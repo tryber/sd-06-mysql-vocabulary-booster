dfe97c1eb6663d25fbec1c576c163f982073ba90
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-07 01:46:46 -0300

    req 06

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,11 @@
--- SELECT CONCAT_WS(" ", hr.) AS 'Nome completo',
--- hr.jobs.JOB_TITLE AS 'Cargo',
--- DATE(MIN_) AS 'Data de início do cargo',
---  AS 'Departamento'
--- FROM hr.jobs
--- ORDER BY hr., hr.jobs.JOB_TITLE;
--- ORDER BY 'Variação Salarial', 'Cargo';
+SELECT CONCAT(hr.employees.FIRST_NAME, ' ', hr.employees.LAST_NAME) AS 'Nome completo',
+hr.jobs.JOB_TITLE AS 'Cargo',
+hr.job_history.START_DATE AS  'Data de início do cargo',
+-- hr.employees.HIRE_DATE AS 'Data de início do cargo',
+hr.departments.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history
+INNER JOIN hr.employees ON hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
+INNER JOIN hr.jobs ON hr.job_history.JOB_ID = hr.jobs.JOB_ID
+INNER JOIN hr.departments ON hr.job_history.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID
+ORDER BY hr.employees.FIRST_NAME DESC, hr.jobs.JOB_TITLE;
+-- ORDER BY `Nome completo` DESC, `Cargo`;
