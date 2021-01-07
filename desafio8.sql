92ecf44fa154f09bca9ea6cc761518c9d55d0d4e
Paulo Frazão <paulo4187@gmail.com>
2021-01-07 15:55:41 -0300

    challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,6 @@
+SELECT C.ContactName AS 'Nome de contato', S.ShipperName AS 'Empresa que fez o envio',
+O.OrderDate AS 'Data do pedido' from w3schools.customers AS C
+INNER JOIN w3schools.orders AS O ON O.CustomerID = C.CustomerID
+INNER JOIN w3schools.shippers AS S ON S.ShipperID = O.ShipperID
+WHERE S.ShipperName = 'Speedy Express' OR S.ShipperName = 'United Package'
+ORDER BY C.ContactName, S.ShipperName, O.OrderDate;
