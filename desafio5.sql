b82197fb9b64085000fed68b2641d396f27c3cca
pa-volpin <paulo.volpin@gmail.com>
2021-01-07 12:29:34 -0300

    Correction on Task 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,6 @@
-SELECT t_job.JOB_TITLE AS Cargo,
-MAX(t_emp.SALARY) - MIN(t_emp.SALARY) AS 'Variação Salarial',
-ROUND(MIN(t_emp.SALARY), 2) AS 'Média mínima mensal',
-ROUND(MAX(t_emp.SALARY), 2) AS 'Média máxima mensal'
-FROM hr.employees AS t_emp
-INNER JOIN hr.jobs AS t_job
-ON t_emp.JOB_ID = t_job.JOB_ID
-GROUP BY t_job.JOB_ID
-ORDER BY MAX(t_emp.SALARY) - MIN(t_emp.SALARY), t_job.JOB_TITLE;
+SELECT JOB_TITLE AS Cargo,
+ROUND(MAX_SALARY - MIN_SALARY, 2) AS 'Variação Salarial',
+ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
+FROM  hr.jobs
+ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
