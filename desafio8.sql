7ba8d1c6a9c51afacffa7d8927bee3b0efbf3e1d
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-07 01:39:01 -0300

    desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,11 @@
 SELECT
-cust.ContactName AS `Nome de Contato`,
+cust.ContactName AS `Nome de contato`,
 ship.ShipperName AS `Empresa que fez o envio`,
-orders.OrderDate AS `Data do Pedido`
-FROM w3schools.orders AS orders
+ord.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS ord
 INNER JOIN w3schools.customers AS cust
-ON orders.CustomerID = cust.CustomerID
+ON ord.CustomerID = cust.CustomerID
 INNER JOIN w3schools.shippers AS ship
-ON ship.ShipperID = orders.ShipperID
-WHERE ship.ShipperName = 'Speedy Express' || ship.ShipperName = "United Package"
-ORDER BY `Nome de Contato`, `Empresa que fez o envio`, `Data do Pedido`;
+ON ship.ShipperID = ord.ShipperID
+WHERE ship.ShipperName = 'Speedy Express' || ship.ShipperName = 'United Package'
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
