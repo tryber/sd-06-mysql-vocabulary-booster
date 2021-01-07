41a4728b81c1e71bb5535d2ed71a0f315de0f47b
Júlia Pôrto <juportob@gmail.com>
2021-01-07 10:16:16 -0300

    Desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT P.ProductName AS Produto,
+P.Price AS Price
+FROM w3schools.products AS P
+JOIN w3schools.order_details AS O ON O.ProductID = P.ProductID
+WHERE Quantity > 80
+ORDER BY Produto;
