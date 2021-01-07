efaaff2d9124fdd5458e5cfb33c71a317d6d085a
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-07 15:30:30 -0300

    fix: filter products with order quantity > 80

--- desafio13.sql
+++ desafio13.sql
@@ -1,5 +1,10 @@
 SELECT
   p.ProductName Produto,
   p.Price Preço
-FROM w3schools.products p
+FROM w3schools.order_details od
+  LEFT JOIN w3schools.products p
+    ON od.ProductID = p.ProductID
+  LEFT JOIN w3schools.orders o
+    ON od.OrderID = o.OrderID
+WHERE od.Quantity > 80
 ORDER BY p.ProductName;
