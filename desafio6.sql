31e2d2765eb89cee7a4c993cb19e9c0648d0158d
Paulo Frazão <paulo4187@gmail.com>
2021-01-07 13:16:52 -0300

    challenge 6 change code

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,6 @@
 SELECT concat(Emp.FIRST_NAME,' ', Emp.LAST_NAME) AS 'Nome completo', J.JOB_TITLE AS 'Cargo',
-Emp.HIRE_DATE AS 'Data de início do cargo', D.DEPARTMENT_NAME AS 'Departamento'  FROM hr.jobs AS J
-INNER JOIN hr.employees AS Emp  ON J.JOB_ID = Emp.JOB_ID 
-INNER JOIN departments AS D ON D.MANAGER_ID = Emp.MANAGER_ID ORDER BY `Nome Completo` DESC, 'Cargo' ASC;
+HIS.START_DATE AS 'Data de início do cargo', D.DEPARTMENT_NAME AS 'Departamento'  FROM hr.job_history AS HIS 
+INNER JOIN hr.employees AS Emp  ON HIS.EMPLOYEE_ID = Emp.EMPLOYEE_ID
+INNER JOIN hr.jobs AS J ON J.JOB_ID = HIS.JOB_ID
+INNER JOIN departments AS D ON D.DEPARTMENT_ID = HIS.DEPARTMENT_ID
+ORDER BY `Nome Completo` DESC, `Cargo` ASC;
