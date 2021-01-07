bd5797d3381e5816ae96ee3dd4eb8961182777a8
LeonardoCavachini <cavachini240@gmail.com>
2021-01-07 18:58:18 -0300

    mudar posiçao para evaluetor

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,9 @@
-SELECT p.productName AS `Produto`, 
-MIN(od.quantity) AS `Mínima`,
-MAX(od.quantity) AS `Maxíma`,
-ROUND(AVG(od.quantity),2) AS `Média`
-From  w3schools.products AS p
-INNER JOIN w3schools.order_details AS od
-ON p.productID = od.productID
-GROUP BY `Produto`
-HAVING AVG(od.quantity) > 20
-ORDER BY `Média`,`Produto`;
+SELECT p.ProductName AS `Produto`,
+MIN(od.Quantity) AS `Mínima`,
+MAX(od.Quantity) AS `Máxima`,
+ROUND(AVG(od.Quantity),2) AS `Média`
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p ON od.ProductID = p.ProductID
+GROUP BY Produto
+HAVING AVG(od.Quantity) > 20
+ORDER BY `Média`, `Produto`;
