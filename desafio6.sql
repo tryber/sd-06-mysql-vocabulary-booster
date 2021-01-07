c5be2f7bcc1f7a8adf1957653f55763bd5ca56b4
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-06 22:20:28 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 SELECT (SELECT CONCAT(first_name,' ', last_name)) AS `Nome completo`,
 (SELECT job_title FROM hr.jobs WHERE job_id = main.job_id) AS 'Cargo',
 hire_date AS 'Data de início do cargo',
-(SELECT department_name FROM hr.departments WHERE department_id = main.department_id) AS 'Cargo'
+(SELECT department_name FROM hr.departments WHERE department_id = main.department_id) AS 'Departamento'
 FROM hr.employees as main
 ORDER BY `Nome completo` DESC, Cargo;
