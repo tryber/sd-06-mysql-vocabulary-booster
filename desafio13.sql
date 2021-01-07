d46ee6f1e8d3387ea29a7fcaf9e0c80f545711bc
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-07 19:30:28 -0300

    Corrigindo 13.

--- desafio13.sql
+++ desafio13.sql
@@ -1,6 +1,7 @@
-SELECT DISTINCT p.ProductName AS 'Produto',
+SELECT p.ProductName AS 'Produto',
   p.Price AS 'Preço'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o
-ON o.Quantity>80
+ON o.ProductID = p.ProductID
+WHERE o.Quantity > 80
 ORDER BY 'Produto';
