d7815dc99346ad841a9c67aa111d792de2b5ba64
Paulo Frazão <paulo4187@gmail.com>
2021-01-07 16:36:18 -0300

    challenge 9 change string 'total de pedidos'

--- desafio9.sql
+++ desafio9.sql
@@ -1,3 +1,3 @@
 SELECT CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome completo', COUNT(O.EmployeeID) AS 'Total pedidos'
 FROM w3schools.employees AS E INNER JOIN w3schools.orders AS O ON E.EmployeeID = O.EmployeeID
-GROUP BY `Nome completo` ORDER BY `Total pedidos`;
+GROUP BY `Nome completo` ORDER BY `Total de pedidos`;
