cacf2b41016d537b00de03d3255359e309152333
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-01-07 19:26:11 -0300

    Chal.13

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT pro.ProductName AS 'Produto',
+  pro.Price
+FROM w3schools.orders AS ord
+  INNER JOIN w3schools.order_details AS ord_d
+    ON ord.OrderID = ord_d.OrderID
+  INNER JOIN w3schools.products AS pro
+    ON ord_d.ProductID = pro.ProductID
+WHERE ord_d.Quantity > 80
+ORDER BY pro.ProductName ASC;
