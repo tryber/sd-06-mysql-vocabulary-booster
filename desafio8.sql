9cfb919423defa0538930756cfb7eb0e5527a5c7
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-07 19:43:16 -0300

    Corrigindo desafio 8.

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,8 @@
-SELECT c.CustomerName AS 'Nome de contato',
+SELECT c.ContactName AS 'Nome de contato',
 s.ShipperName AS 'Empresa que fez o envio',
 o.OrderDate AS 'Data do pedido'
 FROM w3schools.customers AS c
-INNER JOIN w3schools.orders AS o ON o.CustomerID = c.CustomerID
-INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+JOIN w3schools.orders AS o ON o.CustomerID = c.CustomerID
+JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
 WHERE o.ShipperID BETWEEN 1 AND 2
-ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido`;
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
