4494a5c98d5e02a2ca35ed77da43d1b1156444ee
vapatty6848 <vapatty@gmail.com>
2021-01-07 13:43:14 -0300

    casa decimal

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT P.ProductName AS 'Produto',
 MIN(OD.Quantity) AS 'Mínima',
 MAX(OD.Quantity) AS 'Máxima',
-ROUND(AVG(OD.Quantity)) AS 'Média'
+ROUND(AVG(OD.Quantity), 2) AS 'Média'
 FROM w3schools.products AS P
 INNER JOIN w3schools.order_details AS OD
 ON P.ProductID = OD.ProductID
