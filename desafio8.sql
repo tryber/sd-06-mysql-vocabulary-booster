c83ec5b8e0a8d0bd19c75d2239aa5df00b9fd302
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-07 11:23:52 -0300

    Desafios 8 e 9.

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,8 @@
-USE w3schools;
-
 SELECT c.CustomerName AS 'Nome de contato',
 s.ShipperName AS 'Empresa que fez o envio',
 o.OrderDate AS 'Data do pedido'
-FROM w3schools.customers AS c,
-w3schools.orders AS o,
-w3schools.shippers AS s
-WHERE o.CustomerID = c.CustomerID
-AND o.ShipperID = s.ShipperID
-AND o.ShipperID BETWEEN 1 AND 2
+FROM w3schools.customers AS c
+INNER JOIN w3schools.orders AS o ON o.CustomerID = c.CustomerID
+INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+WHERE o.ShipperID BETWEEN 1 AND 2
 ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido`;
---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+COUNT(o.EmployeeID) AS 'Total de pedidos'
+FROM w3schools.employees AS e,
+w3schools.orders AS o
+WHERE o.EmployeeID = e.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;
