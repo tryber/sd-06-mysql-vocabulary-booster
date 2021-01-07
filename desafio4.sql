172baf2cd30b66cd83244e8f407414478fb5b8e8
vapatty6848 <vapatty@gmail.com>
2021-01-07 13:31:44 -0300

    identaçao

--- desafio4.sql
+++ desafio4.sql
@@ -5,7 +5,7 @@ CASE
   WHEN AVG(E.salary) >= 7501 AND AVG(E.salary) <= 10500 THEN 'Sênior'
   WHEN AVG(E.salary) > 10500 THEN 'CEO'
 END AS 'Senioridade'
- FROM jobs AS J
- INNER JOIN employees AS E ON J.job_id = E.job_id
- GROUP BY J.job_id
- ORDER BY `Média salarial`, `Cargo`;
+  FROM jobs AS J
+  INNER JOIN employees AS E ON J.job_id = E.job_id
+  GROUP BY J.job_id
+  ORDER BY `Média salarial`, `Cargo`;
