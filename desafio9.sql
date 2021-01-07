071c609cf94d030dab4e63f804a4794c7d89b2a7
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-07 11:04:18 -0300

    style: eslint compliance change

--- desafio9.sql
+++ desafio9.sql
@@ -3,6 +3,6 @@ SELECT
   COUNT(o.OrderID) 'Total de pedidos'
 FROM w3schools.employees e
   INNER JOIN w3schools.orders o
-   ON e.EmployeeID = o.CustomerID
+    ON e.EmployeeID = o.CustomerID
   GROUP BY e.EmployeeID
 ORDER BY COUNT(o.OrderID);
