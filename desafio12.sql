0bf1455b539cf06b232c3531146b20254f880dc1
ricardorosa-dev <ricardorosa@gmail.com>
2021-01-08 12:26:43 -0300

    Resolvendo problemas no desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -7,5 +7,5 @@ B.SALARY AS "Salário funcionário 2",
 B.PHONE_NUMBER AS "Telefone funcionário 2"
 
 FROM hr.employees AS A, hr.employees AS B
-WHERE A.FIRST_NAME <> B.FIRST_NAME AND A.JOB_ID = B.JOB_ID
+WHERE A.EMPLOYEE_ID <> B.EMPLOYEE_ID AND A.JOB_ID = B.JOB_ID
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
