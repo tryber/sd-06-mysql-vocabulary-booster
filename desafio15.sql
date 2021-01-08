a0c09b9ba9c8266afaafc064e80ec501ff118b22
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-08 14:33:58 -0300

    feat: req. 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(job)
+BEGIN
+  SELECT ROUND(AVG(e.SALARY), 2)
+  FROM employees e
+    LEFT JOIN jobs j
+      ON e.JOB_ID = j.JOB_ID
+  WHERE j.JOB_TITLE = job
+END $$
+
+DELIMITER ;
