0babfa0fcdf10bfdd571195ffd2762f07fe91b85
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-01-08 19:34:39 -0300

    desafio10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT pr.ProductName AS Produto,
+MIN(prd.Quantity) AS Mínima,
+MAX(prd.Quantity) AS Máxima,
+ROUND(AVG(prd.Quantity), 2) AS Média
+FROM w3schools.products AS pr
+INNER JOIN w3schools.order_details AS prd
+ON pr.ProductID = prd.ProductID
+GROUP BY Produto
+HAVING Média >= 20
+ORDER BY Média, Produto;
