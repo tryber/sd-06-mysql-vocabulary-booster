371ca9316661e37863150867bcdc6871eee2ee87
Paulo Frazão <paulo4187@gmail.com>
2021-01-08 18:47:17 -0300

    add file challenge 16

---
+++ desafio16.sql
@@ -0,0 +1,12 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(20))
+RETURNS VARCHAR(200) READS SQL DATA
+BEGIN
+DECLARE result VARCHAR(200);
+SELECT COUNT(JH.EMPLOYEE_ID) AS 'total_empregos' INTO result FROM hr.job_history AS JH
+INNER JOIN hr.employees AS E ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+WHERE E.EMAIL = email;         
+RETURN result;
+END $$
+DELIMITER ;
