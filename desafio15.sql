99aab8601432f2076276407522fb3a18262180b9
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-08 18:22:02 -0300

    fix: database selected

--- desafio15.sql
+++ desafio15.sql
@@ -3,8 +3,8 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(job VARCHAR(127))
 BEGIN
   SELECT ROUND(AVG(e.SALARY), 2)
-  FROM employees e
-    LEFT JOIN jobs j
+  FROM hr.employees e
+    LEFT JOIN hr.jobs j
       ON e.JOB_ID = j.JOB_ID
   WHERE j.JOB_TITLE = job
 END $$
