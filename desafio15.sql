67320314025531bcaac157c28b49c489dde6c1b1
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-01-07 21:31:10 -0300

    desafio 15° passando

---
+++ desafio15.sql
@@ -0,0 +1,10 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN name_cargo VARCHAR(100))
+BEGIN
+SELECT (SELECT ROUND(AVG(b.SALARY), 2) FROM hr.jobs AS a
+INNER JOIN hr.employees AS b ON a.JOB_ID = b.JOB_ID
+GROUP BY a.JOB_TITLE
+HAVING a.JOB_TITLE = name_cargo) AS `Média salarial`;
+END $$
+DELIMITER ;
