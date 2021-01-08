b143f6aa35575eafbfe1b39b0bd3bad4204cceca
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-08 12:08:30 -0300

    req 12

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,11 @@
--- SELECT w3schools. AS 'Nome de contato',
--- w3schools. AS 'Empresa que fez o envio',
--- w3schools. AS 'Data do pedido',
--- FROM w3schools.
--- WHERE w3schools. = 'Speedy Express' OR United Package
--- ORDER BY **Nome de contato**, **Empresa que fez o envio**, **Data do pedido**
+SELECT w3schools.customers.ContactName AS 'Nome de contato',
+w3schools.shippers.ShipperName AS 'Empresa que fez o envio',
+w3schools.orders.OrderDate AS 'Data do pedido'
+FROM w3schools.customers, w3schools.shippers, w3schools.orders
+
+-- w3schools. = 'Speedy Express' OR United Package
+WHERE w3schools.customers.CustomerID = w3schools.orders.CustomerID
+AND w3schools.shippers.ShipperID = w3schools.orders.ShipperID
+AND w3schools.shippers.ShipperID IN (1, 2)
+ORDER BY w3schools.customers.ContactName, w3schools.shippers.ShipperName, w3schools.orders.OrderDate
+--ORDER BY `Nome de Contato`, `Empresa que fez o envio`, `Data do pedido`
