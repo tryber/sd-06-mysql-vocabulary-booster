ebcbee8e1c9f8720166c96bd98004f98078f6185
FlavioSugano <flavio.sugano@gmail.com>
2021-01-08 15:35:18 -0300

    Desafio 6: remoção do use

--- desafio6.sql
+++ desafio6.sql
@@ -1,16 +1,14 @@
-USE hr;
-
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
     j.JOB_TITLE AS `Cargo`,
     h.START_DATE AS `Data de início do cargo`,
     d.DEPARTMENT_NAME AS `Departamento`
 FROM
-    employees AS e
+    hr.employees AS e
         INNER JOIN
-    job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+    hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
         INNER JOIN
-    jobs AS j ON h.JOB_ID = j.JOB_ID
+    hr.jobs AS j ON h.JOB_ID = j.JOB_ID
         INNER JOIN
-    departments AS d ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
+    hr.departments AS d ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC , Cargo ASC;
