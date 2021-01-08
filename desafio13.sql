a9da6da4b45246825c9fc07b1c242050a177cbbf
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-01-08 19:00:37 -0300

    :rotating_light: excluindo tabs

--- desafio13.sql
+++ desafio13.sql
@@ -2,8 +2,8 @@ SELECT ProductName AS `Produto`,
 Price AS `Preço`
 FROM w3schools.products
 WHERE ProductID IN
-	(
-	SELECT ProductID FROM w3schools.order_details
-	WHERE Quantity>80
-	)
+(
+SELECT ProductID FROM w3schools.order_details
+WHERE Quantity>80
+)
 ORDER BY `Produto`;
