ee255bab8464e0115255ff506093f21d0b645e26
higoramorim <1810042@aluno.univesp.br>
2021-01-09 16:47:26 -0300

    q12 corrigido

--- desafio12.sql
+++ desafio12.sql
@@ -3,7 +3,7 @@ E1.SALARY AS `Salário funcionário 1`, E1.PHONE_NUMBER AS `Telefone funcionári
 concat(E2.FIRST_NAME, ' ', E2.LAST_NAME) AS `Nome completo funcionário 2`,
 E2.SALARY AS `Salário funcionaŕio 2`,
 E2.PHONE_NUMBER AS `Telefone funcionário 2`
-from hr.employess as E1, hr.employess as E2 
+from hr.employees as E1, hr.employees as E2 
 where E1.JOB_ID = E2.JOB_ID
 and concat(E1.FIRST_NAME, ' ', E1.LAST_NAME) <> concat(E2.FIRST_NAME, ' ', E2.LAST_NAME)
 order by `Nome completo funcionário 1` asc,
