395daf96703261c6dcfd7754062f6df3023a6f71
Bruna Franciele <brunafranciele1902@gmail.com>
2021-01-09 20:55:54 -0300

    correção do 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,12 +1,12 @@
 DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo(IN search_term VARCHAR(200))
+CREATE PROCEDURE buscar_media_por_cargo(IN param VARCHAR(200))
 BEGIN
 SELECT ROUND(AVG(salary), 2) AS `Média salarial`
 FROM employees
 INNER JOIN jobs ON employees.job_id = jobs.job_id
-WHERE job_title = search_term;
+WHERE job_title = param;
 END $$
 
 DELIMITER;
