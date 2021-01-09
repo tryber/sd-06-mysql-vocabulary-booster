2aac4b83226fb726e2c70e67146aedce8ad3743c
ricardorosa-dev <ricardorosa@gmail.com>
2021-01-09 18:51:15 -0300

    17

--- desafio17.sql
+++ desafio17.sql
@@ -1,10 +1,8 @@
-USE w3schools;
-
 DELIMITER $$
 CREATE TRIGGER trigger_inserir_data_atual
-AFTER INSERT ON orders
+BEFORE INSERT ON orders
 FOR EACH ROW
 BEGIN
-SET orders(OrderDate) = NOW();
+SET NEW.OrderDate = NOW();
 END; $$
 DELIMITER ;
