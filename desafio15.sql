cfbeb0c77485a4319ce08b8328efac7a52a778cd
Alessandra <itsalematos@hotmail.com>
2021-01-09 19:39:45 -0400

    feat: 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(job_title VARCHAR(100))
+BEGIN
+	SELECT ROUND(AVG(E.SALARY),2) AS 'Média Salarial'
+    FROM employees AS E
+    INNER JOIN jobs AS J
+    ON E.JOB_ID = J.JOB_ID
+    WHERE J.JOB_TITLE = job_title;
+END $$
+DELIMITER ;
+CALL buscar_media_por_cargo('Programmer');
