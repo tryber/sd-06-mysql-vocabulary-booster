bfa68bb060c7ff9d0375abfa0cb8174dd65aa842
Alessandra <itsalematos@hotmail.com>
2021-01-09 19:49:49 -0400

    fix: 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,13 +1,12 @@
 DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo(IN job_title VARCHAR(100))
+CREATE PROCEDURE buscar_media_por_cargo(IN search_term VARCHAR(200))
 BEGIN
-SELECT ROUND(AVG(E.SALARY),2) AS 'Média Salarial'
-FROM employees AS E
-INNER JOIN jobs AS J
-ON E.JOB_ID = J.JOB_ID
-WHERE J.JOB_TITLE = job_title;
+  SELECT ROUND(AVG(salary), 2) AS `Média salarial`
+FROM employees
+  INNER JOIN jobs ON employees.job_id = jobs.job_id
+WHERE job_title = search_term;
 END $$
 
 DELIMITER ;
