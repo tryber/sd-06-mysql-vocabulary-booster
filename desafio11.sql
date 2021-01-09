adc6e747520ef4346775b30dab6ed429b805e8dc
Bruna Franciele <brunafranciele1902@gmail.com>
2021-01-09 20:23:55 -0300

    correção do 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,8 @@
-SELECT cn.ContactName AS 'Nome',
-cn.Country AS 'País',
-COUNT(cn.Country) AS 'Número de compatriotas'
-FROM w3schools.customers AS cn, w3schools.customers AS p
-WHERE p.Country = cn.Country AND cn.ContactName <> p.ContactName
-GROUP BY `Nome`
-ORDER BY `Nome`;
+SELECT A.ContactName AS Nome,
+A.Country AS `País`,
+COUNT(B.Country) AS `Número de compatriotas`
+FROM w3schools.customers AS B
+INNER JOIN w3schools.customers AS A
+ON A.Country = B.Country AND A.ContactName <> B.ContactName
+GROUP BY A.CustomerID
+ORDER BY Nome;
