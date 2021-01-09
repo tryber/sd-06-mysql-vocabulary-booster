f9019dec06c593c33daa1ad7f3e193b1196b7767
Alessandra <itsalematos@hotmail.com>
2021-01-09 19:42:19 -0400

    fix: code climate

--- desafio15.sql
+++ desafio15.sql
@@ -2,11 +2,11 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(job_title VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(E.SALARY),2) AS 'Média Salarial'
-    FROM employees AS E
-    INNER JOIN jobs AS J
-    ON E.JOB_ID = J.JOB_ID
-    WHERE J.JOB_TITLE = job_title;
+SELECT ROUND(AVG(E.SALARY),2) AS 'Média Salarial'
+FROM employees AS E
+INNER JOIN jobs AS J
+ON E.JOB_ID = J.JOB_ID
+WHERE J.JOB_TITLE = job_title;
 END $$
 DELIMITER ;
 CALL buscar_media_por_cargo('Programmer');
