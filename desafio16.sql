397ff355241d67e09f51be2b5c3d047d279b15de
higoramorim <1810042@aluno.univesp.br>
2021-01-09 16:20:25 -0300

    q16 +++

--- desafio16.sql
+++ desafio16.sql
@@ -1,11 +1,11 @@
 use hr;
 DELIMITER $$
 
-CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email varchar(50))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(mail varchar(50))
 RETURNS INT READS SQL DATA
 BEGIN
 RETURN (SELECT COUNT(J.JOB_ID) AS `total de empregos` FROM hr.job_history AS J
-WHERE J.EMPLOYEE_ID = (SELECT E.EMPLOYEE_ID FROM hr.employess AS E WHERE E.EMAIL = email));
+WHERE J.EMPLOYEE_ID = (SELECT E.EMPLOYEE_ID FROM hr.employess AS E WHERE E.EMAIL = mail));
 END $$
 
 DELIMITER ;
