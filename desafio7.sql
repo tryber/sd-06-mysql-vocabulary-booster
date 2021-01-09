36e4bfa05fa7d23987d249deed9c2f2ffc9562fc
higoramorim <1810042@aluno.univesp.br>
2021-01-09 16:57:26 -0300

    q7 again

--- desafio7.sql
+++ desafio7.sql
@@ -2,6 +2,6 @@ select ucase(concat(E.FIRST_NAME, ' ', E.LAST_NAME)) as `Nome completo`,
 H.START_DATE as `Data de início do cargo`,
 E.SALARY as `Salário`
 from hr.employees as E
-join hr.job_history as H on E.EMPLOYEE_ID = H.EMPLOYEE_ID
+join hr.job_history as H on H.EMPLOYEE_ID = E.EMPLOYEE_ID
 where month(H.START_DATE) between 1 and 3
 order by concat(E.FIRST_NAME, ' ', E.LAST_NAME) asc, H.START_DATE asc;
