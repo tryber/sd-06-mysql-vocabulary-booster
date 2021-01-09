5843d1d2208f7b420f0bad4342aa91f7b7a07dfd
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-09 00:32:11 -0300

    Desafio 13

--- desafio13.sql
+++ desafio13.sql
@@ -2,4 +2,5 @@ SELECT P.ProductName AS Produto,
 P.Price AS `Preço`
 FROM w3schools.products AS P
 INNER JOIN w3schools.order_details AS D ON P.ProductID = D.ProductID
-WHERE D.Quantity > 80;
+WHERE D.Quantity > 80
+ORDER BY Produto;
