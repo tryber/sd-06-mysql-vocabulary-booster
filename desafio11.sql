3eab4d5412db8ce01c0d50e44af4d9c9d98160d1
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-08 23:53:07 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,6 @@ A.Country AS `País`,
 COUNT(B.ContactName) AS `Número de compatriotas`
 FROM w3schools.customers AS A
 INNER JOIN w3schools.customers AS B
-WHERE A.Country = B.Country AND A.ContactName <> B.ContactName
+ON A.Country = B.Country AND A.ContactName <> B.ContactName
 GROUP BY Nome
 ORDER BY Nome;
