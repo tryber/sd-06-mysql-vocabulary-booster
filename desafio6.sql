fe1f1822e637df70c4aa0d3fc5e07d1e1e82395e
Bruna Franciele <brunafranciele1902@gmail.com>
2021-01-09 17:36:15 -0300

    envio do desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
+j.JOB_TITLE AS Cargo,
+e.HIRE_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees AS e
+JOIN hr.jobs AS j
+ON j.JOB_ID = e.JOB_ID
+JOIN hr.departments AS d
+ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+GROUP BY Cargo
+ORDER BY `Nome completo`, Cargo DESC;
