803917119ad433586c1cbcfa05a09946026f2fac
Daniel Moura <darthur.moura@gmail.com>
2021-01-10 15:19:43 -0300

    Corrigindo 10

--- desafio10.sql
+++ desafio10.sql
@@ -6,6 +6,6 @@ SELECT
 FROM w3schools.order_details AS odet
 JOIN w3schools.products AS prod
 ON odet.ProductID = prod.ProductID
-WHERE odet.Quantity > 20
 GROUP BY prod.ProductName
+HAVING ROUND(AVG(odet.Quantity), 2) > 20
 ORDER BY ROUND(AVG(odet.Quantity), 2), prod.ProductName;
