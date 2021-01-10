282fcf96252ac2f2833d5ae71b61d9f00df5d219
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-10 18:42:53 -0300

    req 10-correcao - req 09-IP

--- desafio10.sql
+++ desafio10.sql
@@ -2,14 +2,14 @@ SELECT
 w3schools.products.ProductName AS `Produto`,
 MIN(w3schools.order_details.Quantity) AS 'Mínima',
 MAX(w3schools.order_details.Quantity) AS 'Máxima',
-ROUD(AVG(w3schools.order_details.Quantity)) AS `Média`
+ROUND(AVG(w3schools.order_details.Quantity)) AS `Média`
 
 FROM w3schools.products, w3schools.order_details
 
 WHERE w3schools.products.ProductID = w3schools.order_details.ProductID
 
 GROUP BY w3schools.products.ProductID
-HAVING `Média` > 20
+HAVING `Média` > 20.00
 -- EXISTS `Média` > 20
 
 ORDER BY `Média`, `Produto`;
