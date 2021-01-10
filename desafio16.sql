b78a2984eadbdd2aaa2d510d6646b955dc1850f0
kissyla <kissylamottapereira@gmail.com>
2021-01-10 16:12:08 -0300

    consertando desafio 16

--- desafio16.sql
+++ desafio16.sql
@@ -4,6 +4,7 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (email varchar(50)
 RETURNS int
 DETERMINISTIC
 BEGIN
+DECLARE employee_email char(50);
 SELECT COUNT(jh.JOB_ID) 
 FROM hr.job_history AS jh
 INNER JOIN hr.employees AS em
