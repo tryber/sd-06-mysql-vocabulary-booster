7bc4f62aa524c8276b933ad2c03a0569fbef7340
pa-volpin <paulo.volpin@gmail.com>
2021-01-10 13:50:13 -0300

    Sintax corrections

--- desafio10.sql
+++ desafio10.sql
@@ -1,11 +1,11 @@
 SELECT * FROM (
-SELECT t_prod.ProductName AS 'Produto',
-MIN(t_details.Quantity) AS 'Mínima',
-MAX(t_details.Quantity) AS 'Máxima',
-ROUND(AVG(t_details.Quantity), 2) AS 'Média'
-FROM w3schools.products AS t_prod
-INNER JOIN w3schools.order_details AS t_details ON t_details.ProductID = t_prod.ProductID
-GROUP BY t_prod.ProductID
+  SELECT t_prod.ProductName AS 'Produto',
+  MIN(t_details.Quantity) AS 'Mínima',
+  MAX(t_details.Quantity) AS 'Máxima',
+  ROUND(AVG(t_details.Quantity), 2) AS 'Média'
+  FROM w3schools.products AS t_prod
+  INNER JOIN w3schools.order_details AS t_details ON t_details.ProductID = t_prod.ProductID
+  GROUP BY t_prod.ProductID
 ) table_before_filter
 WHERE Média > 20.00
 ORDER BY Média, Produto;
--- desafio7.ssql.sql
+++
@@ -1,5 +0,0 @@
-SELECT t_hist.START_TIME, t_emp.FIRST_NAME
-FROM hr.employees AS t_emp
-INNER JOIN hr.job_history AS t_hist
-ON t_emp.EMPLOYEE_ID = t_hist.EMPLOYEE_ID
-ORDER BY t_hist.START_TIME;
