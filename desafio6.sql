67d45265d29954bb6def58816b9ea1dcd31d948a
Cássio Rubens <cassiorubensadv@gmail.com>
2021-01-10 18:29:58 -0300

    FEAT: Desafio 7

--- desafio6.sql
+++ desafio6.sql
@@ -6,7 +6,7 @@
 -- A terceira deve possuir o alias "Data de início do cargo" e exibir a data que a pessoa iniciou o cargo.
 -- A quarta deve possuir o alias "Departamento" e mostrar o nome do departamento em que a pessoa exerceu seu cargo.
 -- Os resultados devem estar ordenados pelo nome completo das pessoas empregadas em ordem decrescente.
--- Em caso de empate no nome completo, ordene os resultados pelo nome do cargo em ordem alfabética.-- SELECT * FROM hr.jobs;
+-- Em caso de empate no nome completo, ordene os resultados pelo nome do cargo em ordem alfabética.
 SELECT CONCAT(epy.FIRST_NAME, ' ', epy.LAST_NAME) AS `Nome completo`,
 jb.JOB_TITLE AS `Cargo`,
 his.START_DATE AS 'Data de início do cargo',
