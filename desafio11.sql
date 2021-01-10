9983d41e64530077c987f96a9b5523e8f0ac1fdb
Daniel Moura <darthur.moura@gmail.com>
2021-01-10 15:25:49 -0300

    Corrigindo 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
 SELECT
-    cust.CustomerName AS 'Nome',
+    cust.ContactName AS 'Nome',
     cust.Country AS 'País',
     (COUNT(*) - 1) AS 'Número de compatriotas'
 FROM w3schools.customers c, w3schools.customers cust
-WHERE c.Country = cust.Country AND c.CustomerID != cust.CustomerID
-GROUP BY cust.CustomerName
+WHERE c.Country = cust.Country
+GROUP BY cust.ContactName
 HAVING (COUNT(*) - 1) > 0
-ORDER BY cust.CustomerName;
+ORDER BY cust.ContactName;
