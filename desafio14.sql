19162d6d6a3d2bf0571d79f3e2eb56f02f375bfc
pa-volpin <paulo.volpin@gmail.com>
2021-01-10 16:26:43 -0300

    Corrections on task 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT Country  FROM w3schools.customers
+SELECT DISTINCT Country AS País FROM w3schools.customers
 UNION
-SELECT DISTINCT Country  FROM w3schools.suppliers
-ORDER BY Country
+SELECT DISTINCT Country FROM w3schools.suppliers
+ORDER BY País
 LIMIT 5;
