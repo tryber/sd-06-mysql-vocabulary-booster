40ca2f38f5580d24edeea1bb45b51d05076996b1
pa-volpin <paulo.volpin@gmail.com>
2021-01-10 19:32:05 -0300

    Corrections on task 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(T_EMP.FIRST_NAME, " ", T_EMP.LAST_NAME) AS 'Nome completo',
 DATE_FORMAT(T_HIST.START_DATE, "%d/%m/%Y") AS 'Data de início',
-DATE_FORMAT(T_HIST.END_DATE, "%d/%m/%Y") AS 'Data de recisão',
+DATE_FORMAT(T_HIST.END_DATE, "%d/%m/%Y") AS 'Data de rescisão',
 ROUND(TIMESTAMPDIFF(day, T_HIST.START_DATE, T_HIST.END_DATE)/365, 2) AS 'Anos trabalhados'
 FROM hr.job_history AS T_HIST
 INNER JOIN hr.employees AS T_EMP
