c98ec760a9732c86bd645e01e2a633d1fa7420d7
Forna <fornagieri.artur@gmail.com>
2021-01-10 09:45:56 -0300

    fixing some issues

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
--- SELECT C.ContactName AS 'Nome de contato',
--- S.ShipperName AS 'Empresa que fez o envio',
--- O.OrderDate AS 'Data do pedido'
--- FROM customers AS C, orders AS O, shippers AS S
--- WHERE S.ShipperName IN ('Speedy Express','United Package')
--- GROUP BY C.ContactName
--- ORDER BY C.ContactName, S.ShipperName;
+SELECT C.ContactName AS 'Nome de contato',
+S.ShipperName AS 'Empresa que fez o envio',
+O.OrderDate AS 'Data do pedido'
+FROM customers AS C, orders AS O, shippers AS S
+WHERE S.ShipperName IN ('Speedy Express','United Package')
+GROUP BY C.ContactName
+ORDER BY C.ContactName, S.ShipperName;
