61816bfbb76e1ed2bc9230a442d57c0c6be8fe2c
kissyla <kissylamottapereira@gmail.com>
2021-01-10 16:00:48 -0300

    codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -2,11 +2,11 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN JobTitle varchar(50))
 BEGIN
-	SELECT ROUND(AVG(em.SALARY), 2) AS 'Média salarial'
-    FROM hr.employees AS em
-    INNER JOIN hr.jobs AS j
-    ON em.JOB_ID = j.JOB_ID
-    WHERE JOB_TITLE = JobTitle;
+SELECT ROUND(AVG(em.SALARY), 2) AS 'Média salarial'
+  FROM hr.employees AS em
+  INNER JOIN hr.jobs AS j
+  ON em.JOB_ID = j.JOB_ID
+  WHERE JOB_TITLE = JobTitle;
 END $$
 DELIMITER ;
 
