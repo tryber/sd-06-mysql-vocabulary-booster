5ae5cfe77333fea2d76f0e9332ea94dba2a796d0
PAULO LINS <paulolins100@gmail.com>
2021-01-10 17:56:00 -0300

    desafio5-FIX

--- desafio5.sql
+++ desafio5.sql
@@ -3,4 +3,4 @@ JOB_TITLE as Cargo,
 round((MAX_SALARY-MIN_SALARY),2) AS `Variação Salarial`,
 round((MIN_SALARY/12), 2) AS 'Média mínima mensal',
 round((MAX_SALARY/12), 2) AS 'Média máxima mensal'
-FROM HR.JOBS order by `Variação Salarial`, JOB_TITLE;
+FROM hr.jobs order by `Variação Salarial`, JOB_TITLE;
