4afced47d470d65bf024e15802472ff7d3122b6f
kissyla <kissylamottapereira@gmail.com>
2021-01-10 17:21:48 -0300

    consertando desafio 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,4 +1,7 @@
-SELECT productName AS 'Produto',
-Price AS 'Preço'
-FROM w3schools.products
+SELECT p.productName AS 'Produto',
+p.Price AS 'Preço'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON p.ProductID = od.ProductID
+WHERE od.Quantity > 80
 ORDER BY ProductName;
