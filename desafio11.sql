5d434c4c32e37456e56b896c09c83092a3702120
Daniel Moura <darthur.moura@gmail.com>
2021-01-10 16:04:12 -0300

    Tentando corrigir o 11 novamente

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,9 @@
 SELECT
     cust.ContactName AS 'Nome',
     cust.Country AS 'País',
-    COUNT(c.Country) AS 'Número de compatriotas'
-FROM w3schools.customers c, w3schools.customers cust
-WHERE c.Country = cust.Country AND cust.ContactName != c.ContactName
+    (COUNT(*) - 1) AS 'Número de compatriotas'
+FROM w3schools.customers cust, w3schools.customers c
+WHERE cust.Country = c.Country
 GROUP BY cust.ContactName
+HAVING (COUNT(*) - 1) > 0
 ORDER BY cust.ContactName;
