a9970a0a9db039c4b4ad954e0b0bebe053db8b3f
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-10 15:54:36 -0300

    req 09

--- desafio9.sql
+++ desafio9.sql
@@ -1,1 +1,10 @@
-SELECT notes FROM northwind.purchase_orders WHERE notes IS NOT NULL;
+SELECT 
+CONCAT(w3schools.employees.FirstName, ' ', w3schools.employees.LastName) AS `Nome completo`,
+COUNT(w3schools.orders.EmployeeID) AS `Total de Pedidos`
+
+FROM w3schools.employees, w3schools.orders
+WHERE w3schools.employees.EmployeeID = w3schools.orders.EmployeeID
+GROUP BY  CONCAT(w3schools.employees.FirstName, ' ', w3schools.employees.LastName)
+-- GROUP BY `Nome completo`
+ORDER BY COUNT(w3schools.orders.EmployeeID)
+-- ORDER `Total de Pedidos`
