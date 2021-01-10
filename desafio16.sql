561076603a49d98deab98dddfbef10befd7bf257
HCaetano <hrcgeo@gmail.com>
2021-01-10 16:58:25 -0300

    Fix challenge 16, make it use function

--- desafio16.sql
+++ desafio16.sql
@@ -1,13 +1,16 @@
+DROP FUNCTION IF EXISTS buscar_quantidade_de_empregos_por_funcionario;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN search_term VARCHAR(200))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(search_term VARCHAR(200))
+RETURNS INT READS SQL DATA
 BEGIN
-SELECT COUNT(job_history.employee_id) AS total_empregos
+DECLARE result INT;
+SELECT COUNT(job_history.employee_id) 
 FROM job_history
 INNER JOIN employees ON job_history.employee_id = employees.employee_id
-WHERE email = search_term;
+WHERE email = search_term
+INTO result;
+RETURN result;
 END $$
 
 DELIMITER ;
-
-CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
