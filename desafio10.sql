40b74159f0c67332f4bcfab63a468445af78189c
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-10 18:32:18 -0300

    req 10 - req 09-IP

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,15 @@
-SELECT * FROM northwind.purchase_orders WHERE created_by >= 3 ORDER BY created_by DESC, id;
+SELECT
+w3schools.products.ProductName AS `Produto`,
+MIN(w3schools.order_details.Quantity) AS 'Mínima',
+MAX(w3schools.order_details.Quantity) AS 'Máxima',
+ROUD(AVG(w3schools.order_details.Quantity)) AS `Média`
+
+FROM w3schools.products, w3schools.order_details
+
+WHERE w3schools.products.ProductID = w3schools.order_details.ProductID
+
+GROUP BY w3schools.products.ProductID
+HAVING `Média` > 20
+-- EXISTS `Média` > 20
+
+ORDER BY `Média`, `Produto`;
