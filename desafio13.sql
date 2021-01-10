db245932d2b44b358784af11fd0eeff0c1c54e0f
Leandro <leandro.feitoza8018@gmail.com>
2021-01-10 00:54:52 -0300

    desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT 
+    P.ProductName AS 'Produto', P.Price AS 'Preço'
+FROM
+    w3schools.products AS P
+        INNER JOIN
+    w3schools.order_details AS Od ON P.ProductID = Od.ProductID
+WHERE
+    Od.Quantity > 80
+ORDER BY `Produto
