b3db327a8c08a9f304f4f9caceb097eb7af374ca
luiz <phellipe501@gmail.com>
2021-01-10 12:06:15 -0300

    fixing 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT co.COUNTRY_NAME as 'País', IF(co.REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão' FROM hr.countries as co order by co.COUNTRY_NAME;
+SELECT COUNTRY_NAME as 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão' FROM hr.countries as co order by COUNTRY_NAME;
