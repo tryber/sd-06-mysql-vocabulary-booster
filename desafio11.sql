b9d0defc43929e6e46a5f86d94d9b14c0122e50c
FlavioSugano <flavio.sugano@gmail.com>
2021-01-10 16:04:11 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+SELECT 
+    c.ContactName AS `Nome`,
+    c.Country AS `País`,
+    s.`Número de compatriotas`
+FROM
+    w3schools.customers AS c
+        INNER JOIN
+    (SELECT 
+        Country, COUNT(Country) AS `Número de compatriotas`
+    FROM
+        w3schools.customers
+    GROUP BY Country) AS s ON s.Country = c.Country
+HAVING `Número de compatriotas` > 2
+ORDER BY `Nome`;
