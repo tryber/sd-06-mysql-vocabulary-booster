9a7d5275ebb476ccbd44ccc4a6afcb973a881f8d
vapatty6848 <vapatty@gmail.com>
2021-01-10 11:04:06 -0300

    lint

--- desafio14.sql
+++ desafio14.sql
@@ -2,5 +2,4 @@ SELECT Country AS 'País' FROM w3schools.customers
 UNION
 SELECT Country FROM w3schools.suppliers
 ORDER BY `País`
-LIMIT 5;
- 
+LIMIT 5; 
--- desafio15.sql
+++ desafio15.sql
@@ -2,14 +2,14 @@ Use hr
 DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN nome_cargo VARCHAR(100))
-BEGIN
-  SELECT ROUND(AVG(E.salary), 2) AS "Média Salarial"
-  FROM hr.employees AS E
-  INNER JOIN hr.jobs AS J ON E.job_id = J.job_id
-  WHERE J.job_title = nome_cargo;
-END $$ 
 
-  DELIMITER ; 
+  BEGIN
+      SELECT ROUND(AVG(E.salary), 2) AS "Média Salarial"
+      FROM hr.employees AS E
+      INNER JOIN hr.jobs AS J ON E.job_id = J.job_id
+      WHERE J.job_title = nome_cargo;
+  END $$ 
+
+DELIMITER ; 
 
 CALL buscar_media_por_cargo('Programmer');
-
