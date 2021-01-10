b8448bef333ba3315e9c56da377baa096ecf6091
PAULO LINS <paulolins100@gmail.com>
2021-01-10 16:54:46 -0300

    desafio5-FIX

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,6 @@
 SELECT 
-j.JOB_TITLE as Cargo,
-round((j.MAX_SALARY-j.MIN_SALARY),2) AS `Variação Salarial`,
-round((j.MIN_SALARY/12), 2) AS 'Média mínima mensal',
-round((j.MAX_SALARY/12), 2) AS 'Média máxima mensal'
-FROM HR.JOBS AS j
-join HR.EMPLOYEES as e
-on j.job_id = e.JOB_ID
-group by Cargo
-order by `Variação Salarial`, Cargo;
+JOB_TITLE as Cargo,
+round((MAX_SALARY-MIN_SALARY),2) AS `Variação Salarial`,
+round((MIN_SALARY/12), 2) AS 'Média mínima mensal',
+round((MAX_SALARY/12), 2) AS 'Média máxima mensal'
+FROM HR.JOBS order by `Variação Salarial`, Cargo;
