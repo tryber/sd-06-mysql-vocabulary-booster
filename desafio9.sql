ddb299bd427a5bbfd653e9bede640aefadebf284
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-10 18:54:09 -0300

    req 09-correcao.03

--- desafio9.sql
+++ desafio9.sql
@@ -5,7 +5,8 @@ COUNT(w3schools.orders.EmployeeID) AS `Total de pedidos`
 FROM w3schools.employees, w3schools.orders
 WHERE w3schools.employees.EmployeeID = w3schools.orders.EmployeeID
 -- GROUP BY  CONCAT(w3schools.employees.FirstName, ' ', w3schools.employees.LastName)
-GROUP BY `Nome completo`
+-- GROUP BY `Nome completo`
+GROUP BY w3schools.orders.EmployeeID
 -- GROUP BY `Total de pedidos`
--- ORDER BY COUNT(w3schools.orders.EmployeeID)
-ORDER `Total de pedidos`;
+ORDER BY COUNT(w3schools.orders.EmployeeID)
+-- ORDER `Total de pedidos`;
