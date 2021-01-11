1de951c5bb3e969466a708c7761b3de23d65ac00
rafaelgelatti <rafael.gelatti@gmail.com>
2021-01-11 17:23:04 -0300

    req16

---
+++ desafio16.sql
@@ -0,0 +1,13 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(
+IN email VARCHAR(100))
+BEGIN
+SELECT COUNT(jh.EMPLOYEE_ID) AS total_empregos
+FROM hr.job_history jh, hr.employees e
+WHERE jh.EMPLOYEE_ID = e.EMPLOYEE_ID AND email = e.EMAIL;
+END $$
+
+DELIMITER ;
