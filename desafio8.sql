5d471fd8c36ccceba556772d3480bfb84a8a3da0
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-01-11 20:07:26 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT ContactName as `Nome do contato`,
+  ShipperName AS `Empresa que faz o envio`,
+  OrderDate AS `Data do pedido`
+FROM w3schools.customers AS c
+  JOIN w3schools.orders AS o
+    ON c.CustomerID = o.CustomerID
+  JOIN w3schools.shippers AS s
+    ON o.ShipperID  = s.ShipperID
+WHERE s.ShipperID IN (1, 2)
+ORDER BY `Nome do contato`, `Data do pedido`;
