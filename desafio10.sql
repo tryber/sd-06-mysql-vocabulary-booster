ed4e40fe96ac40332be7c203f012245b2a13da88
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-11 16:32:56 -0300

    fix: correct bug

--- desafio10.sql
+++ desafio10.sql
@@ -1,11 +1,11 @@
 SELECT
 P.ProductName AS Produto,
-MIN(Quantity) AS Mínima,
-MAX(Quantity) AS Máxima,
-ROUND(AVG(Quantity), 2) AS Média
+MIN(O.Quantity) AS Mínima,
+MAX(O.Quantity) AS Máxima,
+ROUND(AVG(O.Quantity), 2) AS Média
 FROM w3schools.products AS P
 INNER JOIN w3schools.order_details AS O
 ON P.ProductID = O.ProductID
 GROUP BY Produto
 HAVING Média > 20
-ORDER BY Média ASC;
+ORDER BY Média, Produto ASC;
--- desafio11.sql
+++ desafio11.sql
@@ -4,6 +4,6 @@ A.Country AS País,
 COUNT(B.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS B
 INNER JOIN w3schools.customers AS A
-ON A.Country = B.Country AND A.CustomerName <> B.CustomerName
-GROUP BY Nome
+ON A.Country = B.Country AND A.CustomerID <> B.CustomerID
+GROUP BY Nome, País
 ORDER BY Nome ASC;
