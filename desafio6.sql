a84def6b65362f864d40f2daf1cecdc86755b251
PAULO LINS <paulolins100@gmail.com>
2021-01-11 12:27:40 -0300

    desafio6fix

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
 j.JOB_TITLE AS 'Cargo',
 h.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
@@ -6,4 +6,4 @@ FROM hr.job_history AS h
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j ON j.JOB_ID = h.JOB_ID
 INNER JOIN hr.departments AS d ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, 'Cargo' ASC;
+ORDER BY `Nome completo` DESC, JOB_TITLE;
