89d657e6a9424913e9d7cf8bf96096cd4e2c480f
PAULO LINS <paulolins100@gmail.com>
2021-01-11 12:34:42 -0300

    desafio7fix

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT UPPER(CONCAT(N.FIRST_NAME, ' ', N.LAST_NAME)) AS `Nome completo`,
 S.START_DATE AS `Data de início`,
-n.SALARY AS `Salário`
+N.SALARY AS `Salário`
 FROM hr.job_history AS S
 JOIN hr.employees AS N ON S.EMPLOYEE_ID = N.EMPLOYEE_ID
 where month(START_DATE) <= 3
