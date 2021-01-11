635f45d065bfded6c9a94c150452c4477ed9345b
ricknois <rickssti@outlook.com>
2021-01-11 00:20:50 -0300

    ...

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS 'Produto',
+min(o.Quantity) AS 'Mínima',
+min(o.Quantity) AS 'Máxima',
+ROUND(AVG(o.Quantity), 2) AS 'Média'
+FROM w3schools.order_details AS o
+INNER JOIN w3schools.products AS p
+ON o.ProductID = p.ProductID
+GROUP BY p.ProductName HAVING ROUND(AVG(o.Quantity), 2) > 20
+ORDER BY ROUND(AVG(o.Quantity), 2) ASC, p.ProductName ASC;
---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT c1.ContactName AS 'Nome', 
+c1.Country AS 'País',
+(COUNT(*) -1) AS 'Número de compatriotas'
+FROM w3schools.customers AS c1, 
+w3schools.customers AS c2
+WHERE c1.Country = c2.Country
+GROUP BY c1.ContactName, c1.Country HAVING (COUNT(*) - 1) > 0
+ORDER BY c1.ContactName ASC;
