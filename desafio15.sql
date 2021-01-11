bfe82f98e6b5329560d3312e97b051abcb0d1268
Renan Cunha <renan2610@gmail.com>
2021-01-10 22:04:09 -0300

    Adding challenge 15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+use hr;
+
+DELIMITER $$
+
+create procedure buscar_media_por_cargo(in cargo varchar(100))
+begin
+select round(avg(e.SALARY), 2) from hr.employees as e
+inner join hr.jobs as j on e.JOB_ID = j.JOB_ID
+where j.JOB_TITLE = cargo;
+end $$
+
+delimiter ;
+
+CALL buscar_media_por_cargo('Programmer');
