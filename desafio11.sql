940bd6e4b1694b4bbbc26e6cab31114a87131d3d
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-11 15:18:12 -0300

    feat: number of compatriots

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT
+A.ContactName AS Nome,
+A.Country AS País,
+COUNT(B.Country) AS `Número de compatriotas`
+FROM w3schools.customers AS B
+INNER JOIN w3schools.customers AS A
+ON A.Country = B.Country AND A.CustomerName <> B.CustomerName
+GROUP BY Nome
+ORDER BY Nome;
