74c769fd46057ba74084eec57e0508e59e0aea61
kissyla <kissylamottapereira@gmail.com>
2021-01-11 18:54:19 -0300

    desafio 7 corrigido pela milesima vez

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,4 @@ FROM hr.employees
 INNER JOIN hr.job_history AS jh
 ON employees.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE MONTH(jh.START_DATE) BETWEEN 1 AND 3
-ORDER BY `Nome completo` ASC, jh.START_DATE DESC;
+ORDER BY `Nome completo`, jh.START_DATE;
