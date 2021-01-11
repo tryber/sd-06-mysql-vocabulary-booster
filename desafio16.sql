0604ac2e195e54d89f0a975aedd11f280c4708c7
rafaelgelatti <rafael.gelatti@gmail.com>
2021-01-11 17:46:16 -0300

    req16 is supposed to be a FUNCTION, not a PROCEDURE fml

--- desafio16.sql
+++ desafio16.sql
@@ -1,13 +1,14 @@
 USE hr;
-
 DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(
-IN email VARCHAR(100))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA
 BEGIN
+DECLARE number_of_jobs INT;
 SELECT COUNT(jh.EMPLOYEE_ID)
 FROM hr.job_history jh, hr.employees e
-WHERE jh.EMPLOYEE_ID = e.EMPLOYEE_ID AND email = e.EMAIL;
+WHERE jh.EMPLOYEE_ID = e.EMPLOYEE_ID AND email = e.EMAIL INTO number_of_jobs;
+RETURN number_of_jobs;
 END $$
 
 DELIMITER ;
