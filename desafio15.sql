35d22287f8d68d1de1b9f57403372ed918766df6
Renan Cunha <renan2610@gmail.com>
2021-01-10 22:10:49 -0300

    fix: challenge 15 - column name

--- desafio15.sql
+++ desafio15.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 
 create procedure buscar_media_por_cargo(in cargo varchar(100))
 begin
-select round(avg(e.SALARY), 2) from hr.employees as e
+select round(avg(e.SALARY), 2) as 'Média salarial' from hr.employees as e
 inner join hr.jobs as j on e.JOB_ID = j.JOB_ID
 where j.JOB_TITLE = cargo;
 end $$
