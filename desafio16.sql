cf8b118c1eb0b06e15643cf75cf419c5192196a4
rafaelgelatti <rafael.gelatti@gmail.com>
2021-01-11 17:30:43 -0300

    removing req16 alias

--- desafio16.sql
+++ desafio16.sql
@@ -5,7 +5,7 @@ DELIMITER $$
 CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(
 IN email VARCHAR(100))
 BEGIN
-SELECT COUNT(jh.EMPLOYEE_ID) AS total_empregos
+SELECT COUNT(jh.EMPLOYEE_ID)
 FROM hr.job_history jh, hr.employees e
 WHERE jh.EMPLOYEE_ID = e.EMPLOYEE_ID AND email = e.EMAIL;
 END $$
