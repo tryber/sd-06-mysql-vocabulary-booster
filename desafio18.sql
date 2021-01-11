95ada8d8f38b3a251b0b926a5a35d1afd4d95df6
Paulo Frazão <paulo4187@gmail.com>
2021-01-11 19:01:17 -0300

    add file challenge 18, change code

--- desafio18.sql
+++ desafio18.sql
@@ -3,4 +3,4 @@ date_format(JH.START_DATE, '%d/%m/%Y') 'Data de início',
 date_format(JH.END_DATE, '%d/%m/%Y') 'Data de rescisão',
 ROUND((DATEDIFF(JH.END_DATE, JH.START_DATE)/365), 2) 'Anos trabalhados' 
 FROM hr.job_history JH
-INNER JOIN hr.employees E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID ORDER BY `Nome completo`, `Anos trabalhados` ;
+INNER JOIN hr.employees E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID ORDER BY `Nome completo`, `Anos trabalhados`;
