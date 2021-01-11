6b7e972b98e49514222c79571c365e9fcdb76c0a
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-01-11 16:39:57 -0300

    Fixed challenge 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,6 @@
-SELECT j.JOB_TITLE AS 'Cargo',
-  ROUND(AVG(e.SALARY), 2) AS 'Média Salarial',
+SELECT
+	j.JOB_TITLE AS 'Cargo',
+  ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
   CASE
   WHEN ROUND(AVG(e.SALARY), 2) > 2000 AND ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
   WHEN ROUND(AVG(e.SALARY), 2) > 5801 AND ROUND(AVG(e.SALARY), 2) <= 7500 THEN 'Pleno'
