6ad418e0b8a7f2c016bb0ab824d84b4971494b24
Pedro <ikbenpedro@gmail.com>
2021-01-11 11:26:59 -0300

    req 8

--- desafio7.sql
+++ desafio7.sql
@@ -1,12 +1,10 @@
 SELECT UCASE(CONCAT(employees.first_name, ' ', employees.last_name)) AS `Nome completo`,
 job_history.start_date AS `Data de início`,
 employees.salary AS `Salário`
-FROM (((hr.job_history
+FROM ((hr.job_history
 INNER JOIN hr.jobs
 ON jobs.job_id = job_history.job_id)
 INNER JOIN hr.employees
 ON job_history.employee_id = employees.employee_id)
-INNER JOIN hr.departments
-ON job_history.department_id = departments.department_id)
 WHERE MONTH(job_history.start_date) BETWEEN 0 AND 4
 ORDER BY `Nome completo` ASC, `Data de início` ASC;
--- desafio8.sql
+++ desafio8.sql
@@ -1,1 +1,7 @@
-desafio8.sql
+SELECT customers.contactname AS `Nome de contato`,
+orders.shipperid AS `Empresa que fez o envio`,
+orders.orderdate AS `Data do pedido`
+FROM (w3schools.customers
+INNER JOIN w3schools.orders
+ON customers.customerid = orders.customerid)
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
