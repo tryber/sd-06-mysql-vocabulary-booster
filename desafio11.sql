d302a16ff9476d501e1c78885f65d8c289de7061
Pedro <ikbenpedro@gmail.com>
2021-01-11 18:11:37 -0300

    req 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,5 +2,5 @@ SELECT customers.contactname AS `Nome`,
 customers.country AS `País`,
 count(customers.country) AS `Número de compatriotas`,
 FROM w3schools.customers
-group by customers.country
-ORDER BY customers.contactname ASC;
+group by `País`
+ORDER BY `Nome` ASC;
