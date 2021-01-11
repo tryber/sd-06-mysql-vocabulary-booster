d391d65918b9a6738d6ea60bdf9a66ee239419ea
PAULO LINS <paulolins100@gmail.com>
2021-01-11 18:45:51 -0300

    desafio11fixagain

--- desafio11.sql
+++ desafio11.sql
@@ -3,5 +3,6 @@ c.Country as `País`,
 count(b.Country)-1 as `Número de compatriotas`
 from w3schools.customers as c
 inner join w3schools.customers as b on c.Country = b.Country
-group by `Nome`
-order by `Nome`;
+GROUP BY `Nome`
+having `Número de compatriotas` >= 1
+order by `Nome` asc;
