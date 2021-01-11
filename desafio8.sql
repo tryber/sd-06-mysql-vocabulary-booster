9afd146c4ac047475048918aa8086591a3ae6f68
Pedro <ikbenpedro@gmail.com>
2021-01-11 12:46:50 -0300

    req 8, shippername

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,9 @@
 SELECT customers.contactname AS `Nome de contato`,
-orders.shipperid AS `Empresa que fez o envio`,
+shippers.shippername AS `Empresa que fez o envio`,
 orders.orderdate AS `Data do pedido`
-FROM (w3schools.customers
+FROM ((w3schools.customers
 INNER JOIN w3schools.orders
-ON customers.customerid = orders.customerid)
+ON orders.customerid = customers.customerid)
+INNER JOIN w3schools.shippers
+ON shippers.shipperid = orders.shipperid)
 ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
