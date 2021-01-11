40bbfde9f230db4549fdd9419ac95c262b8baef6
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-11 14:46:11 -0300

    feat: average orders

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+P.ProductName AS Produto,
+MIN(Quantity) AS Mínima,
+MAX(Quantity) AS Máxima,
+ROUND(AVG(Quantity), 2) AS Média
+FROM w3schools.products AS P
+INNER JOIN w3schools.order_details AS O
+ON P.ProductID = O.ProductID
+GROUP BY Produto
+HAVING Média > 20
+ORDER BY Média;
