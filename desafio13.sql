ca9c352fcebd12a4e5297310c083490fecadf168
Pedro <ikbenpedro@gmail.com>
2021-01-12 14:27:24 -0300

    req 13 - remove group by

--- desafio13.sql
+++ desafio13.sql
@@ -3,5 +3,4 @@ products.price AS `Preço`
 FROM (w3schools.products
 INNER JOIN w3schools.order_details
 ON products.productid = order_details.productid AND order_details.quantity > 80)
-GROUP BY products.productname
 ORDER BY products.productname ASC;
