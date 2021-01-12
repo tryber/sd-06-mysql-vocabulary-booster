380abecbd1ac1b9db75c931245ccfb4567fbd456
Pedro <ikbenpedro@gmail.com>
2021-01-12 12:12:58 -0300

    req 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,1 +1,10 @@
-select phone_number from hr.employees;
+SELECT CONCAT(one.FIRST_NAME, ' ', one.LAST_NAME) AS `Nome completo funcionário 1`,
+one.salary AS `Salario funcionário 1`,
+one.phone_number AS `Telefone funcionário 1`,
+CONCAT(two.FIRST_NAME, ' ', two.LAST_NAME) AS `Nome completo funcionário 2`,
+two.salary AS `Salario funcionário 2`,
+two.phone_number AS `Telefone funcionário 2`
+FROM (hr.employees AS two
+INNER JOIN hr.employees AS one
+ON one.job_id = two.job_id and one.employee_id != two.employee_id)
+order by one.FIRST_NAME, two.FIRST_NAME;
