367e91b868f04fe8c2a34909c65ef35a787ca227
Lais Gurgel <laisgurgelf@gmail.com>
2021-01-12 10:01:23 -0300

    Challenges 7, 8, 9, 10

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+SELECT
+cust.ContactName AS 'Nome de contato',
+ship.ShipperName AS 'Empresa que fez o envio',
+ord.OrderDate AS 'Data do pedido'
+FROM w3schools.orders AS ord
+JOIN w3schools.customers AS cust
+ON ord.CustomerID = cust.CustomerID
+JOIN w3schools.shippers AS ship
+ON ship.ShipperID = ord.ShipperID
+WHERE ship.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
