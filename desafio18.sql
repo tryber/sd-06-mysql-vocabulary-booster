6c936d871489589d9ed5f181a0b5c72321b38d9d
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-12 08:56:17 -0300

    Desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -2,7 +2,6 @@ SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
 DATE_FORMAT(J.START_DATE,'%d/%m/%Y') AS `Data de início`,
 DATE_FORMAT(J.END_DATE,'%d/%m/%Y') AS `Data de rescisão`,
 ROUND(DATEDIFF(J.END_DATE - J.START_DATE)/365, 2) AS `Anos trabalhados`
-YEAR(J.END_DATE) - YEAR(J.START_DATE) 
 FROM hr.employees AS E
 JOIN hr.job_history AS J ON E.EMPLOYEE_ID = J.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Anos trabalhados`;
