3e975f4c0e83d1cfb4787aef69ee633d65a1f620
Pedro <ikbenpedro@gmail.com>
2021-01-12 12:45:23 -0300

    req 12 acento de salario

--- desafio12.sql
+++ desafio12.sql
@@ -7,4 +7,4 @@ two.phone_number AS `Telefone funcionário 2`
 FROM (hr.employees AS two
 INNER JOIN hr.employees AS one
 ON one.job_id = two.job_id and one.employee_id != two.employee_id)
-order by one.FIRST_NAME, two.FIRST_NAME;
+order by  `Nome completo funcionário 1`, `Nome completo funcionário 2`;
