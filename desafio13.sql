39dbc3956df48bf5ddfdbd4b67ec4ccc621e3cbf
Pedro <ikbenpedro@gmail.com>
2021-01-12 14:20:52 -0300

    req 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,1 +1,7 @@
-SELECT * FROM w3schools.products;
+SELECT products.productname AS `Produto`,
+products.price AS `Preço`
+FROM (w3schools.products
+INNER JOIN w3schools.order_details
+ON products.productid = order_details.productid AND order_details.quantity > 80)
+GROUP BY products.productname
+ORDER BY products.productname ASC;
