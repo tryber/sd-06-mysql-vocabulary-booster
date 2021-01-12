5258f2148c6ca49286a6865bc7288c1e52727297
Anderson <andersoni.trybe@gmail.com>
2021-01-11 22:37:21 -0300

    Desafio 10 e 11

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT w3schools.products.ProductName AS `Produto`,
+MIN(w3schools.order_details.Quantity) AS 'Mínima',
+MAX(w3schools.order_details.Quantity) AS 'Máxima',
+ROUND(AVG(w3schools.order_details.Quantity), 2) AS `Média`
+FROM w3schools.products, w3schools.order_details
+WHERE w3schools.products.ProductID = w3schools.order_details.ProductID
+GROUP BY w3schools.products.ProductID
+HAVING `Média` > 20.00
+ORDER BY `Média`, `Produto`;
