ae01e95268d1e1a26b74af9128bc26580ec6d4a5
Leandro <leandro.feitoza8018@gmail.com>
2021-01-12 16:07:47 -0300

    corrigido identação

--- desafio16.sql
+++ desafio16.sql
@@ -5,10 +5,14 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100)
 RETURNS INT READS SQL DATA
 BEGIN
  DECLARE total_empregos INT;
-SELECT COUNT(*) AS 'total_empregos'
-FROM hr.job_history
-INNER JOIN hr.employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
-WHERE employees.EMAIL = email INTO total_empregos;
+SELECT 
+    COUNT(*) AS 'total_empregos'
+FROM
+    hr.job_history
+        INNER JOIN
+    hr.employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+WHERE
+    employees.EMAIL = email INTO total_empregos;
 RETURN total_empregos;
 END $$
 
