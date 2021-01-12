0431cd448a1bdda5a2966787efd8b013b05f023f
LughWalle <civalfran1@gmail.com>
2021-01-12 11:25:31 -0300

    desafio 1 erro de digitação acento em 'incluído'

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
 SELECT COUNTRY_NAME AS País,
 CASE 
-WHEN REGION_ID = 1 THEN 'incluido'
-ELSE 'não incluido'
+WHEN REGION_ID = 1 THEN 'incluído'
+ELSE 'não incluído'
 END AS 'Status Inclusão'
 from hr.countries;
