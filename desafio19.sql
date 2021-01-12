c742fd78364d7b8605539ffdd237af8d1343e381
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-01-12 09:06:35 -0300

    Desafio 19

--- desafio19.sql
+++ desafio19.sql
@@ -1,19 +1,17 @@
 USE hr;
-DROP Function IF EXISTS exibir_quantidade_pessoas_contratadas_por_mes_e_ano;
-
-USE hr;
 
 DELIMITER $$
 
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(hired_month INT, hired_year INT)
 RETURNS INT READS SQL DATA
 BEGIN
+DECLARE result INT;
 SELECT COUNT(*) FROM hr.employees 
-WHERE MONTH(HIRE_DATE) = hired_month AND YEAR(HIRE_DATE) = hired_year;
+WHERE MONTH(HIRE_DATE) = hired_month AND YEAR(HIRE_DATE) = hired_year
+INTO result;
+RETURN result;
 END$$
 
 DELIMITER ;
 
-SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987) FROM hr.employees;
-
-SELECT * FROM hr.employees WHERE MONTH(HIRE_DATE) = 6 AND YEAR(HIRE_DATE) = 1987;
+SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987);
