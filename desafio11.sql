270d9fe4847bf5190a46981caf51dc73b59793e7
PAULO LINS <paulolins100@gmail.com>
2021-01-11 21:24:44 -0300

    desafio11fixagain

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
-SELECT c.ContactName AS 'Nome',
-c.Country AS 'País',
-count(b.Country)-1 AS 'Número de compatriotas'
-FROM w3schools.customers AS c
-INNER JOIN w3schools.customers as b on c.Country = b.Country
-GROUP BY c.ContactName
-HAVING count(b.Country)-1 <> 0
-ORDER by c.ContactName asc;
+SELECT a.ContactName AS 'Nome',
+a.Country AS 'País',
+COUNT(b.Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS a, w3schools.customers AS b
+WHERE a.Country = b.Country
+AND a.ContactName <> b.ContactName
+GROUP BY a.CustomerID
+ORDER BY a.ContactName;
