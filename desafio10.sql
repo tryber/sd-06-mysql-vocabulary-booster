2d3bf866c9bc59d5bc2be03c12a7bd0cfcf940f7
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-01-11 23:11:00 -0300

    desafio 10 - refaz solução

--- desafio10.sql
+++ desafio10.sql
@@ -1,8 +1,10 @@
 SELECT ProductName AS `Produto`,
   MIN(Quantity) AS `Mínima`,
-  MAX(Quantity) AS `Máxima`
+  MAX(Quantity) AS `Máxima`,
+  ROUND(AVG(Quantity), 2) AS `Média`
 FROM w3schools.order_details AS od
   LEFT JOIN w3schools.products AS p
     ON od.ProductID = p.ProductID
-WHERE Quantity > 20
-GROUP BY od.ProductID;
+GROUP BY od.ProductID
+  HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
