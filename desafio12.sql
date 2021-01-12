e305d94460f4a14b30ec40a5a0359bbf62798305
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-01-11 23:49:09 -0300

    desafios 12 a 14

---
+++ desafio12.sql
@@ -0,0 +1,17 @@
+SELECT CONCAT(FIRST_NAME, " ", LAST_NAME) AS `Nome completo funcionário 1`,
+ SALARY AS `Salário funcionário 1`,
+ PHONE_NUMBER AS `Telefone funcionário 1`,
+ g.n2 AS `Nome completo funcionário 2`,
+ g.s2 AS `Salário funcionário 2`,
+ g.pn2 AS `Telefone funcionário 2`
+FROM hr.employees,
+  (
+    SELECT CONCAT(FIRST_NAME, " ", LAST_NAME) AS n2,
+      JOB_ID AS j,
+      SALARY AS s2,
+      PHONE_NUMBER AS pn2
+    FROM hr.employees
+  ) AS g
+WHERE JOB_ID = g.j 
+  AND CONCAT(FIRST_NAME, " ", LAST_NAME) <> g.n2
+ORDER BY CONCAT(FIRST_NAME, " ", LAST_NAME), g.n2;
