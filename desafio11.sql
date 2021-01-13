2926f9ffe4c5f564f933725e7f300e5f3936c4af
LughWalle <civalfran1@gmail.com>
2021-01-13 04:20:09 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT cs1.ContactName AS Nome,
+cs1.Country AS País,
+COUNT(cs2.ContactName) AS `Número de compatriotas`
+FROM w3schools.customers AS cs1,
+w3schools.customers AS cs2
+WHERE cs1.ContactName <> cs2.ContactName AND cs1.Country = cs2.Country
+GROUP BY Nome
+ORDER BY Nome;
