c8e69da08852986f25dfeb6b897b3687d60c6561
Matheuscara <matheuscara2000@gmail.com>
2021-01-13 01:21:03 -0300

    tentando terminar

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT
+A.ContactName AS Nome,
+A.Country AS País,
+COUNT(B.Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS A, w3schools.customers AS B
+WHERE A.ContactName <> B.ContactName AND A.Country = B.Country
+GROUP BY A.CustomerID ORDER BY A.ContactName;
