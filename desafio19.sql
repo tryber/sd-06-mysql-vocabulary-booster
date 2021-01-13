49f91fcc130b0fa943b89ae64cdb28b6e5cb46e3
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-13 13:23:13 -0300

    fix req. 19 and req. 20

--- desafio19.sql
+++ desafio19.sql
@@ -4,7 +4,8 @@ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(month INT, y
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE hired_employees INT;
-  SELECT COUNT *
+  SELECT
+    COUNT jh.JOB_ID 'que foram contratadas'
     FROM hr.job_history jh
   WHERE MONTH(jh.START_DATE) = month AND YEAR(jh.START_DATE) = year
     INTO hired_employees;
--- desafio20.sql
+++ desafio20.sql
@@ -3,14 +3,16 @@ DELIMITER $$
 CREATE PROCEDURE exibir_historico_completo_por_funcionario(email)
 BEGIN
   SELECT
-    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME ) 'Nome completo'
-    e.DEPARTMENT_NAME 'Departamento'
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME ) 'Nome completo',
+    d.DEPARTMENT_NAME 'Departamento',
     j.JOB_TITLE 'Cargo'
   FROM hr.job_history jh
     INNER JOIN hr.employees e
       ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
     INNER JOIN hr.jobs j
       ON e.JOB_ID = j.JOB_ID
+    INNER JOIN hr.departments d
+      ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
   ORDER BY `Departamento`, `Cargo`;
 END $$
 
