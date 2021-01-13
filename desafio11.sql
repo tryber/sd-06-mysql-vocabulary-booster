38c96da6147a1b25b4573138280b59303bd010e6
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-13 07:24:05 -0300

    corrigindo desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,6 @@
-USE w3schools;
-DELIMITER $$
-CREATE TRIGGER Inserções
-BEFORE INSERT ON orders
-FOR EACH ROW
-BEGIN
-    SET NEW.OrderDate = CURRENT_DATE();
-END; $$
-DELIMITER ;
+SELECT  c1 . NomeDoContato  AS  ` Nome ` , c1 . País  AS  ` País ` , COUNT ( c2 . País ) -  1  AS  ` Número de compatriotas `
+DE  w3schools . clientes  AS c1
+PARTICIPE do  w3schools . clientes  AS c2 ON  c1 . País  =  c2 . País
+GRUPO POR  c1 . Identificação do Cliente
+TENDO  ` Número de compatriotas `  <>  0
+ORDEM DE  ` Nome ` ;
