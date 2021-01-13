f20bfadde6de11b7e87e19f30c6650fe39fd6b15
Matheus Coutinho <matheuscavalcante88@hotmail.com>
2021-01-13 11:48:15 -0300

    CFC fix

--- desafio13.sql
+++ desafio13.sql
@@ -1,7 +1,7 @@
 SELECT p.ProductName 'Produto',
-    p.Price 'Preço'
+p.Price 'Preço'
 FROM order_details od
-    LEFT JOIN products p
-        ON p.ProductID = od.ProductID
+LEFT JOIN products p
+ON p.ProductID = od.ProductID
 WHERE od.Quantity > 80
 ORDER BY p.ProductName;
