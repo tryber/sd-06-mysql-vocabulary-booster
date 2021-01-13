e7753462e9ea8bb5bab6444eeb0e2a7b89d00821
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-13 07:32:08 -0300

    corrigindo desafio11 PARTE 2

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
-SELECT  c1 . NomeDoContato  AS  ` Nome ` , c1 . País  AS  ` País ` , COUNT ( c2 . País ) -  1  AS  ` Número de compatriotas `
-DE  w3schools . clientes  AS c1
-PARTICIPE do  w3schools . clientes  AS c2 ON  c1 . País  =  c2 . País
-GRUPO POR  c1 . Identificação do Cliente
-TENDO  ` Número de compatriotas `  <>  0
-ORDEM DE  ` Nome ` ;
+SELECT c1.ContactName AS `Nome`, c1.Country AS `País`, COUNT(c2.Country) - 1 AS `Número de compatriotas`
+FROM w3schools.customers AS c1
+JOIN w3schools.customers AS c2 ON c1.Country = c2.Country
+GROUP BY c1.CustomerID
+HAVING `Número de compatriotas` <> 0
+ORDER BY `Nome`;
