a3de1b63e26f17da8303dd29361494a7d850dfb4
flaviolunaferreiral <flaviolunaferreira@gmail.com>
2021-01-13 06:42:55 -0300

    incluindo desafio 17

--- desafiio14.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-P.ProductName AS `Produto`, P.Price AS `Preço`
-FROM w3schools.products AS P INNER JOIN w3schools.order_details AS O
-ON O.ProductID = P.ProductID WHERE  O.Quantity > 80
-ORDER BY `Produto`;
--- desafio15.sql
+++ desafio15.sql
@@ -1,2 +1,14 @@
-SELECT Country AS 'País' FROM w3schools.customers) UNION
-(SELECT Country AS 'País' FROM w3schools.suppliers) ORDER BY `País` LIMIT 5;
+USE hr;
+DROP  PROCEDURE IF EXISTS buscar_media_por_cargo;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo (cargo VARCHAR (40))
+BEGIN
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial' FROM hr.employees AS E
+INNER JOIN hr.jobs AS  J ON J.JOB_ID = E.JOB_ID
+WHERE J.JOB_TITLE = cargo;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
