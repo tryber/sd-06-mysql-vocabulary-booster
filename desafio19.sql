9edb77e1e9dbbdfafdc710b754f1ddebac048ffb
Pedro Risso <pmerchiorattor@gmail.com>
2021-01-15 13:43:24 -0300

    fix table change job_history -employees Data issue

--- desafio19.sql
+++ desafio19.sql
@@ -1,13 +1,13 @@
 USE hr;
 DELIMITER $$
-CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(month INT, year INT)
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(chosen_month INT, chosen_year INT)
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE hired_employees INT;
   SELECT
-    COUNT(jh.JOB_ID) 'que foram contratadas'
-    FROM hr.job_history jh
-  WHERE MONTH(jh.START_DATE) = month AND YEAR(jh.START_DATE) = year
+    COUNT(e.HIRE_DATE) 'que foram contratadas'
+    FROM hr.employees e
+  WHERE MONTH(e.HIRE_DATE) = chosen_month AND YEAR(e.HIRE_DATE) = chosen_year
     INTO hired_employees;
   RETURN hired_employees;
 END $$
