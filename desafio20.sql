67987111efcb887c412943d518194c81d21e29b5
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-01-17 18:47:47 -0300

    Finished challenge 20

--- desafio20.sql
+++ desafio20.sql
@@ -10,12 +10,12 @@ BEGIN
     d.DEPARTMENT_NAME AS `Departamento`
   FROM
     hr.job_history AS jh
-    INNER JOIN hr.employees AS e
-      ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-	  INNER JOIN hr.jobs AS j
-      ON jh.JOB_ID = j.JOB_ID
-	  INNER JOIN hr.departments AS d
-      ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+  INNER JOIN hr.employees AS e
+    ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+	INNER JOIN hr.jobs AS j
+    ON jh.JOB_ID = j.JOB_ID
+	INNER JOIN hr.departments AS d
+    ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
   WHERE e.EMAIL = email
   ORDER BY
     `Nome completo` DESC,
