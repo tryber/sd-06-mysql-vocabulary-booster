8cc823d27f6b49155b7296704ff30de22867c3a3
fomes <contato.felipeluna@gmail.com>
2021-01-19 11:41:27 -0300

    desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
-SELECT country_name AS "País",
-  IF(region_id = 1, "Incluído", NULL) AS "Status Inclusão"
+SELECT country_name AS 'País',
+  IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM countries
-WHERE region_id = 1;
+WHERE region_id = 1
+ORDER BY country_name ASC;
