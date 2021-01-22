7440a0d2e7b2af0a342b772f2674699204e64f5a
fomes <contato.felipeluna@gmail.com>
2021-01-22 17:45:34 -0300

    fix desafio 6, 20

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,13 @@
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-  j.job_title AS 'Cargo',
-  jh.start_date AS 'Data de início do cargo',
-  d.department_name AS 'Departamento'
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+  j.JOB_TITLE AS 'Cargo',
+  jh.START_DATE AS 'Data de início do cargo',
+  d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history AS jh
 LEFT JOIN hr.employees AS e
-  ON jh.employee_id = e.employee_id
+  ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 LEFT JOIN hr.jobs AS j
   ON jh.JOB_ID = j.JOB_ID
 LEFT JOIN hr.departments AS d
-  ON d.department_id = jh.department_id
-WHERE j.job_title IS NOT NULL
-ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC, j.job_title ASC
+  ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+WHERE j.JOB_TITLE IS NOT NULL
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.JOB_TITLE ASC
