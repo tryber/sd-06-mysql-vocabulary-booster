26c3accc33065312b4cde26edb2c5aa0c2c33c34
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-01-22 18:11:59 -0300

    :art: Reqs 15 e 16

---
+++ desafio15.sql
@@ -0,0 +1,10 @@
+USE hr;
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN search_term VARCHAR(10))
+BEGIN
+  SELECT ROUND(AVG(SALARY), 2)  AS "Média salarial" FROM hr.employees
+  WHERE JOB_ID = (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = search_term);
+END $$
+DELIMITER ;
+CALL buscar_media_por_cargo("Programmer");
