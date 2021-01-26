33579898be5ddd0cdb1c634adabc1a327b70e352
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-26 19:07:41 -0300

    Requirement 06 - push 01

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(em.FIRST_NAME, " ", em.LAST_NAME) AS "Nome completo",
+jo.JOB_TITLE AS Cargo,
+jh.START_DATE AS "Data de início do cargo",
+de.DEPARTMENT_NAME AS "Departamento"
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS em ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
+INNER JOIN hr.jobs AS jo ON jh.JOB_ID = jo.JOB_ID
+INNER JOIN hr.departments AS de ON jh.DEPARTMENT_ID = de.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo;
