13dc04737528bff1a6c3be0d8d340da4b12a3501
fomes <contato.felipeluna@gmail.com>
2021-01-26 11:47:59 -0300

    cc fix 11.47

--- desafio20.sql
+++ desafio20.sql
@@ -1,23 +1,14 @@
 USE hr;
-DELIMITER $$
-
-CREATE PROCEDURE exibir_historico_completo_por_funcionario(email VARCHAR(50))
-BEGIN
-SELECT
-CONCAT(e.first_name, ' ', e.last_name) AS `Nome completo`,
-d.depar AS 'Departamento',
-j.job_title AS 'Cargo'
+DELIMITER $$ CREATE PROCEDURE exibir_historico_completo_por_funcionario(email VARCHAR(50)) BEGIN
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+  d.DEPARTMENT_NAME AS 'Departamento',
+  j.JOB_TITLE AS 'Cargo'
 FROM hr.employees AS e
-INNER JOIN hr.job_history AS jh
-ON jh.employee_id = e.employee_id
-INNER JOIN hr.jobs AS j
-ON jh.JOB_ID = j.JOB_ID
-INNER JOIN hr.departments AS d
-ON d.department_id = jh.department_id
+  INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+  INNER JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
+  INNER JOIN hr.departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 WHERE e.EMAIL = "NKOCHHAR"
-ORDER BY `Departamento`, `Cargo`;
-END $$
-
-DELIMITER ;
-
+ORDER BY `Departamento`,
+  `Cargo`;
+END $$ DELIMITER;
 CALL exibir_historico_completo_por_funcionario("NKOCHHAR");
--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,10 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
   j.JOB_TITLE AS 'Cargo',
   jh.START_DATE AS 'Data de início do cargo',
   d.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.job_history AS jh
-LEFT JOIN hr.employees AS e
-  ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-LEFT JOIN hr.jobs AS j
-  ON jh.JOB_ID = j.JOB_ID
-LEFT JOIN hr.departments AS d
-  ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
-WHERE j.JOB_TITLE IS NOT NULL
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.JOB_TITLE ASC
+FROM hr.employees AS e
+  INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+  INNER JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
+  INNER JOIN hr.departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC,
+  j.JOB_TITLE;
