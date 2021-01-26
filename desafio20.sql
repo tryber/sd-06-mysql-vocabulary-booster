ec57b961947daacae4405fbd74f34f86f6fe42f1
fomes <contato.felipeluna@gmail.com>
2021-01-26 11:37:38 -0300

    cc fix 11.37

--- desafio20.sql
+++ desafio20.sql
@@ -1,22 +1,23 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN employee_email VARCHAR(255))
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(email VARCHAR(50))
 BEGIN
-
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-d.department_name AS 'Departamento',
+SELECT
+CONCAT(e.first_name, ' ', e.last_name) AS `Nome completo`,
+d.depar AS 'Departamento',
 j.job_title AS 'Cargo'
 FROM hr.employees AS e
-LEFT JOIN hr.job_history AS jh
-ON e.employee_id = jh.employee_id
-LEFT JOIN hr.departments AS d
+INNER JOIN hr.job_history AS jh
+ON jh.employee_id = e.employee_id
+INNER JOIN hr.jobs AS j
+ON jh.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments AS d
 ON d.department_id = jh.department_id
-LEFT JOIN hr.jobs AS j
-ON j.job_id = jh.job_id
-WHERE e.EMAIL = employee_email
-ORDER BY d.department_name ASC, j.job_title ASC;
-
+WHERE e.EMAIL = "NKOCHHAR"
+ORDER BY `Departamento`, `Cargo`;
 END $$
 
 DELIMITER ;
+
+CALL exibir_historico_completo_por_funcionario("NKOCHHAR");
