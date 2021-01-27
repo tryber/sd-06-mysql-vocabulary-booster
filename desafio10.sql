5b4aa59c54d09511c6da3ed46e2764a0b2d8e867
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-27 10:57:33 -0300

    Requirement 11 - push 01

--- desafio10.sql
+++ desafio10.sql
@@ -6,4 +6,4 @@ FROM w3schools.order_details AS od
 INNER JOIN w3schools.products AS p
 ON od.ProductID = p.ProductID
 GROUP BY `Produto` HAVING Média > 20
-ORDER BY `Média`, `Produto`
+ORDER BY `Média`, `Produto`;
--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,4 @@ FROM w3schools.customers AS c
 INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
 WHERE s.ShipperName = "Speedy Express" OR s.ShipperName = "United Package"
-ORDER BY c.ContactName, s.ShipperName, o.OrderDate
+ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
