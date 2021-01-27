ddfdc0498d6b3f63862bf7f65596569bf2c2718b
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-27 10:53:57 -0300

    Requirement 10 - push 02 change req 09 by mistake

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS "Produto",
+MIN(Quantity) AS "Mínima",
+MAX(Quantity) AS "Máxima",
+ROUND(AVG(Quantity), 2) AS "Média"
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p
+ON od.ProductID = p.ProductID
+GROUP BY `Produto` HAVING Média > 20
+ORDER BY `Média`, `Produto`
