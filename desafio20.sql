289645b9c7e5c5e070d29ef826d48c2ad0e2ca29
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-01-30 18:18:57 -0300

    :art: Desafio 20
    Corrigido o erro

--- desafio20.sql
+++ desafio20.sql
@@ -1,19 +1,23 @@
 USE hr;
 DELIMITER //
 
-CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN search_email VARCHAR(80))
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(email VARCHAR(50))
 BEGIN
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-dep.DEPARTMENT_NAME AS 'Departamento',
+SELECT
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+d.DEPARTMENT_NAME AS 'Departamento',
 j.JOB_TITLE AS 'Cargo'
 FROM hr.employees AS e
-INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-INNER JOIN hr.departments AS dep ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-INNER JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
-WHERE jh.EMPLOYEE_ID = (SELECT EMPLOYEE_ID FROM hr.employees WHERE EMAIL = search_email)
+INNER JOIN hr.job_history AS jh
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.jobs AS j
+ON jh.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments AS d
+ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+WHERE e.EMAIL = "NKOCHHAR"
 ORDER BY `Departamento`, `Cargo`;
 END //
 
 DELIMITER ;
 
-CALL exibir_historico_completo_por_funcionario('NKOCHHAR');
+CALL exibir_historico_completo_por_funcionario("NKOCHHAR");
