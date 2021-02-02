db7c23c3cd4ee718e8ace57960bf3c2821e6b6a7
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-02-02 08:22:48 -0300

    ate 13

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,9 @@
-SELECT JOB_TITLE AS 'Cargo',
-FROM hr.jobs
-ORDER BY
+SELECT c1.ContactName AS 'Nome',
+c1.Country AS 'País',
+(COUNT(*) - 1) AS 'Número de compatriotas'
+FROM w3schools.customers AS c1
+LEFT JOIN w3schools.customers AS c2
+  ON c1.Country = c2.Country
+GROUP BY c1.ContactName, c1.Country
+HAVING (COUNT(*) - 1) > 0
+ORDER BY c1.ContactName;
--- desafio13.sql
+++ desafio13.sql
@@ -1,3 +1,6 @@
-SELECT JOB_TITLE AS 'Cargo',
-FROM hr.jobs
-ORDER BY
+SELECT pr.ProductName AS Produto,
+pr.Price AS Preço
+FROM w3schools.products AS pr
+INNER JOIN w3schools.order_details AS od ON od.ProductID = pr.ProductID
+WHERE od.Quantity > 80
+ORDER BY 1;
