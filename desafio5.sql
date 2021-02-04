ff2168e6fe19b93e72af3c665a01dd09a8250eae
Dan <dansantosalfa@gmail.com>
2021-02-04 17:51:54 -0300

    Refatorando questao 4 e 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,6 @@
-SELECT JOB_TITLE AS 'Cargo',
+SELECT JOB_TITLE AS `Cargo`,
 MAX_SALARY-MIN_SALARY AS `Variação Salarial`,
-ROUND(MIN_SALARY/12,2) AS 'Média mínima mensal',
-FROM hr.jobs ORDER BY `Variação Salarial`, JOB_TITLE;
+ROUND(MIN_SALARY /12, 2) AS `Média mínima mensal`,
+ROUND(MAX_SALARY /12, 2) AS `Média máxima mensal`
+FROM hr.jobs
+ORDER BY `Variação Salarial` ASC, JOB_TITLE ASC;
