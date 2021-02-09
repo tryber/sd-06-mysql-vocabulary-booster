28c9b52ebb445760e34b87184861f388eb69d346
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2021-02-09 10:44:22 -0300

    challenges thirteen to fifteen done

---
+++ desafio15.sql
@@ -0,0 +1,15 @@
+USE hr;
+
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
+
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN name_cargo VARCHAR(50))
+BEGIN
+SELECT (SELECT ROUND(AVG(b.SALARY), 2) 
+FROM hr.jobs AS hj
+INNER JOIN hr.employees AS he
+ON hj.JOB_ID = he.JOB_ID
+GROUP BY hj.JOB_TITLE
+HAVING he.JOB_TITLE = name_cargo) AS `Média salarial`;
+END $$
+DELIMITER ;
