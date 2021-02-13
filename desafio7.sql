a33bf90c3fbc6a00c1226d07f502145df0082bb1
Isabela Rosa <isabelarosa954@gmail.com>
2021-02-13 18:59:51 -0300

    desafio 8 e comentário no desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -5,3 +5,7 @@ FROM hr.employees AS E
 INNER JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 WHERE MONTH(JH.START_DATE) BETWEEN 01 AND 03
 ORDER BY `Nome completo`, `Data de início`;
+
+/* Não compreendo porque o teste não reconhece a query usando o operador IN.
+Mesmo que ela traga exatamente os mesmos resultados que o BETWEEN. Li sobre
+os dois e não vi a diferenca de aplicacao nos dois casos. */
