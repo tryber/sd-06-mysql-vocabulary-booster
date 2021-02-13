2db8000c9a2bac36ea67cfc38d9a420c53d37230
Isabela Rosa <isabelarosa954@gmail.com>
2021-02-13 16:57:49 -0300

    desafio 7: teste: trocando IN por BETWEEN

--- desafio7.sql
+++ desafio7.sql
@@ -3,5 +3,5 @@ SELECT UCASE(CONCAT(E.FIRST_NAME, " ", E.LAST_NAME)) AS `Nome completo`,
   E.SALARY AS "Salário"
 FROM hr.employees AS E
 INNER JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
-WHERE MONTH(JH.START_DATE) IN (01, 02, 03)
-ORDER BY `Nome completo`, `Data de início do cargo`;
+WHERE MONTH(JH.START_DATE) BETWEEN 01 AND 03
+ORDER BY `Nome completo`, `Data de início`;
