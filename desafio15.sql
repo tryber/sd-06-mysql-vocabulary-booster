caa387725be4aa395f935ad94d607e257c2ecff3
Samuel <samuelpp.s@hotmail.com>
2021-02-15 16:17:14 -0300

    tirei todos os tabs

--- desafio15.sql
+++ desafio15.sql
@@ -2,9 +2,9 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
-    FROM hr.employees
-    WHERE JOB_ID IN(SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = cargo);
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+FROM hr.employees
+WHERE JOB_ID IN(SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = cargo);
 END $$
 
 DELIMITER ;
--- desafio19.sql
+++ desafio19.sql
@@ -5,9 +5,9 @@ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano
 RETURNS INT READS SQL DATA
 
 BEGIN
-	DECLARE empregados_nesse_período INT;
-    SELECT COUNT(*) FROM hr.employees WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano INTO empregados_nesse_período;
-    RETURN empregados_nesse_período;
+DECLARE empregados_nesse_período INT;
+SELECT COUNT(*) FROM hr.employees WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano INTO empregados_nesse_período;
+RETURN empregados_nesse_período;
 END $$
 
 DELIMITER ;
