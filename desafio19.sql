dca2b573eb0b076b740835f3d37eb30771e21b30
Lais Gurgel <laisgurgelf@gmail.com>
2021-03-05 11:16:51 -0300

    Challenges 18, 19, 20

---
+++ desafio19.sql
@@ -0,0 +1,18 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(searched_month INT, searched_year INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE hire_count INT;
+
+SELECT COUNT(*)
+FROM hr.employees
+WHERE MONTH(HIRE_DATE) = searched_month AND YEAR(HIRE_DATE) = searched_year
+INTO hire_count;
+
+RETURN hire_count;
+END $$
+
+DELIMITER ;
